239498a49628ea09ee3ac8ead586022b775237f1
jvr <jvr@4cde692c-a291-49d1-8350-778aa11640f8>
2001-08-16 10:35:28 +0000

    behave nicely when the font doesn't do an initial moveto.
    git-svn-id: svn://svn.code.sf.net/p/fonttools/code/trunk@170 4cde692c-a291-49d1-8350-778aa11640f8

--- Lib/fontTools/misc/psCharStrings.py
+++ Lib/fontTools/misc/psCharStrings.py
@@ -400,6 +400,11 @@ class T2OutlineExtractor(SimpleT2Decompiler):
 	def appendPoint(self, point, isPrimary):
 		import Numeric
 		point = self.currentPoint + Numeric.array(point, Numeric.Int16)
+		if not self.contours:
+			# The glyph doesn't start with a moveto. Not sure whether
+			# this is legal, but apparently it usually works.
+			self.newPath()
+			self.appendPoint(self.currentPoint, 1)
 		self.currentPoint = point
 		points, flags, isClosed = self.contours[-1]
 		points.append(point)
