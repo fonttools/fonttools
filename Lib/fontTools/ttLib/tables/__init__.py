20d41f31e40a8d20902fcfea4543fa9c2c4d8cae
jvr <jvr@4cde692c-a291-49d1-8350-778aa11640f8>
2002-06-04 19:08:31 +0000

    add dummy import function, so modulefinder can find our tables.
    git-svn-id: svn://svn.code.sf.net/p/fonttools/code/trunk@269 4cde692c-a291-49d1-8350-778aa11640f8

--- Lib/fontTools/ttLib/tables/__init__.py
+++ Lib/fontTools/ttLib/tables/__init__.py
@@ -1,3 +1,46 @@
-"""Empty __init__.py file to signal Python this directory is a package.
-(It can't be completely empty since WinZip seems to skip empty files.)
-"""
+def _moduleFinderHint():
+	import B_A_S_E_
+	import C_F_F_
+	import D_S_I_G_
+	import DefaultTable
+	import G_D_E_F_
+	import G_P_O_S_
+	import G_S_U_B_
+	import J_S_T_F_
+	import L_T_S_H_
+	import O_S_2f_2
+	import T_S_I_B_
+	import T_S_I_D_
+	import T_S_I_J_
+	import T_S_I_P_
+	import T_S_I_S_
+	import T_S_I_V_
+	import T_S_I__0
+	import T_S_I__1
+	import T_S_I__2
+	import T_S_I__3
+	import T_S_I__5
+	import __init__
+	import _c_m_a_p
+	import _c_v_t
+	import _f_p_g_m
+	import _g_a_s_p
+	import _g_l_y_f
+	import _h_d_m_x
+	import _h_e_a_d
+	import _h_h_e_a
+	import _h_m_t_x
+	import _k_e_r_n
+	import _l_o_c_a
+	import _m_a_x_p
+	import _n_a_m_e
+	import _p_o_s_t
+	import _p_r_e_p
+	import _v_h_e_a
+	import _v_m_t_x
+	import asciiTable
+	import otBase
+	import otConverters
+	import otData
+	import otTables
+	import ttProgram
