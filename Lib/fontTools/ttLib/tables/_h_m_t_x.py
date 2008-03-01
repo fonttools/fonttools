212ea147062ac1ba9c914836b25bb4a8a8463522
jvr <jvr@4cde692c-a291-49d1-8350-778aa11640f8>
2008-03-01 15:31:01 +0000

    fixed oversight in sys.byteorder transition
    git-svn-id: svn://svn.code.sf.net/p/fonttools/code/trunk@528 4cde692c-a291-49d1-8350-778aa11640f8

--- Lib/fontTools/ttLib/tables/_h_m_t_x.py
+++ Lib/fontTools/ttLib/tables/_h_m_t_x.py
@@ -37,7 +37,6 @@ class table__h_m_t_x(DefaultTable.DefaultTable):
 			metrics = Numeric.concatenate((metrics, 
 					Numeric.transpose(additionalMetrics)))
 		if data:
-			import sys
 			sys.stderr.write("too much data for hmtx/vmtx table\n")
 		metrics = metrics.tolist()
 		self.metrics = {}
