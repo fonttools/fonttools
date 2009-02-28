724ca7cf4f4bdd0bdade87022361f322f854f5d3
Erik van Blokland <erik@letterror.com>
2009-02-28 17:45:26 +0000

    Added the BSD license as it is on robofab.com. Upped the version to 1.2 "release".
    git-svn-id: http://svn.robofab.com/trunk@172 b5fa9d6c-a76f-4ffd-b3cb-f825fc41095c

--- Lib/robofab/__init__.py
+++ Lib/robofab/__init__.py
@@ -15,30 +15,24 @@ MORE INFO
 The RoboFab homepage, documentation etc.
 http://robofab.com
 
+SVN REPOSITORY
+http://code.robofab.com
+
 LICENSE
-Some restrictions apply.
-http://robofab.com/download/index.html
+Copyright (c) 2003-2009, The RoboFab Developers, Just van Rossum, Tal Leming, Erik van Blokland
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the RoboFab Developers nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 HISTORY
 RoboFab starts somewhere during the 
 TypoTechnica in Heidelberg, 2003.
-
-COPYRIGHT
-The package is copyrighted by the RoboFab
-developers and can only be used with the 
-express permission of the developers.
-
-DISCLAIMER
-This code is under construction. Make backup
-copies of your work before using code from 
-RoboFab. The RoboFab developers won't accept
-any responsibility for loss of data, damaged
-work, or problems of any kind caused by
-operating RoboFab or the inability to use
-parts of or the entire module. Etcetera.
-RoboFab is not time sensitive. Don't use RoboFab
-in Nuclear powerstations or airtraffic control
-software.
 
 DEPENDENCIES
 RoboFab expects fontTools to be installed.
@@ -75,5 +69,5 @@ class RoboFabError(Exception): pass
 class RoboFabWarning(Warning): pass
 
 
-numberVersion = (1, 2, "develop", 0)
-version = "1.2.0d"
+numberVersion = (1, 2, "release", 0)
+version = "1.2.0"
