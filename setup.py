406cb2f7ba2c4998033372402a1842849b41d623
pabs3 <pabs3@4cde692c-a291-49d1-8350-778aa11640f8>
2009-11-08 15:54:25 +0000

    Install the manual page to the correct location.
    Shame on MacPorts and Gentoo for not forwarding this change.
    
    git-svn-id: svn://svn.code.sf.net/p/fonttools/code/trunk@576 4cde692c-a291-49d1-8350-778aa11640f8

--- setup.py
+++ setup.py
@@ -99,6 +99,6 @@ setup(
 		],
 		scripts = ["Tools/ttx"],
 		cmdclass = {"build_ext": build_ext_optional},
-		data_files = [('man/man1', ["Doc/ttx.1"])],
+		data_files = [('share/man/man1', ["Doc/ttx.1"])],
 		**classifiers
 	)
