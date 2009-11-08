e7689a4856936c382cb053e9999b304bae985af7
pabs3 <pabs3@4cde692c-a291-49d1-8350-778aa11640f8>
2009-11-08 15:58:24 +0000

    Release fonttools version 2.3
    git-svn-id: svn://svn.code.sf.net/p/fonttools/code/trunk@579 4cde692c-a291-49d1-8350-778aa11640f8

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ TrueType/OpenType fonts to and from an XML-based format.
 
 setup(
 		name = "fonttools",
-		version = "2.2",
+		version = "2.3",
 		description = "Tools to manipulate font files",
 		author = "Just van Rossum",
 		author_email = "just@letterror.com",
