8def59e71ae0f7053433074b44f1ee6187599a8f
Tal Leming <tal@typesupply.com>
2011-10-28 16:54:11 +0000

    More verbose method name.
    git-svn-id: http://svn.robofab.com/branches/ufo3k@482 b5fa9d6c-a76f-4ffd-b3cb-f825fc41095c

--- Lib/ufoLib/__init__.py
+++ Lib/ufoLib/__init__.py
@@ -210,16 +210,16 @@ class UFOReader(object):
 		f = codecs.open(path, READ_MODE, encoding=encoding)
 		return f
 
-	def getFileModTime(self, path):
+	def getFileModificationTime(self, path):
 		"""
-		Returns the modification time (as reported by os.stat st_mtime)
+		Returns the modification time (as reported by os.path.getmtime)
 		for the file at the given path. The path must be relative to
 		the UFO path. Returns None if the file does not exist.
 		"""
 		fullPath = os.path.join(self._path, path)
 		if not self._checkForFile(fullPath):
 			return None
-		return os.stat(fullPath).st_mtime
+		return os.path.getmtime(fullPath)
 
 	# metainfo.plist
 
