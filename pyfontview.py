75858f653ac9122cdb02375597bdeb8e0a9ed857
Behdad Esfahbod <behdad@behdad.org>
2013-08-19 13:01:45 -0400

    Minor

--- pyfontview.py
+++ pyfontview.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+# Copyright 2013 Google, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0(the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Google Author(s): Behdad Esfahbod
+
+"""Python Font Viewer.
+"""
+
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -218,7 +237,7 @@ class FontTreeModel(gtk.GenericTreeModel):
 
 class PyFontView:
 
-	def delete_event(self, widget, event, data=None):
+	def _delete_event(self, widget, event, data=None):
 		gtk.main_quit()
 		return False
 
@@ -226,7 +245,7 @@ class PyFontView:
 
 		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.set_title("%s - PyFontView" % fontfile)
-		self.window.connect("delete_event", self.delete_event)
+		self.window.connect("delete_event", self._delete_event)
 		self.window.set_size_request(400, 600)
 
 		self.scrolled_window = gtk.ScrolledWindow()
