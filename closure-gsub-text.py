9a72de4b8936ca8efe6393dfca945d8425f20375
Behdad Esfahbod <behdad@behdad.org>
2013-08-20 11:43:34 -0400

    Minor

--- closure-gsub-text.py
+++ closure-gsub-text.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 
+# Copyright 2013 Google, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0(the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Google Author(s): Behdad Esfahbod
+
 import sys, pyotlss
 
 args = sys.argv[1:]
@@ -10,7 +26,7 @@ args = options.parse_opts (args)
 subsetter = pyotlss.Subsetter (options=options)
 subsetter.populate (text = args[1])
 
-font = pyotlss.load_font (args[0], dont_load_glyph_names=not options.glyph_names)
+font = pyotlss.load_font (args[0], options, dontLoadGlyphNames=not options.glyph_names)
 
 font['cmap'].closure_glyphs (subsetter)
 font['GSUB'].closure_glyphs (subsetter)
