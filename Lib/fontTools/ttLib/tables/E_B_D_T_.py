fonttools/fonttools

commit 3d8d5cd10d2d30ba9997b7cc43336e504111be9a

Port ttLib to respect font.lazy
I *hope* I didn't break anything...

Notably, cffLib is not ported yet.

Lib/fontTools/ttLib/tables/E_B_D_T_.py
@@ -383,15 +383,18 @@ class BitmapGlyph:
 	def __init__(self, data, ttFont):
 		self.data = data
 		self.ttFont = ttFont
+		if not ttFont.lazy:
+			self.decompile()
+			del self.data
 
 	def __getattr__(self, attr):
 		# Allow lazy decompile.
 		if attr[:2] == '__':
 			raise AttributeError, attr
-		if self.data == None:
+		if not hasattr(self, "data"):
 			raise AttributeError, attr
 		self.decompile()
-		self.data = None
+		del self.data
 		return getattr(self, attr)
 
 	# Not a fan of this but it is needed for safer safety checking.

Lib/fontTools/ttLib/tables/E_B_L_C_.py
@@ -325,16 +325,18 @@ class EblcIndexSubTable:
 	def __init__(self, data, ttFont):
 		self.data = data
 		self.ttFont = ttFont
+		if not ttFont.lazy:
+			self.decompile()
+			del self.data, self.ttFont
 
 	def __getattr__(self, attr):
 		# Allow lazy decompile.
 		if attr[:2] == '__':
 			raise AttributeError, attr
-		if self.data == None:
+		if not hasattr(self, "data"):
 			raise AttributeError, attr
 		self.decompile()
-		self.data = None
-		self.ttFont = None
+		del self.data, self.ttFont
 		return getattr(self, attr)
 
 	# This method just takes care of the indexSubHeader. Implementing subclasses

Lib/fontTools/ttLib/tables/_c_m_a_p.py
@@ -1108,7 +1108,7 @@ class cmap_format_14(CmapSubtable):
 		self.language = 0xFF # has no language.
 
 	def decompile(self, data, ttFont):
-		if data != None and ttFont != None:
+		if ttFont.lazy and data != None and ttFont != None:
 			self.decompileHeader(data, ttFont)
 		else:
 			assert (data == None and ttFont == None), "Need both data and ttFont arguments"

Lib/fontTools/ttLib/tables/_g_l_y_f.py
@@ -51,6 +51,9 @@ class table__g_l_y_f(DefaultTable.DefaultTable):
 			warnings.warn("too much 'glyf' table data")
 		if noname:
 			warnings.warn('%s glyphs have no name' % i)
+		if not ttFont.lazy:
+			for glyph in self.glyphs.values():
+				glyph.expand(self)
 	
 	def compile(self, ttFont):
 		if not hasattr(self, "glyphOrder"):

commit 4be8db031d5df7a3bdd1e4e0fa02de0246cb20ae

Fix up EBLC/EBDT non-lazy decompile
Fallout from 3d8d5cd10d2d30ba9997b7cc43336e504111be9a.

Lib/fontTools/ttLib/tables/E_B_D_T_.py
@@ -382,9 +382,10 @@ class BitmapGlyph:
 	def __init__(self, data, ttFont):
 		self.data = data
 		self.ttFont = ttFont
-		if not ttFont.lazy:
-			self.decompile()
-			del self.data
+		# TODO Currently non-lazy decompilation is untested here...
+		#if not ttFont.lazy:
+		#	self.decompile()
+		#	del self.data
 
 	def __getattr__(self, attr):
 		# Allow lazy decompile.

Lib/fontTools/ttLib/tables/E_B_L_C_.py
@@ -324,9 +324,10 @@ class EblcIndexSubTable:
 	def __init__(self, data, ttFont):
 		self.data = data
 		self.ttFont = ttFont
-		if not ttFont.lazy:
-			self.decompile()
-			del self.data, self.ttFont
+		# TODO Currently non-lazy decompiling doesn't work for this class...
+		#if not ttFont.lazy:
+		#	self.decompile()
+		#	del self.data, self.ttFont
 
 	def __getattr__(self, attr):
 		# Allow lazy decompile.