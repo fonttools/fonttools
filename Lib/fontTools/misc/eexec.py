1acf4d66daa1bb85008c50be23bd2d74a74867dc
Behdad Esfahbod <behdad@behdad.org>
2013-12-07 17:36:12 -0500

    Remove eexecOp C extension
    It hasn't been working:
    
      https://github.com/khaledhosny/fonttools/commit/6102ba7500171966c436aac0eb61185c26c5fcdd
    
    And it's only used to load Type1 fonts, so who cares if the Python
    implementation is slow...

--- Lib/fontTools/misc/eexec.py
+++ Lib/fontTools/misc/eexec.py
@@ -5,13 +5,6 @@ charstring encryption algorithm as used by PostScript Type 1 fonts.
 from __future__ import print_function, division
 from fontTools.misc.py23 import *
 
-# Warning: Although a Python implementation is provided here, 
-# all four public functions get overridden by the *much* faster 
-# C extension module eexecOp, if available.
-
-error = "eexec.error"
-
-
 def _decryptChar(cipher, R):
 	cipher = byteord(cipher)
 	plain = ( (cipher ^ (R>>8)) ) & 0xFF
@@ -26,8 +19,6 @@ def _encryptChar(plain, R):
 
 
 def decrypt(cipherstring, R):
-	# I could probably speed this up by inlining _decryptChar,
-	# but... we've got eexecOp, so who cares ;-)
 	plainList = []
 	for cipher in cipherstring:
 		plain, R = _decryptChar(cipher, R)
@@ -55,20 +46,10 @@ def deHexString(h):
 
 
 def _test():
-	import fontTools.misc.eexecOp
 	testStr = "\0\0asdadads asds\265"
 	print(decrypt, decrypt(testStr, 12321))
-	print(eexecOp.decrypt, eexecOp.decrypt(testStr, 12321))
 	print(encrypt, encrypt(testStr, 12321))
-	print(eexecOp.encrypt, eexecOp.encrypt(testStr, 12321))
 
 
 if __name__ == "__main__":
 	_test()
-
-
-try:
-	from fontTools.misc.eexecOp import *
-except ImportError:
-	pass # Use the slow Python versions
-
