3a86b2a95984795a1ffbcbfe8607696663596f91
Patrick Lam <prof.lam@gmail.com>
2014-06-06 11:45:44 -0400

    Cleanup imports.

--- Lib/fontTools/analysis.py
+++ Lib/fontTools/analysis.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division, absolute_import
 from fontTools.ttLib import TTFont
-from ttLib.instructions import *
+import fontTools.ttLib.instructions
 import sys
 
 
