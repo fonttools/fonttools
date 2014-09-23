2f94365340daf0d0d5de7af98e5b92f2536a9173
Jens Kutilek <webmail@kutilek.de>
2014-09-23 18:16:01 +0200

    Rename self.bitmapSets to self.strikes etc.

--- Lib/fontTools/ttLib/tables/_s_b_i_x.py
+++ Lib/fontTools/ttLib/tables/_s_b_i_x.py
@@ -72,46 +72,46 @@ class table__s_b_i_x(DefaultTable.DefaultTable):
 		self.version = 1
 		self.flags = 1
 		self.numStrikes = 0
-		self.bitmapSets = {}
-		self.bitmapSetOffsets = []
+		self.strikes = {}
+		self.strikeOffsets = []
 
 	def decompile(self, data, ttFont):
 		# read table header
 		sstruct.unpack(sbixHeaderFormat, data[ : sbixHeaderFormatSize], self)
-		# collect offsets to individual bitmap sets in self.bitmapSetOffsets
+		# collect offsets to individual strikes in self.strikeOffsets
 		for i in range(self.numStrikes):
 			myOffset = sbixHeaderFormatSize + i * sbixStrikeOffsetFormatSize
 			offsetEntry = sbixBitmapSetOffset()
 			sstruct.unpack(sbixStrikeOffsetFormat, \
 				data[myOffset : myOffset+sbixStrikeOffsetFormatSize], \
 				offsetEntry)
-			self.bitmapSetOffsets.append(offsetEntry.strikeOffset)
+			self.strikeOffsets.append(offsetEntry.strikeOffset)
 
 		# decompile Strikes
 		for i in range(self.numStrikes-1, -1, -1):
-			myBitmapSet = Strike(rawdata=data[self.bitmapSetOffsets[i]:])
-			data = data[:self.bitmapSetOffsets[i]]
+			myBitmapSet = Strike(rawdata=data[self.strikeOffsets[i]:])
+			data = data[:self.strikeOffsets[i]]
 			myBitmapSet.decompile(ttFont)
 			#print "  Strike length: %xh" % len(bitmapSetData)
 			#print "Number of Bitmaps:", myBitmapSet.numBitmaps
-			if myBitmapSet.ppem in self.bitmapSets:
+			if myBitmapSet.ppem in self.strikes:
 				from fontTools import ttLib
 				raise ttLib.TTLibError("Pixel 'ppem' must be unique for each Strike")
-			self.bitmapSets[myBitmapSet.ppem] = myBitmapSet
+			self.strikes[myBitmapSet.ppem] = myBitmapSet
 
 		# after the bitmaps have been extracted, we don't need the offsets anymore
-		del self.bitmapSetOffsets
+		del self.strikeOffsets
 
 	def compile(self, ttFont):
 		sbixData = ""
-		self.numStrikes = len(self.bitmapSets)
+		self.numStrikes = len(self.strikes)
 		sbixHeader = sstruct.pack(sbixHeaderFormat, self)
 
 		# calculate offset to start of first bitmap set
 		setOffset = sbixHeaderFormatSize + sbixStrikeOffsetFormatSize * self.numStrikes
 
-		for si in sorted(self.bitmapSets.keys()):
-			myBitmapSet = self.bitmapSets[si]
+		for si in sorted(self.strikes.keys()):
+			myBitmapSet = self.strikes[si]
 			myBitmapSet.compile(ttFont)
 			# append offset to this bitmap set to table header
 			myBitmapSet.strikeOffset = setOffset
@@ -126,8 +126,8 @@ class table__s_b_i_x(DefaultTable.DefaultTable):
 		xmlWriter.newline()
 		xmlWriter.simpletag("flags", value=self.flags)
 		xmlWriter.newline()
-		for i in sorted(self.bitmapSets.keys()):
-			self.bitmapSets[i].toXML(xmlWriter, ttFont)
+		for i in sorted(self.strikes.keys()):
+			self.strikes[i].toXML(xmlWriter, ttFont)
 
 	def fromXML(self, name, attrs, content, ttFont):
 		if name in ["version", "flags"]:
@@ -138,7 +138,7 @@ class table__s_b_i_x(DefaultTable.DefaultTable):
 				if isinstance(element, tuple):
 					name, attrs, content = element
 					myBitmapSet.fromXML(name, attrs, content, ttFont)
-			self.bitmapSets[myBitmapSet.ppem] = myBitmapSet
+			self.strikes[myBitmapSet.ppem] = myBitmapSet
 		else:
 			from fontTools import ttLib
 			raise ttLib.TTLibError("can't handle '%s' element" % name)
