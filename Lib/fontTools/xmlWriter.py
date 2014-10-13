f8c35b6f98717a3f1f799b722e3bcd5b9303735c
Behdad Esfahbod <behdad@behdad.org>
2014-10-13 10:55:58 -0700

    Add back fontTools.xmlWriter
    Fixes https://github.com/behdad/fonttools/issues/190

---
+++ Lib/fontTools/xmlWriter.py
@@ -0,0 +1,6 @@
+"""xmlWriter.py -- Added back here for backward compatibility"""
+
+from __future__ import print_function, division, absolute_import
+from fontTools.misc.py23 import *
+
+from fontTools.misc.xmlWriter import *
