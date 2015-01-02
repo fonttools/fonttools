b6cb1beabbcb6b84e9bf72d5868d46e7fabf2247
Behdad Esfahbod <behdad@behdad.org>
2015-01-02 13:08:57 -0800

    Ignore top four bytes of head table timestamps
    Warn instead.

--- Lib/fontTools/misc/timeTools.py
+++ Lib/fontTools/misc/timeTools.py
@@ -12,12 +12,7 @@ import calendar
 epoch_diff = calendar.timegm((1904, 1, 1, 0, 0, 0, 0, 0, 0))
 
 def timestampToString(value):
-	# https://github.com/behdad/fonttools/issues/99#issuecomment-66776810
-	try:
-		value = time.asctime(time.gmtime(max(0, value + epoch_diff)))
-	except (OverflowError, ValueError):
-		value = time.asctime(time.gmtime(0))
-	return value
+	return time.asctime(time.gmtime(max(0, value + epoch_diff)))
 
 def timestampFromString(value):
 	return calendar.timegm(time.strptime(value)) - epoch_diff
