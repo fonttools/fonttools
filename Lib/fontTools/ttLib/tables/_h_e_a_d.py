b056318ca9ddc17855fe1c8191d61ff38f0f4f7e
Behdad Esfahbod <behdad@behdad.org>
2015-01-02 13:08:57 -0800

    Ignore top four bytes of head table timestamps
    Warn instead.

--- Lib/fontTools/ttLib/tables/_h_e_a_d.py
+++ Lib/fontTools/ttLib/tables/_h_e_a_d.py
@@ -4,6 +4,7 @@ from fontTools.misc import sstruct
 from fontTools.misc.textTools import safeEval, num2binary, binary2num
 from fontTools.misc.timeTools import timestampFromString, timestampToString, timestampNow
 from . import DefaultTable
+import warnings
 
 
 headFormat = """
@@ -35,7 +36,20 @@ class table__h_e_a_d(DefaultTable.DefaultTable):
 		dummy, rest = sstruct.unpack2(headFormat, data, self)
 		if rest:
 			# this is quite illegal, but there seem to be fonts out there that do this
+			warnings.warn("extra bytes at the end of 'head' table")
 			assert rest == "\0\0"
+
+		# For timestamp fields, ignore the top four bytes.  Some fonts have
+		# bogus values there.  Since till 2038 those bytes only can be zero,
+		# ignore them.
+		#
+		# https://github.com/behdad/fonttools/issues/99#issuecomment-66776810
+		for stamp in 'created', 'modified':
+			value = getattr(self, stamp)
+			if value > 0xFFFFFFFF:
+				warnings.warn("'%s' timestamp out of range; ignoring top bytes" % stamp)
+				value &= 0xFFFFFFFF
+				setattr(self, stamp, value)
 	
 	def compile(self, ttFont):
 		if ttFont.recalcTimestamp:
