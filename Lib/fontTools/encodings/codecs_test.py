6a823290e1c73ff1b340e77faf25c23d32d0dbf5
Behdad Esfahbod <behdad@behdad.org>
2015-04-16 14:12:59 -0700

    Really fix python 2.6 build this time

--- Lib/fontTools/encodings/codecs_test.py
+++ Lib/fontTools/encodings/codecs_test.py
@@ -6,7 +6,7 @@ import fontTools.encodings.codecs # Not to be confused with "import codecs"
 class ExtendedCodecsTest(unittest.TestCase):
 
 	def test_decode(self):
-		self.assertEqual(b'x\xfe\xfdy'.decode(encoding="x-mac-japanese-ttx"),
+		self.assertEqual(b'x\xfe\xfdy'.decode("x-mac-japanese-ttx"),
 				 unichr(0x78)+unichr(0x2122)+unichr(0x00A9)+unichr(0x79))
 
 	def test_encode(self):
