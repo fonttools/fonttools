6e01a6e3509cb5929b78e89041648ff43c509f6a
Behdad Esfahbod <behdad@behdad.org>
2015-04-16 18:24:07 -0700

    Add Roman Croatian and Romanian encodings
    Concludes https://github.com/behdad/fonttools/issues/236

--- Lib/fontTools/encodings/codecs_test.py
+++ Lib/fontTools/encodings/codecs_test.py
@@ -5,13 +5,17 @@ import fontTools.encodings.codecs # Not to be confused with "import codecs"
 
 class ExtendedCodecsTest(unittest.TestCase):
 
-	def test_decode(self):
+	def test_decode_japanese(self):
 		self.assertEqual(b'x\xfe\xfdy'.decode("x-mac-japanese-ttx"),
 				 unichr(0x78)+unichr(0x2122)+unichr(0x00A9)+unichr(0x79))
 
-	def test_encode(self):
+	def test_encode_japanese(self):
 		self.assertEqual(b'x\xfe\xfdy',
 				 (unichr(0x78)+unichr(0x2122)+unichr(0x00A9)+unichr(0x79)).encode("x-mac-japanese-ttx"))
+
+	def test_decode_romanian(self):
+		self.assertEqual(b'x\xfb'.decode("x-mac-romanian-ttx"),
+				 unichr(0x78)+unichr(0x02DA))
 
 if __name__ == '__main__':
 	unittest.main()
