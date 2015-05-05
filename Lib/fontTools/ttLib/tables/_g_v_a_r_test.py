6d5049cb57a11bda11aefb5560df5ad3c387158c
Sascha Brawer <sascha@brawer.ch>
2015-05-05 16:34:03 +0200

    Minor changes towards supporting python3.

--- Lib/fontTools/ttLib/tables/_g_v_a_r_test.py
+++ Lib/fontTools/ttLib/tables/_g_v_a_r_test.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 from fontTools.misc.py23 import *
+from fontTools.misc.textTools import deHexStr
 from fontTools.misc.xmlWriter import XMLWriter
 from fontTools.ttLib import TTLibError
 from fontTools.ttLib.tables._g_l_y_f import GlyphCoordinates
@@ -7,16 +8,13 @@ from fontTools.ttLib.tables._g_v_a_r import table__g_v_a_r, GlyphVariation
 import random
 import unittest
 
-def hexdecode(s):
-	return bytesjoin([c.decode("hex") for c in s.split()])
-
 def hexencode(s):
 	return ' '.join([c.encode("hex").upper() for c in s])
 
 # Glyph variation table of uppercase I in the Skia font, as printed in Apple's
 # TrueType spec. The actual Skia font uses a different table for uppercase I.
 # https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6gvar.html
-SKIA_GVAR_I = hexdecode(
+SKIA_GVAR_I = deHexStr(
 	"00 08 00 24 00 33 20 00 00 15 20 01 00 1B 20 02 "
 	"00 24 20 03 00 15 20 04 00 26 20 07 00 0D 20 06 "
 	"00 1A 20 05 00 40 01 01 01 81 80 43 FF 7E FF 7E "
@@ -36,29 +34,29 @@ SKIA_GVAR_I = hexdecode(
 	"FF FF FF 81 00 08 81 82 02 EE EE EE 8B 6D 00")
 
 # Shared coordinates in the Skia font, as printed in Apple's TrueType spec.
-SKIA_SHARED_COORDS = hexdecode(
+SKIA_SHARED_COORDS = deHexStr(
 	"40 00 00 00 C0 00 00 00 00 00 40 00 00 00 C0 00 "
 	"C0 00 C0 00 40 00 C0 00 40 00 40 00 C0 00 40 00")
 
 
 class GlyphVariationTableTest(unittest.TestCase):
 	def test_compileOffsets_shortFormat(self):
-		self.assertEqual((hexdecode("00 00 00 02 FF C0"), 0),
+		self.assertEqual((deHexStr("00 00 00 02 FF C0"), 0),
 				 table__g_v_a_r.compileOffsets_([0, 4, 0x1ff80]))
 
 	def test_compileOffsets_longFormat(self):
-		self.assertEqual((hexdecode("00 00 00 00 00 00 00 04 CA FE BE EF"), 1),
+		self.assertEqual((deHexStr("00 00 00 00 00 00 00 04 CA FE BE EF"), 1),
 				 table__g_v_a_r.compileOffsets_([0, 4, 0xCAFEBEEF]))
 
 	def test_decompileOffsets_shortFormat(self):
 		decompileOffsets = table__g_v_a_r.decompileOffsets_
-		data = hexdecode("00 11 22 33 44 55 66 77 88 99 aa bb")
+		data = deHexStr("00 11 22 33 44 55 66 77 88 99 aa bb")
 		self.assertEqual([2*0x0011, 2*0x2233, 2*0x4455, 2*0x6677, 2*0x8899, 2*0xaabb],
 				 list(decompileOffsets(data, format=0, glyphCount=5)))
 
 	def test_decompileOffsets_longFormat(self):
 		decompileOffsets = table__g_v_a_r.decompileOffsets_
-		data = hexdecode("00 11 22 33 44 55 66 77 88 99 aa bb")
+		data = deHexStr("00 11 22 33 44 55 66 77 88 99 aa bb")
 		self.assertEqual([0x00112233, 0x44556677, 0x8899aabb],
 				 list(decompileOffsets(data, format=1, glyphCount=2)))
 
@@ -335,7 +333,7 @@ class GlyphVariationTest(unittest.TestCase):
 
 	def test_decompileCoord(self):
 		decompileCoord = GlyphVariation.decompileCoord_
-		data = hexdecode("DE AD C0 00 20 00 DE AD")
+		data = deHexStr("DE AD C0 00 20 00 DE AD")
 		self.assertEqual(({"wght": -1.0, "wdth": 0.5}, 6), decompileCoord(["wght", "wdth"], data, 2))
 
 	def test_decompileCoords(self):
@@ -346,7 +344,7 @@ class GlyphVariationTest(unittest.TestCase):
 			{"wght": -1.0, "wdth": 0.0, "opsz": 0.25},
 			{"wght":  0.0, "wdth": -1.0, "opsz": 1.0}
 		]
-		data = hexdecode("DE AD 40 00 00 00 20 00 C0 00 00 00 10 00 00 00 C0 00 40 00")
+		data = deHexStr("DE AD 40 00 00 00 20 00 C0 00 00 00 10 00 00 00 C0 00 40 00")
 		self.assertEqual((coords, 20), decompileCoords(axes, numCoords=3, data=data, offset=2))
 
 	def test_compilePoints(self):
@@ -368,30 +366,30 @@ class GlyphVariationTest(unittest.TestCase):
 		numPoints = 65536
 		allPoints = range(numPoints)
 		# all points in glyph
-		self.assertEqual((allPoints, 1), decompilePoints(numPoints, hexdecode("00"), 0))
+		self.assertEqual((allPoints, 1), decompilePoints(numPoints, deHexStr("00"), 0))
 		# all points in glyph (in overly verbose encoding, not explicitly prohibited by spec)
-		self.assertEqual((allPoints, 2), decompilePoints(numPoints, hexdecode("80 00"), 0))
+		self.assertEqual((allPoints, 2), decompilePoints(numPoints, deHexStr("80 00"), 0))
 		# 2 points; first run: [9, 9+6]
-		self.assertEqual(([9, 15], 4), decompilePoints(numPoints, hexdecode("02 01 09 06"), 0))
+		self.assertEqual(([9, 15], 4), decompilePoints(numPoints, deHexStr("02 01 09 06"), 0))
 		# 2 points; first run: [0xBEEF, 0xCAFE]. (0x0C0F = 0xCAFE - 0xBEEF)
-		self.assertEqual(([0xBEEF, 0xCAFE], 6), decompilePoints(numPoints, hexdecode("02 81 BE EF 0C 0F"), 0))
+		self.assertEqual(([0xBEEF, 0xCAFE], 6), decompilePoints(numPoints, deHexStr("02 81 BE EF 0C 0F"), 0))
 		# 1 point; first run: [7]
-		self.assertEqual(([7], 3), decompilePoints(numPoints, hexdecode("01 00 07"), 0))
+		self.assertEqual(([7], 3), decompilePoints(numPoints, deHexStr("01 00 07"), 0))
 		# 1 point; first run: [7] in overly verbose encoding
-		self.assertEqual(([7], 4), decompilePoints(numPoints, hexdecode("01 80 00 07"), 0))
+		self.assertEqual(([7], 4), decompilePoints(numPoints, deHexStr("01 80 00 07"), 0))
 		# 1 point; first run: [65535]; requires words to be treated as unsigned numbers
-		self.assertEqual(([65535], 4), decompilePoints(numPoints, hexdecode("01 80 FF FF"), 0))
+		self.assertEqual(([65535], 4), decompilePoints(numPoints, deHexStr("01 80 FF FF"), 0))
 		# 4 points; first run: [7, 8]; second run: [255, 257]. 257 is stored in delta-encoded bytes (0xFF + 2).
-		self.assertEqual(([7, 8, 255, 257], 7), decompilePoints(numPoints, hexdecode("04 01 07 01 01 FF 02"), 0))
+		self.assertEqual(([7, 8, 255, 257], 7), decompilePoints(numPoints, deHexStr("04 01 07 01 01 FF 02"), 0))
 		# combination of all encodings, preceded and followed by 4 bytes of unused data
-		data = hexdecode("DE AD DE AD 04 01 07 01 81 BE EF 0C 0F DE AD DE AD")
+		data = deHexStr("DE AD DE AD 04 01 07 01 81 BE EF 0C 0F DE AD DE AD")
 		self.assertEqual(([7, 8, 0xBEEF, 0xCAFE], 13), decompilePoints(numPoints, data, 4))
 
 	def test_decompilePoints_shouldGuardAgainstBadPointNumbers(self):
 		decompilePoints = GlyphVariation.decompilePoints_
 		# 2 points; first run: [3, 9].
 		numPoints = 8
-		self.assertRaises(TTLibError, decompilePoints, numPoints, hexdecode("02 01 03 06"), 0)
+		self.assertRaises(TTLibError, decompilePoints, numPoints, deHexStr("02 01 03 06"), 0)
 
 	def test_decompilePoints_roundTrip(self):
 		numPointsInGlyph = 500  # greater than 255, so we also exercise code path for 16-bit encoding
@@ -452,13 +450,13 @@ class GlyphVariationTest(unittest.TestCase):
 	def test_decompileDeltas(self):
 		decompileDeltas = GlyphVariation.decompileDeltas_
 		# 83 = zero values (0x80), count = 4 (1 + 0x83 & 0x3F)
-		self.assertEqual(([0, 0, 0, 0], 1), decompileDeltas(4, hexdecode("83"), 0))
+		self.assertEqual(([0, 0, 0, 0], 1), decompileDeltas(4, deHexStr("83"), 0))
 		# 41 01 02 FF FF = signed 16-bit values (0x40), count = 2 (1 + 0x41 & 0x3F)
-		self.assertEqual(([258, -1], 5), decompileDeltas(2, hexdecode("41 01 02 FF FF"), 0))
+		self.assertEqual(([258, -1], 5), decompileDeltas(2, deHexStr("41 01 02 FF FF"), 0))
 		# 01 81 07 = signed 8-bit values, count = 2 (1 + 0x01 & 0x3F)
-		self.assertEqual(([-127, 7], 3), decompileDeltas(2, hexdecode("01 81 07"), 0))
+		self.assertEqual(([-127, 7], 3), decompileDeltas(2, deHexStr("01 81 07"), 0))
 		# combination of all three encodings, preceded and followed by 4 bytes of unused data
-		data = hexdecode("DE AD BE EF 83 40 01 02 01 81 80 DE AD BE EF")
+		data = deHexStr("DE AD BE EF 83 40 01 02 01 81 80 DE AD BE EF")
 		self.assertEqual(([0, 0, 0, 0, 258, -127, -128], 11), decompileDeltas(7, data, 4))
 
 	def test_decompileDeltas_roundTrip(self):
