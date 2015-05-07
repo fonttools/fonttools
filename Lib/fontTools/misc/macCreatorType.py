44eb1e85b52bb143e4c29923af4bb53bd954d679
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-05-07 19:07:22 +0100

    [macCreatorType] disable workaround for setting function for all python versions

--- Lib/fontTools/misc/macCreatorType.py
+++ Lib/fontTools/misc/macCreatorType.py
@@ -29,10 +29,4 @@ def getMacCreatorAndType(path):
 
 def setMacCreatorAndType(path, fileCreator, fileType):
 	if MacOS is not None:
-		if sys.version_info[:2] < (2, 7) and sys.byteorder == "little":
-			# work around bug in MacOS.SetCreatorAndType() on intel:
-			# http://bugs.python.org/issue1594
-			# (fixed with Python 2.7)
-			fileCreator = _reverseString(fileCreator)
-			fileType = _reverseString(fileType)
 		MacOS.SetCreatorAndType(path, fileCreator, fileType)
