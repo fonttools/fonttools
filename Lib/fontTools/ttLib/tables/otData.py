fonttools/fonttools

commit e06166b83aa82582dab2e011520b1a77f72aa9f9

Fix SingleSubstFormat1 modulo math

Lib/fontTools/ttLib/tables/otTables.py
@@ -140,7 +140,6 @@ class SingleSubst(FormatSwitchingBaseTable):
 		if self.Format == 1:
 			delta = rawTable["DeltaGlyphID"]
 			inputGIDS =  [ font.getGlyphID(name) for name in input ]
-			inputGIDS = map(doModulo, inputGIDS)
 			outGIDS = [ glyphID + delta for glyphID in inputGIDS ]
 			outGIDS = map(doModulo, outGIDS)
 			outNames = [ font.getGlyphName(glyphID) for glyphID in outGIDS ]
@@ -160,7 +159,7 @@ class SingleSubst(FormatSwitchingBaseTable):
 			mapping = self.mapping = {}
 		items = list(mapping.items())
 		getGlyphID = font.getGlyphID
-		gidItems = ((getGlyphID(a), getGlyphID(b)) for a,b in items)
+		gidItems = [(getGlyphID(a), getGlyphID(b)) for a,b in items]
 		sortableItems = sorted(zip(gidItems, items))
 
 		# figure out format
@@ -169,6 +168,10 @@ class SingleSubst(FormatSwitchingBaseTable):
 		for inID, outID in gidItems:
 			if delta is None:
 				delta = outID - inID
+				if delta < -32768:
+					delta += 65536
+				elif delta > 32767:
+					delta -= 65536
 			else:
 				if delta != outID - inID:
 					break

commit 6c83fe4846c711123c38654e5eb83d0a595d56ea

Really fix SingleSubst delta math
I had broken this in e06166b83aa82582dab2e011520b1a77f72aa9f9.

Fix that, and other issues.  Since I made that last commit, it has
become clear that SingleSubst works by adding delta and then doing
module 65536.

HarfBuzz was updated for this in 2011:
commit 52ebdff49d13f239efc886de935d47be9860f6e5
Author: Behdad Esfahbod <behdad@behdad.org>
Date:   Tue Sep 27 12:38:16 2011 -0400

    Fix GSUB lookuptype 1 subtype 1 delta wrapping

Lib/fontTools/ttLib/tables/otData.py
@@ -450,7 +450,7 @@ otData = [
 	('SingleSubstFormat1', [
 		('uint16', 'SubstFormat', None, None, 'Format identifier-format = 1'),
 		('Offset', 'Coverage', None, None, 'Offset to Coverage table-from beginning of Substitution table'),
-		('int16', 'DeltaGlyphID', None, None, 'Add to original GlyphID to get substitute GlyphID'),
+		('uint16', 'DeltaGlyphID', None, None, 'Add to original GlyphID modulo 65536 to get substitute GlyphID'),
 	]),
 
 	('SingleSubstFormat2', [

Lib/fontTools/ttLib/tables/otTables.py
@@ -134,11 +134,6 @@ class Coverage(FormatSwitchingBaseTable):
 		glyphs.append(attrs["value"])
 
 
-def doModulo(value):
-	if value < 0:
-		return value + 65536
-	return value
-
 class SingleSubst(FormatSwitchingBaseTable):
 
 	def postRead(self, rawTable, font):
@@ -148,8 +143,7 @@ class SingleSubst(FormatSwitchingBaseTable):
 		if self.Format == 1:
 			delta = rawTable["DeltaGlyphID"]
 			inputGIDS =  [ font.getGlyphID(name) for name in input ]
-			outGIDS = [ glyphID + delta for glyphID in inputGIDS ]
-			outGIDS = map(doModulo, outGIDS)
+			outGIDS = [ (glyphID + delta) % 65536 for glyphID in inputGIDS ]
 			outNames = [ font.getGlyphName(glyphID) for glyphID in outGIDS ]
 			list(map(operator.setitem, [mapping]*lenMapping, input, outNames))
 		elif self.Format == 2:
@@ -173,16 +167,11 @@ class SingleSubst(FormatSwitchingBaseTable):
 
 		# figure out format
 		format = 2
-		delta = None
 		for inID, outID in gidItems:
 			if delta is None:
-				delta = outID - inID
-				if delta < -32768:
-					delta += 65536
-				elif delta > 32767:
-					delta -= 65536
-			else:
-				if delta != outID - inID:
+				delta = (outID - inID) % 65536
+
+			if (inID + delta) % 65536 != outID:
 					break
 		else:
 			format = 1