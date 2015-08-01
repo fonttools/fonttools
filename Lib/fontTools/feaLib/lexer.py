c9ce9aa976c053037762710a4c6d2d977e8d31e5
Sascha Brawer <sascha@brawer.ch>
2015-08-01 10:24:44 +0200

    Make pretty error messages for LexerError

--- Lib/fontTools/feaLib/lexer.py
+++ Lib/fontTools/feaLib/lexer.py
@@ -9,6 +9,14 @@ class LexerError(Exception):
         Exception.__init__(self, message)
         self.location = location
 
+    def __str__(self):
+        message = Exception.__str__(self)
+        if self.location:
+            path, line, column = self.location
+            return "%s:%d:%d: %s" % (path, line, column, message)
+        else:
+            return message
+
 
 class Lexer(object):
     NUMBER = "NUMBER"
