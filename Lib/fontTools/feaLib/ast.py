4f27ce35857a4c79ef3c1b4f0610e6b09aa36348
Sascha Brawer <sascha@brawer.ch>
2015-09-04 16:11:53 +0200

    [feaLib] Ensure that "languagesystem DFLT dflt;" comes first
    According the to the OpenType Feature File specification,
    "languagesystem DFLT dflt;" must be the first languagesystem
    statement in a file.

--- Lib/fontTools/feaLib/ast.py
+++ Lib/fontTools/feaLib/ast.py
@@ -2,100 +2,118 @@ from __future__ import print_function, division, absolute_import
 from __future__ import unicode_literals
 
 
-class FeatureFile(object):
-    def __init__(self):
-        self.statements = []
+class Statement(object):
+    def __init__(self, location):
+        self.location = location
 
     def build(self, builder):
         pass
 
 
-class FeatureBlock(object):
-    def __init__(self, location, name, use_extension):
-        self.location = location
-        self.name, self.use_extension = name, use_extension
+class Block(Statement):
+    def __init__(self, location):
+        Statement.__init__(self, location)
         self.statements = []
 
+    def build(self, builder):
+        for s in self.statements:
+            s.build(builder)
 
-class LookupBlock(object):
+
+class FeatureFile(Block):
+    def __init__(self):
+        Block.__init__(self, location=None)
+
+
+class FeatureBlock(Block):
     def __init__(self, location, name, use_extension):
-        self.location = location
+        Block.__init__(self, location)
         self.name, self.use_extension = name, use_extension
-        self.statements = []
 
 
-class GlyphClassDefinition(object):
+class LookupBlock(Block):
+    def __init__(self, location, name, use_extension):
+        Block.__init__(self, location)
+        self.name, self.use_extension = name, use_extension
+
+
+class GlyphClassDefinition(Statement):
     def __init__(self, location, name, glyphs):
-        self.location = location
+        Statement.__init__(self, location)
         self.name = name
         self.glyphs = glyphs
 
 
-class AlternateSubstitution(object):
+class AlternateSubstitution(Statement):
     def __init__(self, location, glyph, from_class):
-        self.location = location
+        Statement.__init__(self, location)
         self.glyph, self.from_class = (glyph, from_class)
 
 
-class AnchorDefinition(object):
+class AnchorDefinition(Statement):
     def __init__(self, location, name, x, y, contourpoint):
-        self.location = location
+        Statement.__init__(self, location)
         self.name, self.x, self.y, self.contourpoint = name, x, y, contourpoint
 
 
-class LanguageStatement(object):
+class LanguageStatement(Statement):
     def __init__(self, location, language, include_default, required):
-        self.location = location
+        Statement.__init__(self, location)
         self.language = language
         self.include_default = include_default
         self.required = required
 
 
-class LanguageSystemStatement(object):
+class LanguageSystemStatement(Statement):
     def __init__(self, location, script, language):
-        self.location = location
+        Statement.__init__(self, location)
         self.script, self.language = (script, language)
 
+    def build(self, builder):
+        builder.add_language_system(self.location, self.script, self.language)
 
-class IgnoreSubstitutionRule(object):
+
+class IgnoreSubstitutionRule(Statement):
     def __init__(self, location, prefix, glyphs, suffix):
-        self.location = location
+        Statement.__init__(self, location)
         self.prefix, self.glyphs, self.suffix = (prefix, glyphs, suffix)
 
 
-class LookupReferenceStatement(object):
+class LookupReferenceStatement(Statement):
     def __init__(self, location, lookup):
+        Statement.__init__(self, location)
         self.location, self.lookup = (location, lookup)
 
 
-class ScriptStatement(object):
+class ScriptStatement(Statement):
     def __init__(self, location, script):
-        self.location = location
+        Statement.__init__(self, location)
         self.script = script
 
 
-class SubtableStatement(object):
+class SubtableStatement(Statement):
     def __init__(self, location):
-        self.location = location
+        Statement.__init__(self, location)
 
 
-class SubstitutionRule(object):
+class SubstitutionRule(Statement):
     def __init__(self, location, old, new):
-        self.location, self.old, self.new = (location, old, new)
+        Statement.__init__(self, location)
+        self.old, self.new = (old, new)
         self.old_prefix = []
         self.old_suffix = []
         self.lookups = [None] * len(old)
 
 
-class ValueRecord(object):
+class ValueRecord(Statement):
     def __init__(self, location, xPlacement, yPlacement, xAdvance, yAdvance):
-        self.location = location
+        Statement.__init__(self, location)
         self.xPlacement, self.yPlacement = (xPlacement, yPlacement)
         self.xAdvance, self.yAdvance = (xAdvance, yAdvance)
 
 
-class ValueRecordDefinition(object):
+class ValueRecordDefinition(Statement):
     def __init__(self, location, name, value):
-        self.location = location
+        Statement.__init__(self, location)
         self.name = name
         self.value = value
