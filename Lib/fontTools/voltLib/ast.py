34e7a8d904b332a91a615043a629725100367d6f
moyogo <moyogo@gmail.com>
2015-09-30 09:50:12 +0100

    [voltLib] Add ast.GlyphDefinition

---
+++ Lib/fontTools/voltLib/ast.py
@@ -0,0 +1,17 @@
+from __future__ import print_function, division, absolute_import
+from __future__ import unicode_literals
+import fontTools.feaLib.ast as ast
+
+
+class VoltFile(ast.Block):
+    def __init__(self):
+        ast.Block.__init__(self, location=None)
+
+class GlyphDefinition(ast.Statement):
+    def __init__(self, location, name, gid, gunicode, gtype, components):
+        ast.Statement.__init__(self,location)
+        self.name = name
+        self.id = gid
+        self.unicode = gunicode
+        self.type = gtype
+        self.components = components
