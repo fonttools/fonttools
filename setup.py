2c33e23bf3f06bc69d348aa7c0aea2c23b10c322
jamesgk <jamesgk19@gmail.com>
2015-10-05 15:58:58 -0700

    Make "python setup.py install" actually work.
    convert_curves is just a module (file), not a package (directory).

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(
     name='ConvertCurves',
     version='1.0',
-    packages=['convert_curves'],
+    py_modules=['convert_curves'],
     package_dir={'': 'Lib'}
 )
