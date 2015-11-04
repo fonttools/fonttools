1f6ea4548344e90b2c47ecc931652c97c242b61d
Denis Jacquerye <moyogo@gmail.com>
2015-11-04 19:32:05 +0000

    setup.py: fallback on distutils and use extra_kwargs for test_suite

--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 #! /usr/bin/env python
 
 import os, sys
-from distutils.core import Extension
 from setuptools import setup
-
-
-try:
-	# load py2exe distutils extension, if available
-	import py2exe
-except ImportError:
-	pass
 
 try:
 	import fontTools
@@ -18,26 +10,8 @@ except ImportError:
 	print("        http://sourceforge.net/projects/fonttools/")
 
 
-if sys.version_info > (2, 3, 0, 'alpha', 1):
-	# Trove classifiers for PyPI
-	classifiers = {"classifiers": [
-		"Development Status :: 4 - Beta",
-		"Environment :: Console",
-		"Environment :: Other Environment",
-		"Intended Audience :: Developers",
-		"Intended Audience :: End Users/Desktop",
-		"License :: OSI Approved :: BSD License",
-		"Natural Language :: English",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
-		"Topic :: Multimedia :: Graphics",
-		"Topic :: Multimedia :: Graphics :: Graphics Conversion",
-	]}
-else:
-	classifiers = {}
-
 long_description = """\
-RoboFab is a Python library with objects that deal with data usually associated with fonts and type design. RoboFab reads and writes UFO font files and works in any Python 2.3 (and 2.2.1), 2.4. 2.5, 2.6, 2.7. The RoboFab code for FontLab works in in FontLab 4.6, FontLab Studio 5.04, 5.1 and hopefully up.
+RoboFab is a Python library with objects that deal with data usually associated with fonts and type design.
 """
 
 setup(
@@ -66,7 +40,18 @@ setup(
 			"ufoLib",
 		],
 		package_dir = {'': 'Lib'},
-		test_suite="ufoLib.test",
-		#extra_path = 'FontTools',
-		**classifiers
+		test_suite = "ufoLib.test",
+		classifiers = [
+			"Development Status :: 4 - Beta",
+			"Environment :: Console",
+			"Environment :: Other Environment",
+			"Intended Audience :: Developers",
+			"Intended Audience :: End Users/Desktop",
+			"License :: OSI Approved :: BSD License",
+			"Natural Language :: English",
+			"Operating System :: OS Independent",
+			"Programming Language :: Python",
+			"Topic :: Multimedia :: Graphics",
+			"Topic :: Multimedia :: Graphics :: Graphics Conversion",
+		]
 	)
