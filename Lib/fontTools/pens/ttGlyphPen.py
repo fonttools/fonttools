5b3ca041e80bf94069b5d9a7de71b4b1cdb48afe
James Godfrey-Kittle <jamesgk@google.com>
2015-11-04 17:18:03 -0800

    Use BasePen instead of AbstractPen in TTGlyphPen

--- Lib/fontTools/pens/ttGlyphPen.py
+++ Lib/fontTools/pens/ttGlyphPen.py
@@ -1,8 +1,9 @@
 from __future__ import print_function, division, absolute_import
-from array import array
-
 from fontTools.misc.py23 import *
-from fontTools.pens.basePen import AbstractPen
+
+from array import array
+from fontTools.pens.basePen import BasePen
+from fontTools.pens.transformPen import TransformPen
 from fontTools.ttLib.tables import ttProgram
 from fontTools.ttLib.tables._g_l_y_f import Glyph
 from fontTools.ttLib.tables._g_l_y_f import GlyphComponent
@@ -12,10 +13,10 @@ from fontTools.ttLib.tables._g_l_y_f import GlyphCoordinates
 __all__ = ["TTGlyphPen"]
 
 
-class TTGlyphPen(AbstractPen):
+class TTGlyphPen(BasePen):
     """Pen used for drawing to a TrueType glyph."""
 
-    def __init__(self):
+    def __init__(self, glyphSet):
         self.points = []
         self.endPts = []
         self.types = []
@@ -25,23 +26,19 @@ class TTGlyphPen(AbstractPen):
         self.points.append([int(coord) for coord in pt])
         self.types.append(onCurve)
 
-    def lineTo(self, pt):
+    def _lineTo(self, pt):
         self._addPoint(pt, 1)
 
-    def moveTo(self, pt):
+    def _moveTo(self, pt):
         assert (not self.points) or (self.endPts[-1] == len(self.points) - 1), (
             '"move"-type point must begin a new contour.')
         self.lineTo(pt)
 
-    def qCurveTo(self, *points):
-        for pt in points[:-1]:
-            self._addPoint(pt, 0)
+    def _qCurveToOne(self, pt1, pt2):
+        self._addPoint(pt1, 0)
+        self._addPoint(pt2, 1)
 
-        # last point is None if there are no on-curve points
-        if points[-1]:
-            self._addPoint(points[-1], 1)
-
-    def closePath(self):
+    def _closePath(self):
         endPt = len(self.points) - 1
 
         # ignore anchors
@@ -52,29 +49,37 @@ class TTGlyphPen(AbstractPen):
 
         self.endPts.append(endPt)
 
-    def endPath(self):
+    def _endPath(self):
         # TrueType contours are always "closed"
         self.closePath()
 
     def addComponent(self, glyphName, transformation):
-        component = GlyphComponent()
-        component.glyphName = glyphName
-        component.transform = (transformation[:2], transformation[2:4])
-        component.x, component.y = [int(n) for n in transformation[4:]]
-        component.flags = 0
-        self.components.append(component)
+        self.components.append((glyphName, transformation))
 
     def glyph(self):
         glyph = Glyph()
 
+        components = []
+        for glyphName, transformation in self.components:
+            if self.points:
+                # can't have both, so decompose the glyph
+                tpen = TransformPen(self, transformation)
+                self.glyphSet[glyphName].draw(tpen)
+                continue
+
+            component = GlyphComponent()
+            component.glyphName = glyphName
+            component.transform = (transformation[:2], transformation[2:4])
+            component.x, component.y = [int(n) for n in transformation[4:]]
+            component.flags = 0
+            components.append(component)
+
         glyph.coordinates = GlyphCoordinates(self.points)
         glyph.endPtsOfContours = self.endPts
         glyph.flags = array("B", self.types)
-        glyph.components = self.components
 
-        if glyph.components:
-            assert not glyph.endPtsOfContours, (
-                "TrueType glyph can't have both contours and components.")
+        if components:
+            glyph.components = components
             glyph.numberOfContours = -1
         else:
             glyph.numberOfContours = len(glyph.endPtsOfContours)
