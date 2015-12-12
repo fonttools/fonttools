02279457810a1ae172634f69f73454b4a97f7445
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-12-12 18:58:00 +0000

    _t_r_a_k: do not import unicode_literals as it makes struct fail on some old Python 2.7.x
    On the latest Python 2.7.10, struct accepts both unicode and str as 'fmt'.
    However on Travis for py27 we use the built-in OSX Python, which on OSX 10.9 is Python 2.7.5.
    
    Travis log: https://travis-ci.org/anthrotype/fonttools/jobs/96473892#L3010
    
    Python issue: https://bugs.python.org/issue19099

--- Lib/fontTools/ttLib/tables/_t_r_a_k.py
+++ Lib/fontTools/ttLib/tables/_t_r_a_k.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, division, absolute_import, unicode_literals
+from __future__ import print_function, division, absolute_import
 from fontTools.misc.py23 import *
 from fontTools.misc import sstruct
 from fontTools.misc.fixedTools import fixedToFloat as fi2fl, floatToFixed as fl2fi
