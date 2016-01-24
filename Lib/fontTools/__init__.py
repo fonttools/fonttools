1034699a21dc0cf4862624d076d487deae7df9e2
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-01-24 14:20:59 +0000

    [fontTools.__init__] add NullHandler to avoid "no handlers could be found" error.

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -1,4 +1,10 @@
 from __future__ import print_function, division, absolute_import
 from fontTools.misc.py23 import *
+import logging
+
+# add a do-nothing handler to the libary's top-level logger, to avoid
+# "no handlers could be found" error if client doesn't configure logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 version = "3.0"
