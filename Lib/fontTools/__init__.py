c2d0f5fb0adc89b0b7bbe3940d94ac95c453dca5
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-02-01 13:06:29 +0000

    [fontTools.__init__] remove setLoggerClass (will move it to py23 module)

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -1,13 +1,7 @@
 from __future__ import print_function, division, absolute_import
 from fontTools.misc.py23 import *
 import logging
-from fontTools.misc.loggingTools import Logger, configLogger
-
-# set the logging.Logger class to one which supports the "last resort" handler,
-# to be used when the client doesn't explicitly configure logging.
-# It prints the bare message to sys.stderr, only for events of severity WARNING
-# or greater.
-logging.setLoggerClass(Logger)
+from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
