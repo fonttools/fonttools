f998cf1126996e275b82f4322759baeee70570cf
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-02-02 18:26:50 +0000

    [ttLib] use boolean instead of int
    I know it's same, but this looks more 'pythonic'

--- Lib/fontTools/ttLib/__init__.py
+++ Lib/fontTools/ttLib/__init__.py
@@ -197,11 +197,11 @@ class TTFont(object):
 			if self.lazy and self.reader.file.name == file:
 				raise TTLibError(
 					"Can't overwrite TTFont when 'lazy' attribute is True")
-			closeStream = 1
+			closeStream = True
 			file = open(file, "wb")
 		else:
 			# assume "file" is a writable file object
-			closeStream = 0
+			closeStream = False
 
 		tags = list(self.keys())
 		if "GlyphOrder" in tags:
