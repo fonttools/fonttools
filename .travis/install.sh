0be1d722dc360a499b71323d5356ebc5c6a5e6a8
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-04-08 22:04:28 +0100

    test on more recent pypy-5.0.0 (the latest one is 5.0.1 but is not yet available to install via pyenv)

--- .travis/install.sh
+++ .travis/install.sh
@@ -30,8 +30,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
             pyenv global 3.5.0
             ;;
         pypy)
-            pyenv install pypy-4.0.1
-            pyenv global pypy-4.0.1
+            pyenv install pypy-5.0.0
+            pyenv global pypy-5.0.0
             ;;
     esac
     pyenv rehash
@@ -43,8 +43,8 @@ else
         PYENV_ROOT="$HOME/.pyenv"
         PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
-        pyenv install pypy-4.0.1
-        pyenv global pypy-4.0.1
+        pyenv install pypy-5.0.0
+        pyenv global pypy-5.0.0
     fi
     pip install virtualenv
 fi
