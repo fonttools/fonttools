e8c8bb75b1f0a3f9fb9fb8dd698c4771ef2bf80b
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-04-10 22:59:02 +0100

    xmlReader: must import sys
    used at line 22 to read from stdin

--- Lib/fontTools/misc/xmlReader.py
+++ Lib/fontTools/misc/xmlReader.py
@@ -3,6 +3,7 @@ from fontTools.misc.py23 import *
 from fontTools import ttLib
 from fontTools.misc.textTools import safeEval
 from fontTools.ttLib.tables.DefaultTable import DefaultTable
+import sys
 import os
 import logging
 
