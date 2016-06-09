2cb93ee7dd9e1923651cc023f127aa9257333f0d
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-06-09 23:12:11 +0100

    areaPen.py: hook up quadratic_curve_area to AreaPen._qCurveToOne method

--- Lib/fontTools/pens/areaPen.py
+++ Lib/fontTools/pens/areaPen.py
@@ -53,3 +53,8 @@ class AreaPen(BasePen):
         p0 = self._getCurrentPoint()
         self.value += cubic_curve_area(p0, p1, p2, p3)
         self.value += polygon_area(p0, p3)
+
+    def _qCurveToOne(self, pt1, pt2):
+        p0 = self._getCurrentPoint()
+        self.value += quadratic_curve_area(p0, p1, p2)
+        self.value += polygon_area(p0, p2)
