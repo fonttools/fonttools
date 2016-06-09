ad4b94f4b99ce6b2182aa8880afab97aaab9f065
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-06-09 23:00:09 +0100

    areaPen.py: use regular tuples for points instead of namedtuple

--- Lib/fontTools/pens/areaPen.py
+++ Lib/fontTools/pens/areaPen.py
@@ -2,22 +2,18 @@
 
 import math
 from fontTools.pens.basePen import BasePen
-from collections import namedtuple
-
-
-P = namedtuple('P', ('x', 'y'))
 
 
 def distance(p0, p1):
-    return math.hypot(p0.x - p1.x, p0.y - p1.y)
+    return math.hypot(p0[0] - p1[0], p0[1] - p1[1])
 
 
 def interpolate(p0, p1, t):
-    return P(p0.x * (1 - t) + p1.x * t, p0.y * (1 - t) + p1.y * t)
+    return (p0[0] * (1 - t) + p1[0] * t, p0[1] * (1 - t) + p1[1] * t)
 
 
 def polygon_area(p0, p1):
-    return (p1.x - p0.x) * (p1.y + p0.y) * 0.5
+    return (p1[0] - p0[0]) * (p1[1] + p0[1]) * 0.5
 
 
 def quadratic_curve_area(p0, p1, p2):
@@ -27,10 +23,10 @@ def quadratic_curve_area(p0, p1, p2):
 
 
 def cubic_curve_area(p0, p1, p2, p3):
-    x0, y0 = p0.x, p0.y
-    x1, y1 = p1.x - x0, p1.y - y0
-    x2, y2 = p2.x - x0, p2.y - y0
-    x3, y3 = p3.x - x0, p3.y - y0
+    x0, y0 = p0[0], p0[1]
+    x1, y1 = p1[0] - x0, p1[1] - y0
+    x2, y2 = p2[0] - x0, p2[1] - y0
+    x3, y3 = p3[0] - x0, p3[1] - y0
     return (
         x1 * (   -   y2 -   y3) +
         x2 * (y1        - 2*y3) +
@@ -49,9 +45,9 @@ class AreaPen(BasePen):
 
     def _lineTo(self, p1):
         p0 = self._getCurrentPoint()
-        self.value += polygon_area(P(*p0), P(*p1))
+        self.value += polygon_area(p0, p1)
 
     def _curveToOne(self, p1, p2, p3):
         p0 = self._getCurrentPoint()
-        self.value += cubic_curve_area(P(*p0), P(*p1), P(*p2), P(*p3))
-        self.value += polygon_area(P(*p0), P(*p3))
+        self.value += cubic_curve_area(p0, p1, p2, p3)
+        self.value += polygon_area(p0, p3)
