aaf9294d97821f7cd2546e3b13b6d989d0959a77
James Godfrey-Kittle <jamesgk@google.com>
2016-06-21 16:40:17 -0700

    [feaLib] Add another test for language systems

--- Lib/fontTools/feaLib/builder_test.py
+++ Lib/fontTools/feaLib/builder_test.py
@@ -36,7 +36,7 @@ def makeTTFont():
         Q.swash R.swash S.swash T.swash U.swash V.swash W.swash X.swash
         Y.swash Z.swash
         f_l c_h c_k c_s c_t f_f f_f_i f_f_l f_i o_f_f_i s_t f_i.begin
-        a_n_d T_h T_h.swash ydieresis yacute breve
+        a_n_d T_h T_h.swash germandbls ydieresis yacute breve
         grave acute dieresis macron circumflex cedilla umlaut ogonek caron
         damma hamza sukun kasratan lam_meem_jeem noon.final noon.initial
     """.split()
@@ -53,7 +53,7 @@ class BuilderTest(unittest.TestCase):
         lookup lookupflag feature_aalt ignore_pos
         GPOS_1 GPOS_1_zero GPOS_2 GPOS_2b GPOS_3 GPOS_4 GPOS_5 GPOS_6 GPOS_8
         GSUB_2 GSUB_3 GSUB_6 GSUB_8
-        spec4h1 spec5d1 spec5d2 spec5fi1 spec5fi2 spec5fi3 spec5fi4
+        spec4h1 spec4h2 spec5d1 spec5d2 spec5fi1 spec5fi2 spec5fi3 spec5fi4
         spec5f_ii_1 spec5f_ii_2 spec5f_ii_3 spec5f_ii_4
         spec5h1 spec6b_ii spec6d2 spec6e spec6f
         spec6h_ii spec6h_iii_1 spec6h_iii_3d spec8a spec8b spec8c
