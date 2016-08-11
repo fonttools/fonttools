32bd6bcc31c4152ee4e0edff34f9258f2611424b
Behdad Esfahbod <behdad@behdad.org>
2016-08-11 01:02:52 -0700

    [varLib] Add optimizeVarData()
    Though not really effective right now since we don't split into
    multiple VarData sets.

--- Lib/fontTools/varLib/builder.py
+++ Lib/fontTools/varLib/builder.py
@@ -26,7 +26,38 @@ def buildVarRegionList(supports, axisTags):
 	self.VarRegionCount = len(self.VarRegion)
 	return self
 
-def buildVarData(varRegionIndices, items):
+
+def _reorderItem(lst, narrows):
+	out = []
+	count = len(lst)
+	for i in range(count):
+		if i not in narrows:
+			out.append(lst[i])
+	for i in range(count):
+		if i in narrows:
+			out.append(lst[i])
+	return out
+
+def optimizeVarData(self):
+	# Reorder columns such that all SHORT columns come before UINT8
+	count = self.VarRegionCount
+	items = self.Item
+	narrows = set(range(count))
+	for item in items:
+		for i in narrows:
+			if not (-128 <= item[i] <= 127):
+				narrows.remove(i)
+				break
+		if not narrows:
+			break
+
+	self.VarRegionIndex = _reorderItem(self.VarRegionIndex, narrows)
+	for i in range(self.ItemCount):
+		items[i] = _reorderItem(items[i], narrows)
+
+	return self
+
+def buildVarData(varRegionIndices, items, optimize=True):
 	self = ot.VarData()
 	self.VarRegionIndex = list(varRegionIndices)
 	regionCount = self.VarRegionCount = len(self.VarRegionIndex)
@@ -35,7 +66,10 @@ def buildVarData(varRegionIndices, items):
 		assert len(item) == regionCount
 		records.append(list(item))
 	self.ItemCount = len(self.Item)
+	if optimize:
+		optimizeVarData(self)
 	return self
+
 
 def buildVarStore(varTupleList, varDataList):
 	self = ot.VarStore()
