6f6a10dfa2efe420df448512cab191d7565a4ecc
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-09-11 16:09:11 +0100

    setup.py: add pytest runner; only use setuptools, drop distutils; set version 2.0.dev1; pep8 whitespace

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 #! /usr/bin/env python
-
-import os, sys
-
-try:
-	from setuptools import setup
-	extra_kwargs = {
-		"test_suite": "ufoLib.test"
-	}
-except ImportError:
-	from distutils.core import setup
-	extra_kwargs = {}
+import sys
+from setuptools import setup, find_packages
 
 try:
 	import fontTools
@@ -23,35 +14,49 @@ ufoLib reads and writes Unified Font Object (UFO) files. UFO is a file format
 that stores fonts source files.
 """
 
-setup(
-		name = "ufoLib",
-		version = "1.2",
-		description = "A low-level UFO reader and writer.",
-		author = "Just van Rossum, Tal Leming, Erik van Blokland, others",
-		author_email = "info@robofab.com",
-		maintainer = "Just van Rossum, Tal Leming, Erik van Blokland",
-		maintainer_email = "info@robofab.com",
-		url = "http://unifiedfontobject.org",
-		license = "OpenSource, BSD-style",
-		platforms = ["Any"],
-		long_description = long_description,
+needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
 
-		packages = [
-			"ufoLib",
-		],
-		package_dir = {'': 'Lib'},
-		classifiers = [
-			"Development Status :: 4 - Beta",
-			"Environment :: Console",
-			"Environment :: Other Environment",
-			"Intended Audience :: Developers",
-			"Intended Audience :: End Users/Desktop",
-			"License :: OSI Approved :: BSD License",
-			"Natural Language :: English",
-			"Operating System :: OS Independent",
-			"Programming Language :: Python",
-			"Topic :: Multimedia :: Graphics",
-			"Topic :: Multimedia :: Graphics :: Graphics Conversion",
-		],
-		**extra_kwargs
-	)
+setup_params = dict(
+	name="ufoLib",
+	version="2.0.dev1",
+	description="A low-level UFO reader and writer.",
+	author="Just van Rossum, Tal Leming, Erik van Blokland, others",
+	author_email="info@robofab.com",
+	maintainer="Just van Rossum, Tal Leming, Erik van Blokland",
+	maintainer_email="info@robofab.com",
+	url="http://unifiedfontobject.org",
+	license="OpenSource, BSD-style",
+	platforms=["Any"],
+	long_description=long_description,
+	package_dir={'': 'Lib'},
+	packages=find_packages('Lib'),
+	setup_requires=pytest_runner + wheel,
+	tests_require=[
+		'pytest>=3.0.2',
+	],
+	install_requires=[
+		# TODO(anthrotype): un-comment this once fonttools>=3.1 is on PyPI.
+		# In the meantime, pip install -r requirements.txt
+		# "fonttools>=3.1",
+	],
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Environment :: Console",
+		"Environment :: Other Environment",
+		"Intended Audience :: Developers",
+		"Intended Audience :: End Users/Desktop",
+		"License :: OSI Approved :: BSD License",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Topic :: Multimedia :: Graphics",
+		"Topic :: Multimedia :: Graphics :: Graphics Conversion",
+	],
+)
+
+
+if __name__ == "__main__":
+	setup(**setup_params)
