757d44e0078ebea2cc50ac54d3782747f788c804
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-12-16 23:36:31 +0000

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='cu2qu',
-    version='1.1.1.dev0',
+    version='version="1.1.1"',
     description='Cubic-to-quadratic bezier curve conversion',
     author="James Godfrey-Kittle, Behdad Esfahbod",
     author_email="jamesgk@google.com",
