3689f0ffdcde98ffd311c2eaae9153244ad3d9e1
Behdad Esfahbod <behdad@behdad.org>
2016-12-26 16:39:50 -0500

    [mtiLib] Whitespace

--- Lib/fontTools/mtiLib/mti_test.py
+++ Lib/fontTools/mtiLib/mti_test.py
@@ -13,41 +13,41 @@ import unittest
 class BuilderTest(unittest.TestCase):
     # Feature files in testdata/*.txt; output gets compared to testdata/*.ttx.
     TESTS = {
-	None: (
+        None: (
             #'mti/cmap',
-	),
-	'cmap': (
+        ),
+        'cmap': (
             #'mti/cmap',
-	),
-	'GSUB': (
-	    'mti/scripttable',
-	    'mti/chainedclass',
-	    'mti/chainedcoverage',
-	    'mti/chained-glyph',
-	    'mti/gsubalternate',
-	    'mti/gsubligature',
-	    'mti/gsubmultiple',
-	    'mti/gsubreversechanined',
-	    'mti/gsubsingle',
-	),
-	'GPOS': (
-	    'mti/scripttable',
-	    'mti/chained-glyph',
-	    'mti/gposcursive',
-	    #'mti/gposkernset',
-	    'mti/gposmarktobase',
-	    #'mti/gpospairclass',
-	    'mti/gpospairglyph',
-	    'mti/gpossingle',
-	    'mti/mark-to-ligature',
-	),
-	'GDEF': (
-	    'mti/gdefattach',
-	    'mti/gdefclasses',
-	    'mti/gdefligcaret',
-	    'mti/gdefmarkattach',
-	    'mti/gdefmarkfilter',
-	),
+        ),
+        'GSUB': (
+            'mti/scripttable',
+            'mti/chainedclass',
+            'mti/chainedcoverage',
+            'mti/chained-glyph',
+            'mti/gsubalternate',
+            'mti/gsubligature',
+            'mti/gsubmultiple',
+            'mti/gsubreversechanined',
+            'mti/gsubsingle',
+        ),
+        'GPOS': (
+            'mti/scripttable',
+            'mti/chained-glyph',
+            'mti/gposcursive',
+            #'mti/gposkernset',
+            'mti/gposmarktobase',
+            #'mti/gpospairclass',
+            'mti/gpospairglyph',
+            'mti/gpossingle',
+            'mti/mark-to-ligature',
+        ),
+        'GDEF': (
+            'mti/gdefattach',
+            'mti/gdefclasses',
+            'mti/gdefligcaret',
+            'mti/gdefmarkattach',
+            'mti/gdefmarkfilter',
+        ),
     }
     # TODO:
     # https://github.com/Monotype/OpenType_Table_Source/issues/12
@@ -76,12 +76,13 @@ class BuilderTest(unittest.TestCase):
         return os.path.join(path, "testdata", testfile)
 
     def expect_ttx(self, actual_ttx, expected_ttx, fromfile=None, tofile=None):
-        actual = actual_ttx.split('\n')
-        expected = expected_ttx.split('\n')
+        actual = [l+'\n' for l in actual_ttx.split('\n')]
+        expected = [l+'\n' for l in expected_ttx.split('\n')]
         if actual != expected:
+            print('\n')
             for line in difflib.unified_diff(
                     expected, actual, fromfile=fromfile, tofile=tofile):
-                sys.stdout.write(line+'\n')
+                sys.stdout.write(line)
             self.fail("TTX output is different from expected")
 
     def check_mti_file(self, name, tableTag=None):
@@ -89,36 +90,36 @@ class BuilderTest(unittest.TestCase):
         xml_expected = open(xml_expected_path, 'rt', encoding="utf-8").read()
         f = open(self.getpath("%s.txt" % name), 'rt', encoding="utf-8")
 
-	font = MockFont()
-	table = mtiLib.build(f, font, tableTag=tableTag)
+        font = MockFont()
+        table = mtiLib.build(f, font, tableTag=tableTag)
 
-	if tableTag is not None:
-	    self.assertEqual(tableTag, table.tableTag)
-	tableTag = table.tableTag
+        if tableTag is not None:
+            self.assertEqual(tableTag, table.tableTag)
+        tableTag = table.tableTag
 
-	# Make sure it compiles.
-	blob = table.compile(font)
+        # Make sure it compiles.
+        blob = table.compile(font)
 
-	# Make sure it decompiles.
-	decompiled = table.__class__()
-	decompiled.decompile(blob, font)
+        # Make sure it decompiles.
+        decompiled = table.__class__()
+        decompiled.decompile(blob, font)
 
-	# XML from built object.
-	writer = XMLWriter(StringIO(), newlinestr='\n')
-	writer.begintag(tableTag); writer.newline()
-	table.toXML(writer, font)
-	writer.endtag(tableTag); writer.newline()
-	xml_built = writer.file.getvalue()
+        # XML from built object.
+        writer = XMLWriter(StringIO(), newlinestr='\n')
+        writer.begintag(tableTag); writer.newline()
+        table.toXML(writer, font)
+        writer.endtag(tableTag); writer.newline()
+        xml_built = writer.file.getvalue()
 
-	# XML from decompiled object.
-	writer = XMLWriter(StringIO(), newlinestr='\n')
-	writer.begintag(tableTag); writer.newline()
-	decompiled.toXML(writer, font)
-	writer.endtag(tableTag); writer.newline()
-	xml_binary = writer.file.getvalue()
+        # XML from decompiled object.
+        writer = XMLWriter(StringIO(), newlinestr='\n')
+        writer.begintag(tableTag); writer.newline()
+        decompiled.toXML(writer, font)
+        writer.endtag(tableTag); writer.newline()
+        xml_binary = writer.file.getvalue()
 
-	self.expect_ttx(xml_built, xml_binary, fromfile='built', tofile='decompiled')
-	self.expect_ttx(xml_built, xml_expected, fromfile='built', tofile=xml_expected_path)
+        self.expect_ttx(xml_built, xml_binary, fromfile='built', tofile='decompiled')
+        self.expect_ttx(xml_built, xml_expected, fromfile='built', tofile=xml_expected_path)
 
 def generate_mti_file_test(name, tableTag=None):
     return lambda self: self.check_mti_file(name, tableTag=tableTag)
