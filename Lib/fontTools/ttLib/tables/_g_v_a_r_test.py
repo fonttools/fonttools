f5a679661dd0562fab78c2616db3c2471f57fbc9
Sascha Brawer <sascha@brawer.ch>
2017-01-05 17:40:39 +0100

    [gvar] Use ttLib accessors for importing module and module class

--- Lib/fontTools/ttLib/tables/_g_v_a_r_test.py
+++ Lib/fontTools/ttLib/tables/_g_v_a_r_test.py
@@ -1,13 +1,13 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 from fontTools.misc.py23 import *
 from fontTools.misc.textTools import deHexStr, hexStr
-from fontTools.ttLib import TTLibError, getTableModule
+from fontTools.ttLib import TTLibError, getTableClass, getTableModule, newTable
 from fontTools.ttLib.tables.TupleVariation import TupleVariation
-from fontTools.ttLib.tables._g_v_a_r import table__g_v_a_r
 import unittest
 
 
 gvar = getTableModule("gvar")
+gvarClass = getTableClass("gvar")
 
 
 def hexencode(s):
@@ -46,36 +46,38 @@ SKIA_SHARED_TUPLES = deHexStr(
 class GVARTableTest(unittest.TestCase):
 	def test_compileOffsets_shortFormat(self):
 		self.assertEqual((deHexStr("00 00 00 02 FF C0"), 0),
-				 table__g_v_a_r.compileOffsets_([0, 4, 0x1ff80]))
+		                 gvarClass.compileOffsets_([0, 4, 0x1ff80]))
 
 	def test_compileOffsets_longFormat(self):
 		self.assertEqual((deHexStr("00 00 00 00 00 00 00 04 CA FE BE EF"), 1),
-				 table__g_v_a_r.compileOffsets_([0, 4, 0xCAFEBEEF]))
+		                 gvarClass.compileOffsets_([0, 4, 0xCAFEBEEF]))
 
 	def test_decompileOffsets_shortFormat(self):
-		decompileOffsets = table__g_v_a_r.decompileOffsets_
+		decompileOffsets = gvarClass.decompileOffsets_
 		data = deHexStr("00 11 22 33 44 55 66 77 88 99 aa bb")
-		self.assertEqual([2*0x0011, 2*0x2233, 2*0x4455, 2*0x6677, 2*0x8899, 2*0xaabb],
-				 list(decompileOffsets(data, tableFormat=0, glyphCount=5)))
+		self.assertEqual(
+			[2*0x0011, 2*0x2233, 2*0x4455, 2*0x6677, 2*0x8899, 2*0xaabb],
+			list(decompileOffsets(data, tableFormat=0, glyphCount=5)))
 
 	def test_decompileOffsets_longFormat(self):
-		decompileOffsets = table__g_v_a_r.decompileOffsets_
+		decompileOffsets = gvarClass.decompileOffsets_
 		data = deHexStr("00 11 22 33 44 55 66 77 88 99 aa bb")
-		self.assertEqual([0x00112233, 0x44556677, 0x8899aabb],
-				 list(decompileOffsets(data, tableFormat=1, glyphCount=2)))
+		self.assertEqual(
+			[0x00112233, 0x44556677, 0x8899aabb],
+			list(decompileOffsets(data, tableFormat=1, glyphCount=2)))
 
 	def test_compileGlyph_noVariations(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		table.variations = {}
 		self.assertEqual(b"", table.compileGlyph_("glyphname", 8, ["wght", "opsz"], {}))
 
 	def test_compileGlyph_emptyVariations(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		table.variations = {"glyphname": []}
 		self.assertEqual(b"", table.compileGlyph_("glyphname", 8, ["wght", "opsz"], {}))
 
 	def test_compileGlyph_onlyRedundantVariations(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		axes = {"wght": (0.3, 0.4, 0.5), "opsz": (0.7, 0.8, 0.9)}
 		table.variations = {"glyphname": [
 			TupleVariation(axes, [None] * 4),
@@ -85,7 +87,7 @@ class GVARTableTest(unittest.TestCase):
 		self.assertEqual(b"", table.compileGlyph_("glyphname", 8, ["wght", "opsz"], {}))
 
 	def test_compileGlyph_roundTrip(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		axisTags = ["wght", "wdth"]
 		numPointsInGlyph = 4
 		glyphCoords = [(1,1), (2,2), (3,3), (4,4)]
@@ -96,7 +98,7 @@ class GVARTableTest(unittest.TestCase):
 		self.assertEqual([gvar1, gvar2], table.decompileGlyph_(numPointsInGlyph, {}, axisTags, data))
 
 	def test_compileSharedCoords(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		table.variations = {}
 		deltas = [None] * 4
 		table.variations["A"] = [
@@ -136,7 +138,7 @@ class GVARTableTest(unittest.TestCase):
 
 	def test_decompileGlyph_Skia_I(self):
 		axes = ["wght", "wdth"]
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		table.offsetToSharedTuples = 0
 		table.sharedTupleCount = 8
 		table.axisCount = len(axes)
@@ -151,7 +153,7 @@ class GVARTableTest(unittest.TestCase):
 				 " ".join(["%d,%d" % c for c in tuples[0].coordinates]))
 
 	def test_decompileGlyph_empty(self):
-		table = table__g_v_a_r()
+		table = newTable("gvar")
 		self.assertEqual([], table.decompileGlyph_(numPointsInGlyph=5, sharedCoords=[], axisTags=[], data=b""))
 
 
