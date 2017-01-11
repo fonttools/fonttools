e5dbc85463c1f97fecdd733b5bad3327454ded90
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-01-11 12:15:33 +0000

    [mtiLib] add __main__.py to allow running package as module
    This now works:
    
    $ fonttools mtiLib
    
    or which is the same:
    
    $ python -m fontTools.mtiLib

---
+++ Lib/fontTools/mtiLib/__main__.py
@@ -0,0 +1,7 @@
+from __future__ import print_function, division, absolute_import
+from fontTools.misc.py23 import *
+import sys
+from fontTools.mtiLib import main
+
+if __name__ == '__main__':
+	sys.exit(main())
