fonttools/fonttools

commit a738464f775c190bf70e26d0a32ff2344ec43201

[varLib] Let VariationMerger use AligningMerger for PairPos
VariationMerger now has all the intelligence of AligningMerge.
Should be good for a while...

Lib/fontTools/varLib/merger.py
@@ -308,6 +308,31 @@ def merge(merger, self, lst):
 
 	del merger.valueFormat1, merger.valueFormat2
 
+	# Now examine the list of value records, and update to the union of format values,
+	# as merge might have created new values.
+	vf1 = 0
+	vf2 = 0
+	if self.Format == 1:
+		for pairSet in self.PairSet:
+			for pairValueRecord in pairSet.PairValueRecord:
+				pv1 = pairValueRecord.Value1
+				if pv1 is not None:
+					vf1 |= pv1.getFormat()
+				pv2 = pairValueRecord.Value2
+				if pv2 is not None:
+					vf2 |= pv2.getFormat()
+	elif self.Format == 2:
+		for class1Record in self.Class1Record:
+			for class2Record in class1Record.Class2Record:
+				pv1 = class2Record.Value1
+				if pv1 is not None:
+					vf1 |= pv1.getFormat()
+				pv2 = class2Record.Value2
+				if pv2 is not None:
+					vf2 |= pv2.getFormat()
+	self.ValueFormat1 = vf1
+	self.ValueFormat2 = vf2
+
 
 def _PairSet_merge_overlay(lst, font):
 	self = ot.PairSet()
@@ -456,36 +481,6 @@ def merge(merger, self, lst):
 		self.Format = 3
 		self.XDeviceTable = XDeviceTable
 		self.YDeviceTable = YDeviceTable
-
-@VariationMerger.merger(ot.PairPos)
-def merge(merger, self, lst):
-	# We need to interecept the merger processing of the object tree here so
-	# that we can change the PairPos value formats after the ValueRecords are
-	# changed.
-	merger.mergeObjects(self, lst)
-	# Now examine the list of value records, and update to the union of format values.
-	vf1 = self.ValueFormat1
-	vf2 = self.ValueFormat2
-	if self.Format == 1:
-		for pairSet in self.PairSet:
-			for pairValueRecord in pairSet.PairValueRecord:
-				pv1 = pairValueRecord.Value1
-				if pv1 is not None:
-					vf1 |= pv1.getFormat()
-				pv2 = pairValueRecord.Value2
-				if pv2 is not None:
-					vf2 |= pv2.getFormat()
-	elif self.Format == 2:
-		for class1Record in self.Class1Record:
-			for class2Record in class1Record.Class2Record:
-				pv1 = class2Record.Value1
-				if pv1 is not None:
-					vf1 |= pv1.getFormat()
-				pv2 = class2Record.Value2
-				if pv2 is not None:
-					vf2 |= pv2.getFormat()
-	self.ValueFormat1 = vf1
-	self.ValueFormat2 = vf2
 
 @VariationMerger.merger(otBase.ValueRecord)
 def merge(merger, self, lst):

commit b5c34ceb15f19c0403524ba07492f4dee9ca8c90

[varLib] Fix building variation of PairPosFormat2
I broke this with a738464f775c190bf70e26d0a32ff2344ec43201
Ouch!

Lib/fontTools/varLib/merger.py
@@ -278,35 +278,38 @@ def merge(merger, self, lst):
 	merger.valueFormat1 = self.ValueFormat1
 	merger.valueFormat2 = self.ValueFormat2
 
-	if self.Format == 2:
+	if self.Format == 1:
+
+		# Merge everything else; makes sure Format is the same.
+		merger.mergeObjects(self, lst,
+				    exclude=('Coverage',
+					     'PairSet', 'PairSetCount'))
+
+		empty = ot.PairSet()
+		empty.PairValueRecord = []
+		empty.PairValueCount = 0
+
+		# Align them
+		glyphs, padded = _merge_GlyphOrders(merger.font,
+						    [v.Coverage.glyphs for v in lst],
+						    [v.PairSet for v in lst],
+						    default=empty)
+
+		self.Coverage.glyphs = glyphs
+		self.PairSet = [ot.PairSet() for g in glyphs]
+		self.PairSetCount = len(self.PairSet)
+		for glyph, ps in zip(glyphs, self.PairSet):
+			ps._firstGlyph = glyph
+
+		merger.mergeLists(self.PairSet, padded)
+
+	elif self.Format == 2:
+
 		# Everything must match; we don't support smart merge yet.
 		merger.mergeObjects(self, lst)
-		del merger.valueFormat1, merger.valueFormat2
-		return
 
-	assert self.Format == 1
-	# Merge everything else; makes sure Format is the same.
-	merger.mergeObjects(self, lst,
-			    exclude=('Coverage',
-				     'PairSet', 'PairSetCount'))
-
-	empty = ot.PairSet()
-	empty.PairValueRecord = []
-	empty.PairValueCount = 0
-
-	# Align them
-	glyphs, padded = _merge_GlyphOrders(merger.font,
-					    [v.Coverage.glyphs for v in lst],
-					    [v.PairSet for v in lst],
-					    default=empty)
-
-	self.Coverage.glyphs = glyphs
-	self.PairSet = [ot.PairSet() for g in glyphs]
-	self.PairSetCount = len(self.PairSet)
-	for glyph, ps in zip(glyphs, self.PairSet):
-		ps._firstGlyph = glyph
-
-	merger.mergeLists(self.PairSet, padded)
+	else:
+		assert 0
 
 	del merger.valueFormat1, merger.valueFormat2
 