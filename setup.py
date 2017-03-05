c28f3f06b5a16d58f68f0a23e58bd9d38d453072
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-05 21:46:45 +0000

    Bump version: 3.8.0 → 3.8.1.dev0

--- setup.py
+++ setup.py
@@ -309,7 +309,7 @@ def find_data_files(manpath="share/man"):
 
 setup(
 	name="fonttools",
-	version="3.8.0",
+	version="3.8.1.dev0",
 	description="Tools to manipulate font files",
 	author="Just van Rossum",
 	author_email="just@letterror.com",
