6731a906b45c42600b1b7e79ae05e6ac562093b7
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2017-03-07 13:00:37 +0000

    [T_S_I_*] in VOLT's ascii tables, replace '\r' with '\n' when dumping XML
    and viceversa, convert '\n' back to '\r' when reading from XML.
    
    This restores the original newline handling of asciiTable, but only for VOLT's private tables.

--- Lib/fontTools/ttLib/tables/T_S_I_S_.py
+++ Lib/fontTools/ttLib/tables/T_S_I_S_.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division, absolute_import
 from fontTools.misc.py23 import *
-from . import asciiTable
+from .T_S_I_V_ import table_T_S_I_V_
 
-class table_T_S_I_S_(asciiTable.asciiTable):
+class table_T_S_I_S_(table_T_S_I_V_):
 	pass
