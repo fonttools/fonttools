733d828ce500273e147ecfe28b16399848468bba
Sascha Brawer <sascha@brawer.ch>
2017-08-16 02:38:00 +0200

    [AAT] Support `lcar` table

---
+++ Lib/fontTools/ttLib/tables/_l_c_a_r.py
@@ -0,0 +1,7 @@
+from __future__ import print_function, division, absolute_import
+from fontTools.misc.py23 import *
+from .otBase import BaseTTXConverter
+
+
+class table__l_c_a_r(BaseTTXConverter):
+	pass
