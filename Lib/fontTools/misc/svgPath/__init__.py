cb91e3d742f234f8e531b46314c3ed63b5614138
Cosimo Lupo <cosimo@anthrotype.com>
2017-09-08 18:17:08 +0100

    [svgPath] export parse_path function from svgPath.parser submodule

---
+++ Lib/fontTools/misc/svgPath/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import (
+    print_function, division, absolute_import, unicode_literals)
+from fontTools.misc.py23 import *
+
+from .parser import parse_path
+
+__all__ = [tostr("parse_path")]
