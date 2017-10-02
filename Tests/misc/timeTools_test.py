2e4b1d05df2d720bc1f4ba03b6dacb9171116ee1
Cosimo Lupo <cosimo@anthrotype.com>
2017-10-02 14:19:01 +0100

    [timeTools_test] use pytest.raises to catch ValueError

--- Tests/misc/timeTools_test.py
+++ Tests/misc/timeTools_test.py
@@ -3,11 +3,13 @@ from fontTools.misc.py23 import *
 from fontTools.misc.timeTools import asctime, timestampNow, epoch_diff
 import os
 import time
+import pytest
 
 
 def test_asctime():
     assert isinstance(asctime(), basestring)
     assert asctime(time.gmtime(0)) == 'Thu Jan  1 00:00:00 1970'
+
 
 def test_source_date_epoch():
     os.environ["SOURCE_DATE_EPOCH"] = "150687315"
@@ -15,11 +17,8 @@ def test_source_date_epoch():
 
     # Check that malformed value fail, any better way?
     os.environ["SOURCE_DATE_EPOCH"] = "ABCDEFGHI"
-    try:
+    with pytest.raises(ValueError):
         timestampNow()
-        assert False
-    except ValueError:
-        assert True
 
     del os.environ["SOURCE_DATE_EPOCH"]
     assert timestampNow() + epoch_diff != 150687315
