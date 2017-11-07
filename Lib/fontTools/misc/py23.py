d3251241be0a43d34283008340c26acfaa4aa3a2
Cosimo Lupo <cosimo@anthrotype.com>
2017-11-07 11:54:28 +0000

    [py23] Add backports for redirect_stdout/stderr context managers

--- Lib/fontTools/misc/py23.py
+++ Lib/fontTools/misc/py23.py
@@ -496,6 +496,51 @@ except ImportError:
 			return self.__dict__ == other.__dict__
 
 
+if PY3:
+	from contextlib import redirect_stdout, redirect_stderr
+else:
+	# These convenience helpers were added to contextlib module with python3.4
+	# The code below is copied from:
+	# https://github.com/python/cpython/blob/57161aa/Lib/contextlib.py
+
+	class _RedirectStream(object):
+
+		_stream = None
+
+		def __init__(self, new_target):
+			self._new_target = new_target
+			# We use a list of old targets to make this CM re-entrant
+			self._old_targets = []
+
+		def __enter__(self):
+			self._old_targets.append(getattr(sys, self._stream))
+			setattr(sys, self._stream, self._new_target)
+			return self._new_target
+
+		def __exit__(self, exctype, excinst, exctb):
+			setattr(sys, self._stream, self._old_targets.pop())
+
+
+	class redirect_stdout(_RedirectStream):
+		"""Context manager for temporarily redirecting stdout to another file.
+			# How to send help() to stderr
+			with redirect_stdout(sys.stderr):
+				help(dir)
+			# How to write help() to a file
+			with open('help.txt', 'w') as f:
+				with redirect_stdout(f):
+					help(pow)
+		"""
+
+		_stream = "stdout"
+
+
+	class redirect_stderr(_RedirectStream):
+		"""Context manager for temporarily redirecting stderr to another file."""
+
+		_stream = "stderr"
+
+
 if __name__ == "__main__":
 	import doctest, sys
 	sys.exit(doctest.testmod().failed)
