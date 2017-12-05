f0e0954afc787d53c595a472c2346754c8f6f3d7
Khaled Hosny <khaledhosny@eglug.org>
2017-12-05 13:06:05 +0200

    [feaLib] Micro optimization
    Infer glyph classes only when we are going to use them.

--- Lib/fontTools/feaLib/builder.py
+++ Lib/fontTools/feaLib/builder.py
@@ -413,17 +413,16 @@ class Builder(object):
             return None
 
     def buildGDEFGlyphClassDef_(self):
-        inferredGlyphClass = {}
-        for lookup in self.lookups_:
-            inferredGlyphClass.update(lookup.inferGlyphClasses())
-        for markClass in self.parseTree.markClasses.values():
-            for markClassDef in markClass.definitions:
-                for glyph in markClassDef.glyphSet():
-                    inferredGlyphClass[glyph] = 3
         if self.glyphClassDefs_:
             classes = {g: c for (g, (c, _)) in self.glyphClassDefs_.items()}
         else:
-            classes = inferredGlyphClass
+            classes = {}
+            for lookup in self.lookups_:
+                classes.update(lookup.inferGlyphClasses())
+            for markClass in self.parseTree.markClasses.values():
+                for markClassDef in markClass.definitions:
+                    for glyph in markClassDef.glyphSet():
+                        classes[glyph] = 3
         if classes:
             result = otTables.GlyphClassDef()
             result.classDefs = classes
