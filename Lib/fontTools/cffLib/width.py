948361f981ca17a0cb01ecf106f387fd8d562ecb
Behdad Esfahbod <behdad@behdad.org>
2018-02-10 16:27:25 -0600

    [cffLib.width] Fix for py3

--- Lib/fontTools/cffLib/width.py
+++ Lib/fontTools/cffLib/width.py
@@ -7,7 +7,7 @@ from fontTools.misc.py23 import *
 from fontTools.ttLib import TTFont, getTableClass
 from collections import defaultdict
 from operator import add
-from functools import partial
+from functools import partial, reduce
 
 
 class missingdict(dict):
