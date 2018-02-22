29f5c13a37d5aa0ffa3b710b609c35e0d7fa45be
Miguel Sousa <msousa@adobe.com>
2018-02-22 14:40:29 -0800

    [cffLib_test] Test setting topDict.Encoding
    #1177

--- Tests/cffLib/cffLib_test.py
+++ Tests/cffLib/cffLib_test.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division, absolute_import
 from fontTools.cffLib import TopDict, PrivateDict, CharStrings
 from fontTools.misc.testTools import parseXML, DataFilesHandler
+from fontTools.ttLib import TTFont
 import sys
 import unittest
 
@@ -42,6 +43,21 @@ class CffLibTest(DataFilesHandler):
 
         topDict.recalcFontBBox()
         self.assertEqual(topDict.FontBBox, [0, 0, 0, 0])
+
+    def test_topDict_set_Encoding(self):
+        file_name = 'TestOTF.otf'
+        font_path = self.getpath(file_name)
+        temp_path = self.temp_font(font_path, file_name)
+        save_path = temp_path[:-4] + '2.otf'
+        font = TTFont(temp_path)
+        topDict = font["CFF "].cff.topDictIndex[0]
+        encoding = [".notdef"] * 256
+        encoding[0x20] = "space"
+        topDict.Encoding = encoding
+        font.save(save_path)
+        font2 = TTFont(save_path)
+        topDict2 = font2["CFF "].cff.topDictIndex[0]
+        self.assertEqual(topDict2.Encoding[32], "space")
 
 
 if __name__ == "__main__":
