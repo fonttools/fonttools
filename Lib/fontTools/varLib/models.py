0359daad3d5beab5726fe811b4f547a7a6e0b775
Behdad Esfahbod <behdad@behdad.org>
2018-03-28 01:38:10 -0700

    Revert "[varLib.models] Improve modeling"
    This reverts commit e4d363cc5c14471b02a98657e9bc04b75109c687.

--- Lib/fontTools/varLib/models.py
+++ Lib/fontTools/varLib/models.py
@@ -171,7 +171,7 @@ class VariationModel(object):
 		self.mapping = [self.locations.index(l) for l in locations] # Mapping from user's master order to our master order
 		self.reverseMapping = [locations.index(l) for l in self.locations] # Reverse of above
 
-		self._computeMasterSupports(axisPoints, axisOrder)
+		self._computeMasterSupports(axisPoints)
 
 	@staticmethod
 	def getMasterLocationsSortKeyFunc(locations, axisOrder=[]):
@@ -222,7 +222,7 @@ class VariationModel(object):
 		else:
 			return value
 
-	def _computeMasterSupports(self, axisPoints, axisOrder):
+	def _computeMasterSupports(self, axisPoints):
 		supports = []
 		deltaWeights = []
 		locations = self.locations
@@ -253,31 +253,15 @@ class VariationModel(object):
 						break
 				if not relevant:
 					continue
-				# Split the box for new master; split in whatever direction
-				# that results in less area-ratio lost.
-
-				orderedAxes = [axis for axis in axisOrder if axis in m.keys()]
-				orderedAxes.extend([axis for axis in sorted(m.keys()) if axis not in axisOrder])
-				bestAxis = None
-				bestPercentage = -1
-				for axis in reversed(orderedAxes):
-					val = m[axis]
+				# Split the box for new master
+				for axis,val in m.items():
 					assert axis in box
 					lower,locV,upper = box[axis]
-					newLower, newUpper = lower, upper
 					if val < locV:
-						newLower = val
+						lower = val
 					elif locV < val:
-						newUpper = val
-					percentage = (newUpper - newLower) / (upper - lower)
-					if percentage > bestPercentage:
-						bestPercentage = percentage
-						bestAxis = axis
-						bestLower = newLower
-						bestUpper = newUpper
-
-				if bestAxis:
-					box[bestAxis] = (bestLower,locV,bestUpper)
+						upper = val
+					box[axis] = (lower,locV,upper)
 			supports.append(box)
 
 			deltaWeight = {}
