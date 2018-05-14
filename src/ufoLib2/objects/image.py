7cd6739d647c9ec7bf4e5bf9cb4c7831f0260c4a
Cosimo Lupo <cosimo@anthrotype.com>
2018-05-14 11:38:26 +0100

    image: add converter/setter for optional transformation
    the Image class is also used for the default Glyph.image (the background image)
    which by default is empty (no filename, no transform, nothing). But it is
    expected to be there (not None) so that clients can set attributes on it
    without needing to instantiate one.

--- src/ufoLib2/objects/image.py
+++ src/ufoLib2/objects/image.py
@@ -3,8 +3,24 @@ from typing import Optional
 from ufoLib2.objects.misc import Transformation
 
 
+def _to_optional_transformation(v):
+    if v is not None and not isinstance(v, Transformation):
+        return Transformation(*v)
+
+
 @attr.s(slots=True)
 class Image(object):
-    fileName = attr.ib(type=Optional[str])
-    transformation = attr.ib(type=Transformation)
+    fileName = attr.ib(default=None, type=Optional[str])
+    _transformation = attr.ib(
+        default=None,
+        convert=_to_optional_transformation,
+        type=Optional[Transformation])
     color = attr.ib(default=None, type=Optional[str])
+
+    @property
+    def transformation(self):
+        return self._transformation
+
+    @transformation.setter
+    def transformation(self, value):
+        self._transformation = _to_optional_transformation(value)
