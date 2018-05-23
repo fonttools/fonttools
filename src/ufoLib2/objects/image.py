60cd1fa738558957e15b0f485c431a56b8a0baba
Cosimo Lupo <cosimo@anthrotype.com>
2018-05-23 13:18:31 +0100

    image: always set default (1,0,0,1,0,0) transform
    it's never None in defcon.Image

--- src/ufoLib2/objects/image.py
+++ src/ufoLib2/objects/image.py
@@ -3,18 +3,19 @@ from typing import Optional
 from ufoLib2.objects.misc import Transformation
 
 
-def _to_optional_transformation(v):
-    if v is not None and not isinstance(v, Transformation):
+def _to_transformation(v):
+    if not isinstance(v, Transformation):
         return Transformation(*v)
+    return v
 
 
 @attr.s(slots=True)
 class Image(object):
     fileName = attr.ib(default=None, type=Optional[str])
     _transformation = attr.ib(
-        default=None,
-        convert=_to_optional_transformation,
-        type=Optional[Transformation],
+        default=Transformation(),
+        convert=_to_transformation,
+        type=Transformation,
     )
     color = attr.ib(default=None, type=Optional[str])
 
@@ -24,7 +25,7 @@ class Image(object):
 
     @transformation.setter
     def transformation(self, value):
-        self._transformation = _to_optional_transformation(value)
+        self._transformation = _to_transformation(value)
 
     def clear(self):
         self.fileName = None
