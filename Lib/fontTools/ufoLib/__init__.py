f0d705d21ea705568c5c6195fe4e44a8fc0059b9
Cosimo Lupo <cosimo@anthrotype.com>
2018-06-07 18:39:11 +0100

    ufoLib: remove __version__
    we are no longer using setuptools_scm here... :,(

--- Lib/fontTools/ufoLib/__init__.py
+++ Lib/fontTools/ufoLib/__init__.py
@@ -7,10 +7,5 @@ from fontTools.ufoLib.objects import Font
 from fontTools.ufoLib.reader import UFOReader
 from fontTools.ufoLib.writer import UFOWriter
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "0.0.0+unknown"
-
 
 __all__ = ["Font", "UFOReader", "UFOWriter"]
