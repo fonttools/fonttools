c4950ecc8b470ad82b897619ef11a581496c77d0
Cosimo Lupo <cosimo@anthrotype.com>
2018-06-12 17:29:10 +0100

    py23: add 'zip' (alias to izip in py2) to __all__

--- Lib/fontTools/misc/py23.py
+++ Lib/fontTools/misc/py23.py
@@ -7,7 +7,7 @@ import sys
 __all__ = ['basestring', 'unicode', 'unichr', 'byteord', 'bytechr', 'BytesIO',
 		'StringIO', 'UnicodeIO', 'strjoin', 'bytesjoin', 'tobytes', 'tostr',
 		'tounicode', 'Tag', 'open', 'range', 'xrange', 'round', 'Py23Error',
-		'SimpleNamespace']
+		'SimpleNamespace', 'zip']
 
 
 class Py23Error(NotImplementedError):
