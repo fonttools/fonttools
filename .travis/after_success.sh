056bd7d967e55437e54c39a26c50607ea153415a
Cosimo Lupo <cosimo@anthrotype.com>
2018-09-11 12:09:50 +0200

    [travis] use twine to upload to PyPI

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,3 +9,8 @@ fi
 
 # upload coverage data to Codecov.io
 [[ ${TOXENV} == *"-cov"* ]] && tox -e codecov
+
+# if tagged commit, create distribution packages and deploy to PyPI
+if [ -n "$TRAVIS_TAG" ] && [ "$TRAVIS_REPO_SLUG" == "fonttools/fonttools" ] && [ "$BUILD_DIST" == true ]; then
+    tox -e pypi
+fi
