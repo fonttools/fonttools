31ad0eb8410e03dfe13692cc74e8655653d588ac
Cosimo Lupo <cosimo@anthrotype.com>
2018-09-26 14:25:19 +0100

    setup.py: make building of extension module optional
    One can now use either the $CU2QU_WITH_CYTHON environment variable
    or the --with/without-cython command line options to setup.py.
    
    The --without-cython is for when one wants to get a pure, universal
    wheel despite having both cython and a C compiler installed.
    
    By default (when no env var nor command line options are used),
    building from source will attempt to compile the extension module
    using the pre-generated *.c source files that are always included
    in the sdist package that we (will) upload to PyPI.
    If the compilation fails (e.g. the user doesn't have a C compiler),
    then an error message will be printed but installation will proceed
    without the optional native extension.
    When cloning a fresh repo, the *.c sources will not be present, so
    to build the extension from local checkout one always needs Cython
    installed.
    
    When $CU2QU_WITH_CYTHON is true (1, or yes), then cython is required
    and installation will abort if any error occurs during building.

--- setup.py
+++ setup.py
@@ -13,14 +13,184 @@
 # limitations under the License.
 
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
+import pkg_resources
+from distutils import log
 import sys
+import os
+import re
+from io import open
 
 
 needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
 pytest_runner = ['pytest_runner'] if needs_pytest else []
 needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
 wheel = ['wheel'] if needs_wheel else []
+
+# Check if minimum required Cython is available.
+# For consistency, we require the same as our vendored Cython.Shadow module
+cymod = "Lib/cu2qu/cython.py"
+cython_version_re = re.compile('__version__ = ["\']([0-9][0-9\w\.]+)["\']')
+with open(cymod, "r", encoding="utf-8") as fp:
+    for line in fp:
+        m = cython_version_re.match(line)
+        if m:
+            cython_min_version = m.group(1)
+            break
+    else:
+        sys.exit("error: failed to parse cython version in '%s'" % cymod)
+
+required_cython = "cython >= %s" % cython_min_version
+try:
+    pkg_resources.require(required_cython)
+except pkg_resources.ResolutionError:
+    has_cython = False
+else:
+    has_cython = True
+
+# First, check if the CU2QU_WITH_CYTHON environment variable is set.
+# Values "1", "true" or "yes" mean that Cython is required and will be used
+# to regenerate the *.c sources from which the native extension is built;
+# "0", "false" or "no" mean that Cython is not required and no extension
+# module will be compiled (i.e. the wheel is pure-python and universal).
+# If the variable is not set, then the pre-generated *.c sources that
+# are included in the sdist package will be used to try build the extension.
+# However, if any error occurs during compilation (e.g. the host
+# machine doesn't have the required compiler toolchain installed), the
+# installation proceeds without the compiled extensions, but will only have
+# the pure-python module.
+env_with_cython = os.environ.get("CU2QU_WITH_CYTHON")
+with_cython = (
+    True if env_with_cython in {"1", "true", "yes"}
+    else False if env_with_cython in {"0", "false", "no"}
+    else None
+)
+
+# command line options --with-cython and --without-cython are also supported.
+# They override the environment variable
+opt_with_cython = {'--with-cython'}.intersection(sys.argv)
+opt_without_cython = {'--without-cython'}.intersection(sys.argv)
+if opt_with_cython and opt_without_cython:
+    sys.exit(
+        "error: the options '--with-cython' and '--without-cython' are "
+        "mutually exclusive"
+    )
+elif opt_with_cython:
+    sys.argv.remove("--with-cython")
+    with_cython = True
+elif opt_without_cython:
+    sys.argv.remove("--without-cython")
+    with_cython = False
+
+
+class cython_build_ext(_build_ext):
+    """Compile *.pyx source files to *.c using cythonize if Cython is
+    installed, else use the pre-generated *.c sources.
+    """
+
+    def finalize_options(self):
+        if with_cython:
+            if not has_cython:
+                from distutils.errors import DistutilsSetupError
+
+                raise DistutilsSetupError(
+                    "%s is required when using --with-cython" % required_cython
+                )
+
+            from Cython.Build import cythonize
+
+            # optionally enable line tracing for test coverage support
+            linetrace = os.environ.get("CYTHON_TRACE") == "1"
+
+            self.distribution.ext_modules[:] = cythonize(
+                self.distribution.ext_modules,
+                force=linetrace or self.force,
+                annotate=os.environ.get("CYTHON_ANNOTATE") == "1",
+                quiet=not self.verbose,
+                compiler_directives={
+                    "linetrace": linetrace,
+                    "language_level": 3,
+                    "embedsignature": True,
+                },
+            )
+        else:
+            # replace *.py/.pyx sources with their pre-generated *.c versions
+            for ext in self.distribution.ext_modules:
+                ext.sources = [re.sub("\.pyx?$", ".c", n) for n in ext.sources]
+
+        _build_ext.finalize_options(self)
+
+    def build_extensions(self):
+        if not has_cython:
+            log.info(
+                "%s is not installed. Pre-generated *.c sources will be "
+                "will be used to build the extensions." % required_cython
+            )
+
+        try:
+            _build_ext.build_extensions(self)
+        except Exception as e:
+            if with_cython:
+                raise
+            # optional compilation failed: we delete 'ext_modules' and make sure
+            # the generated wheel is 'pure'
+            del self.distribution.ext_modules[:]
+            bdist_wheel = self.get_finalized_command("bdist_wheel")
+            bdist_wheel.root_is_pure = True
+            log.error('error: building extensions failed: %s' % e)
+
+    def get_source_files(self):
+        filenames = _build_ext.get_source_files(self)
+
+        # include pre-generated *.c sources inside sdist, but only if cython is
+        # installed (and hence they will be updated upon making the sdist)
+        if has_cython:
+            for ext in self.extensions:
+                filenames.extend(
+                    [re.sub("\.pyx?$", ".c", n) for n in ext.sources]
+                )
+        return filenames
+
+
+class cython_sdist(_sdist):
+    """ Run 'cythonize' on *.pyx sources to ensure the *.c files included
+    in the source distribution are up-to-date.
+    """
+
+    def run(self):
+        if with_cython and not has_cython:
+            from distutils.errors import DistutilsSetupError
+
+            raise DistutilsSetupError(
+                "%s is required when creating sdist --with-cython"
+                % required_cython
+            )
+
+        if has_cython:
+            from Cython.Build import cythonize
+
+            cythonize(
+                self.distribution.ext_modules,
+                force=True,  # always regenerate *.c sources
+                quiet=not self.verbose,
+                compiler_directives={
+                    "language_level": 3,
+                    "embedsignature": True
+                },
+            )
+
+        _sdist.run(self)
+
+
+# don't build extensions if user explicitly requested --without-cython
+if with_cython is False:
+    extensions = []
+else:
+    extensions = [
+        Extension("cu2qu.cu2qu", ["Lib/cu2qu/cu2qu.py"]),
+    ]
 
 with open('README.rst', 'r') as f:
     long_description = f.read()
@@ -36,6 +206,7 @@ setup(
     long_description=long_description,
     packages=find_packages('Lib'),
     package_dir={'': 'Lib'},
+    ext_modules=extensions,
     include_package_data=True,
     setup_requires=pytest_runner + wheel + ["setuptools_scm"],
     tests_require=[
@@ -60,4 +231,5 @@ setup(
         'Topic :: Multimedia :: Graphics :: Editors :: Vector-Based',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    cmdclass={"build_ext": cython_build_ext, "sdist": cython_sdist},
 )
