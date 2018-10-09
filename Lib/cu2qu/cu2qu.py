7e130ea438875b3a32564fe02adfd60286b3eed0
Behdad Esfahbod <behdad@behdad.org>
2018-10-09 09:46:30 -0400

    [cython] Revert one of the cdivisions
    Seeing if makes bot happy.

--- Lib/cu2qu/cu2qu.py
+++ Lib/cu2qu/cu2qu.py
@@ -104,14 +104,13 @@ def split_cubic_into_n_iter(p0, p1, p2, p3, n):
     return _split_cubic_into_n_gen(p0,p1,p2,p3,n)
 
 
-@cython.cdivision(True)
 @cython.locals(p0=cython.complex, p1=cython.complex, p2=cython.complex, p3=cython.complex, n=cython.int)
 @cython.locals(a=cython.complex, b=cython.complex, c=cython.complex, d=cython.complex)
 @cython.locals(dt=cython.double, delta_2=cython.double, delta_3=cython.double, i=cython.int)
 @cython.locals(a1=cython.complex, b1=cython.complex, c1=cython.complex, d1=cython.complex)
 def _split_cubic_into_n_gen(p0, p1, p2, p3, n):
     a, b, c, d = calc_cubic_parameters(p0, p1, p2, p3)
-    dt = 1. / n
+    dt = 1 / n
     delta_2 = dt * dt
     delta_3 = dt * delta_2
     for i in range(n):
