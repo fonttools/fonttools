19c4b796643a36d20f870c7b132bd8d6a54bfe04
Chris Simpkins <git.simpkins@gmail.com>
2018-10-10 14:18:08 -0400

    [Doc] conf.py - do not display version number

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -90,7 +90,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"display_version": False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
