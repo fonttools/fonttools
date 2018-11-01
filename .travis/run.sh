364a578c1df8276c24b25cb2f25dea49fe03abca
Cosimo Lupo <cosimo@anthrotype.com>
2018-11-01 13:36:13 +0000

    .travis/run.sh: TOXENV variable may list multiple tox envs

--- .travis/run.sh
+++ .travis/run.sh
@@ -11,4 +11,10 @@ tox
 
 # re-run all the XML-related tests, this time without lxml but using the
 # built-in ElementTree library.
-tox -e ${TOXENV:-py}-nolxml -- Tests/ufoLib Tests/misc/etree_test.py Tests/misc/plistlib_test.py
+if [ -z "$TOXENV" ]; then
+    TOXENV="py-nolxml"
+else
+    # strip additional tox envs after the comma, add -nolxml factor
+    TOXENV="${TOXENV%,*}-nolxml"
+fi
+tox -e $TOXENV -- Tests/ufoLib Tests/misc/etree_test.py Tests/misc/plistlib_test.py
