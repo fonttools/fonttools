277c7da95106ee5b1532dd1bb52543921e24efc0
justvanrossum <justvanrossum@gmail.com>
2018-11-01 10:28:05 +0100

    added __all__ module attr

--- Lib/fontTools/fontBuilder.py
+++ Lib/fontTools/fontBuilder.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division, absolute_import
 from __future__ import unicode_literals
 
+__all__ = ["FontBuilder"]
+
 """
 The `FontBuilder` class is a convenient helper to construct working TTF or OTF
 fonts from scratch.
