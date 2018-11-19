66ec4feb6166b6e716556fbf658b9bac2f075513
Cosimo Lupo <cosimo@anthrotype.com>
2018-11-19 12:26:36 +0000

    Set up Windows CI on Travis
    https://blog.travis-ci.com/2018-10-11-windows-early-release

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-if [[ -n "$PYENV_VERSION" ]]; then
+if [[ -n $PYENV_VERSION ]]; then
     wget https://github.com/praekeltfoundation/travis-pyenv/releases/download/${TRAVIS_PYENV_VERSION}/setup-pyenv.sh
     source setup-pyenv.sh
+fi
+
+if [[ $TRAVIS_OS_NAME == windows ]]; then
+    choco install python${PYTHON_VERSION:0:1}
+    # strip the '.' from major.minor version string, e.g. '2.7' -> '27'
+    pyver="${PYTHON_VERSION//\./}"
+    export PATH=/c/Python${pyver}/Scripts:/c/Python${pyver}/:$PATH
 fi
