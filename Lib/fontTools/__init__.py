2f2ccdc2c691f3ca49715a2c9f458871262226dc
Cosimo Lupo <cosimo@anthrotype.com>
2018-12-17 13:37:11 +0000

    Bump version: 3.34.2 → 3.34.3.dev0

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -5,6 +5,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "3.34.2"
+version = __version__ = "3.34.3.dev0"
 
 __all__ = ["version", "log", "configLogger"]
