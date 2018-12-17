74ec7cec2a483052b6420ddd4ff17d2cc028b664
Cosimo Lupo <cosimo@anthrotype.com>
2018-12-17 13:16:01 +0000

    Bump version: 3.34.1 → 3.34.2.dev0

--- setup.py
+++ setup.py
@@ -347,7 +347,7 @@ def find_data_files(manpath="share/man"):
 
 setup(
 	name="fonttools",
-	version="3.34.1",
+	version="3.34.2.dev0",
 	description="Tools to manipulate font files",
 	author="Just van Rossum",
 	author_email="just@letterror.com",
