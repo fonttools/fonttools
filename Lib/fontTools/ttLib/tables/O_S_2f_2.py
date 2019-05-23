47f8d06d9ae70f70c35054e7122f6c828c0e4bf5
schriftgestalt <georg.seifert@mac.com>
2019-05-23 17:40:01 +0200

    fix sign of fsType

--- Lib/fontTools/ttLib/tables/O_S_2f_2.py
+++ Lib/fontTools/ttLib/tables/O_S_2f_2.py
@@ -43,7 +43,7 @@ OS2_format_0 = """
 	xAvgCharWidth:          h       # average character width
 	usWeightClass:          H       # degree of thickness of strokes
 	usWidthClass:           H       # aspect ratio
-	fsType:                 h       # type flags
+	fsType:                 H       # type flags
 	ySubscriptXSize:        h       # subscript horizontal font size
 	ySubscriptYSize:        h       # subscript vertical font size
 	ySubscriptXOffset:      h       # subscript x offset
