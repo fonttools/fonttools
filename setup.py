cbab74f7fcbc27d4e36c02b8d8b0b3e0b6e3bd2c
Cosimo Lupo <clupo@google.com>
2019-05-28 11:30:22 +0100

    update unicodedata2 backport for Unicode 12.0

--- setup.py
+++ setup.py
@@ -57,10 +57,10 @@ extras_require = {
 	# which varies between python versions and may be outdated.
 	"unicode": [
 		# the unicodedata2 extension module doesn't work on PyPy.
-		# Python 3.7 already has Unicode 11, so the backport is not needed.
+		# Python 3.8 already has Unicode 12, so the backport is not needed.
 		(
-			"unicodedata2 >= 11.0.0; "
-			"python_version < '3.7' and platform_python_implementation != 'PyPy'"
+			"unicodedata2 >= 12.0.0; "
+			"python_version < '3.8' and platform_python_implementation != 'PyPy'"
 		),
 	],
 	# for graphite type tables in ttLib/tables (Silf, Glat, Gloc)
