5d7e750b6995f3bb39f40df912e022b1bbdeb1ec
Cosimo Lupo <clupo@google.com>
2019-06-11 12:44:15 +0100

    Snippets/woff2_compress: add --disable-transforms option

--- Snippets/woff2_compress.py
+++ Snippets/woff2_compress.py
@@ -4,13 +4,19 @@ from __future__ import print_function, division, absolute_import
 from fontTools.misc.py23 import *
 from fontTools.ttLib import TTFont
 from fontTools.ttx import makeOutputFileName
+from fontTools.ttLib.woff2 import WOFF2FlavorData
 import sys
-import os
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
+
+    disableTransforms = False
+    if "--disable-transforms" in args:
+        disableTransforms = True
+        args.remove("--disable-transforms")
+
     if len(args) < 1:
         print("One argument, the input filename, must be provided.", file=sys.stderr)
         return 1
@@ -22,6 +28,11 @@ def main(args=None):
 
     font = TTFont(filename, recalcBBoxes=False, recalcTimestamp=False)
     font.flavor = "woff2"
+
+    if disableTransforms:
+        # an empty tuple signals that we don't want any table to be transformed
+        font.flavorData = WOFF2FlavorData(transformedTables=())
+
     font.save(outfilename, reorderTables=False)
 
 
