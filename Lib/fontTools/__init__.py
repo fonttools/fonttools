bcba6abce5c0ee16e8083ba669314de4f947247d
Cosimo Lupo <clupo@google.com>
2019-08-02 11:41:09 +0100

    Bump version: 3.44.0 → 3.44.1.dev0

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -5,6 +5,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "3.44.0"
+version = __version__ = "3.44.1.dev0"
 
 __all__ = ["version", "log", "configLogger"]
--- setup.py
+++ setup.py
@@ -352,7 +352,7 @@ def find_data_files(manpath="share/man"):
 
 setup(
 	name="fonttools",
-	version="3.44.0",
+	version="3.44.1.dev0",
 	description="Tools to manipulate font files",
 	author="Just van Rossum",
 	author_email="just@letterror.com",
