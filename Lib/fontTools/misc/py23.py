1bc0aa0867aa08b6d4ac05dece7548d3d073f4f0
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2019-08-08 19:14:57 +0100

    Reinstate py23 module with Python 3 symbols, we want to deprecate it instead

--- Lib/fontTools/misc/py23.py
+++ Lib/fontTools/misc/py23.py
@@ -1,8 +1,61 @@
 """Python 2/3 compat layer leftovers."""
 
+import decimal as _decimal
+import math as _math
+import warnings
+from contextlib import redirect_stderr, redirect_stdout
+from io import BytesIO
+from io import StringIO as UnicodeIO
+from types import SimpleNamespace
+
+warnings.warn(
+    "The py23 module has been deprecated and will be removed in the next release. "
+    "Please update your code.",
+    DeprecationWarning,
+)
+
+__all__ = [
+    "basestring",
+    "unicode",
+    "unichr",
+    "byteord",
+    "bytechr",
+    "BytesIO",
+    "StringIO",
+    "UnicodeIO",
+    "strjoin",
+    "bytesjoin",
+    "tobytes",
+    "tostr",
+    "tounicode",
+    "Tag",
+    "open",
+    "range",
+    "xrange",
+    "round",
+    "Py23Error",
+    "SimpleNamespace",
+    "zip",
+    "RecursionError",
+]
+
+
+class Py23Error(NotImplementedError):
+    pass
+
+
+RecursionError = RecursionError
+StringIO = UnicodeIO
+
 basestring = str
+isclose = _math.isclose
+isfinite = _math.isfinite
+open = open
+range = range
+round = round3 = round
 unichr = chr
 unicode = str
+zip = zip
 
 
 def bytechr(n):
@@ -59,3 +112,37 @@ class Tag(str):
 
 def bytesjoin(iterable, joiner=b""):
     return tobytes(joiner).join(tobytes(item) for item in iterable)
+
+
+def xrange(*args, **kwargs):
+    raise Py23Error("'xrange' is not defined. Use 'range' instead.")
+
+
+def round2(number, ndigits=None):
+    """
+    Implementation of Python 2 built-in round() function.
+    Rounds a number to a given precision in decimal digits (default
+    0 digits). The result is a floating point number. Values are rounded
+    to the closest multiple of 10 to the power minus ndigits; if two
+    multiples are equally close, rounding is done away from 0.
+    ndigits may be negative.
+    See Python 2 documentation:
+    https://docs.python.org/2/library/functions.html?highlight=round#round
+    """
+    if ndigits is None:
+        ndigits = 0
+
+    if ndigits < 0:
+        exponent = 10 ** (-ndigits)
+        quotient, remainder = divmod(number, exponent)
+        if remainder >= exponent // 2 and number >= 0:
+            quotient += 1
+        return float(quotient * exponent)
+    else:
+        exponent = _decimal.Decimal("10") ** (-ndigits)
+
+        d = _decimal.Decimal.from_float(number).quantize(
+            exponent, rounding=_decimal.ROUND_HALF_UP
+        )
+
+        return float(d)
