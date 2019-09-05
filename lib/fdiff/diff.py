558bb8fb9f55960cc5b14db0ff2331468788bf29
Chris Simpkins <git.simpkins@gmail.com>
2019-09-04 21:02:21 -0400

    [fdiff.diff] add initial implementation of unified diff

---
+++ lib/fdiff/diff.py
@@ -0,0 +1,34 @@
+import os
+import tempfile
+
+from fontTools.ttLib import TTFont
+
+from fdiff.utils import get_file_modtime
+from fdiff.thirdparty.fdifflib import unified_diff
+
+
+def u_diff(filepath_a, filepath_b, context_lines):
+    tt_left = TTFont(filepath_a)
+    tt_right = TTFont(filepath_b)
+
+    fromdate = get_file_modtime(filepath_a)
+    todate = get_file_modtime(filepath_b)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tt_left.saveXML(os.path.join(tmpdirname, "left.ttx"))
+        tt_right.saveXML(os.path.join(tmpdirname, "right.ttx"))
+
+        with open(os.path.join(tmpdirname, "left.ttx")) as ff:
+            fromlines = ff.readlines()
+        with open(os.path.join(tmpdirname, "right.ttx")) as tf:
+            tolines = tf.readlines()
+
+        return unified_diff(
+            fromlines,
+            tolines,
+            filepath_a,
+            filepath_b,
+            fromdate,
+            todate,
+            n=context_lines,
+        )
