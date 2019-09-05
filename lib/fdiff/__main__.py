9f2c511404db424b8fd6d1b0ed4d17a89f6c7e2c
Chris Simpkins <git.simpkins@gmail.com>
2019-09-04 21:02:53 -0400

    [__main__] add support for unified diffs and unified color diffs

--- lib/fdiff/__main__.py
+++ lib/fdiff/__main__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import argparse
 
 from fdiff import __version__
+from fdiff.color import color_unified_diff_line
+from fdiff.diff import u_diff
+from fdiff.utils import file_exists
 
 
 def main():  # pragma: no cover
@@ -15,19 +19,40 @@ def run(argv):
     # ===========================================================
     # argparse command line argument definitions
     # ===========================================================
-    parser = argparse.ArgumentParser(
-        description="A font OpenType table diff tool"
-    )
+    parser = argparse.ArgumentParser(description="A font OpenType table diff tool")
     parser.add_argument(
         "--version", action="version", version="fdiff v{}".format(__version__)
     )
-    parser.add_argument('-c', action='store_true', default=False,
-                        help='ANSI escape code colored diff')
-    parser.add_argument('-l', '--lines', type=int, default=3,
-                        help='Number of context lines (default 3)')
-    parser.add_argument('PREFILE',
-                        help='Font file path 1')
-    parser.add_argument('POSTFILE',
-                        help='Font file path 2')
+    parser.add_argument(
+        "-c",
+        "--color",
+        action="store_true",
+        default=False,
+        help="ANSI escape code colored diff",
+    )
+    parser.add_argument(
+        "-l", "--lines", type=int, default=3, help="Number of context lines (default 3)"
+    )
+    parser.add_argument("PREFILE", help="Font file path 1")
+    parser.add_argument("POSTFILE", help="Font file path 2")
 
     args = parser.parse_args(argv)
+
+    if not file_exists(args.PREFILE):
+        sys.stderr.write(
+            f"[*] ERROR: The file path '{args.PREFILE}' can not be found.{os.linesep}"
+        )
+        sys.exit(1)
+    if not file_exists(args.POSTFILE):
+        sys.stderr.write(
+            f"[*] ERROR: The file path '{args.POSTFILE}' can not be found.{os.linesep}"
+        )
+        sys.exit(1)
+
+    diff = u_diff(args.PREFILE, args.POSTFILE, context_lines=args.lines)
+
+    if args.color:
+        for line in diff:
+            sys.stdout.write(color_unified_diff_line(line))
+    else:
+        sys.stdout.writelines(diff)
