9170bd5ac130baff40bbc38b6dbe1db53d5ec83e
Chris Simpkins <git.simpkins@gmail.com>
2019-09-07 20:55:53 -0400

    add textiter module with head and tail functions

---
+++ lib/fdiff/textiter.py
@@ -0,0 +1,12 @@
+from collections import deque
+from itertools import islice
+
+
+def head(iterable, n):
+    """Returns the first n indices of `iterable` as an iterable."""
+    return islice(iterable, n)
+
+
+def tail(iterable, n):
+    """Returns the last n indices of `iterable` as an iterable."""
+    return iter(deque(iterable, maxlen=n))
