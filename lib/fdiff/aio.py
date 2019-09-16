b998b42690f3c262f5d48d8c08d0afffe963360f
Chris Simpkins <git.simpkins@gmail.com>
2019-09-15 22:36:21 -0400

    add aio module for async I/O support

---
+++ lib/fdiff/aio.py
@@ -0,0 +1,7 @@
+import aiofiles
+
+
+async def async_write_bin(path, binary):
+    """Asynchronous IO writes of binary data `binary` to disk on the file path `path`"""
+    async with aiofiles.open(path, "wb") as f:
+        await f.write(binary)
