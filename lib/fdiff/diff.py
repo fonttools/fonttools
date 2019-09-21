0ce85656b0b9fc3499d2f15e06aae986152e158b
Chris Simpkins <git.simpkins@gmail.com>
2019-09-21 15:41:41 -0400

    [fdiff.diff] major refactor, add new external_diff function
    Adds library support for external executables as the source of diff output with new --external command line flag

--- lib/fdiff/diff.py
+++ lib/fdiff/diff.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
 from multiprocessing import Pool, cpu_count
+import shlex
+import subprocess
 import tempfile
 
 from fontTools.ttLib import TTFont
@@ -14,10 +16,147 @@ from fdiff.thirdparty.fdifflib import unified_diff
 from fdiff.utils import get_file_modtime
 
 
+#
+#
+#  Private functions
+#
+#
+
+
+def _async_fetch_files(dirpath, urls):
+    loop = asyncio.get_event_loop()
+    tasks = loop.run_until_complete(
+        create_async_get_request_session_and_run(urls, dirpath)
+    )
+    for task in tasks:
+        if task.exception():
+            # raise exception here to notify calling code that something
+            # did not work
+            raise AIOError(f"{task.exception()}")
+        elif task.result().http_status != 200:
+            # handle non-200 HTTP response status codes + file write fails
+            raise AIOError(
+                f"failed to pull '{task.result().url}' with HTTP status code {task.result().http_status}"
+            )
+
+
+def _get_fonts_and_save_xml(
+    filepath_a, filepath_b, tmpdirpath, include_tables, exclude_tables, use_multiprocess
+):
+    post_pathname, postpath, pre_pathname, prepath = _get_pre_post_paths(
+        filepath_a, filepath_b, tmpdirpath
+    )
+    # instantiate left and right fontTools.ttLib.TTFont objects
+    tt_left = TTFont(prepath)
+    tt_right = TTFont(postpath)
+    _validate_table_includes(include_tables, tt_left, tt_right)
+    _validate_table_excludes(exclude_tables, tt_left, tt_right)
+    left_ttxpath = os.path.join(tmpdirpath, "left.ttx")
+    right_ttxpath = os.path.join(tmpdirpath, "right.ttx")
+    _mp_save_ttx_xml(
+        tt_left,
+        tt_right,
+        left_ttxpath,
+        right_ttxpath,
+        exclude_tables,
+        include_tables,
+        use_multiprocess,
+    )
+    return left_ttxpath, right_ttxpath, pre_pathname, prepath, post_pathname, postpath
+
+
+def _get_pre_post_paths(filepath_a, filepath_b, dirpath):
+    urls = []
+    if filepath_a.startswith("http"):
+        urls.append(filepath_a)
+        prepath = _get_filepath_from_url(filepath_a, dirpath)
+        # keep URL as path name for remote file requests
+        pre_pathname = filepath_a
+    else:
+        prepath = filepath_a
+        pre_pathname = filepath_a
+    if filepath_b.startswith("http"):
+        urls.append(filepath_b)
+        postpath = _get_filepath_from_url(filepath_b, dirpath)
+        # keep URL as path name for remote file requests
+        post_pathname = filepath_b
+    else:
+        postpath = filepath_b
+        post_pathname = filepath_b
+    # Async IO fetch and write of any remote file requests
+    if len(urls) > 0:
+        _async_fetch_files(dirpath, urls)
+    return post_pathname, postpath, pre_pathname, prepath
+
+
+def _mp_save_ttx_xml(
+    tt_left,
+    tt_right,
+    left_ttxpath,
+    right_ttxpath,
+    exclude_tables,
+    include_tables,
+    use_multiprocess,
+):
+    if use_multiprocess and cpu_count() > 1:
+        # Use parallel fontTools.ttLib.TTFont.saveXML dump
+        # by default on multi CPU systems.  This is a performance
+        # optimization. Profiling demonstrates that this can reduce
+        # execution time by up to 30% for some fonts
+        mp_args_list = [
+            (tt_left, left_ttxpath, include_tables, exclude_tables),
+            (tt_right, right_ttxpath, include_tables, exclude_tables),
+        ]
+        with Pool(processes=2) as pool:
+            pool.starmap(_ttfont_save_xml, mp_args_list)
+    else:
+        # use sequential fontTools.ttLib.TTFont.saveXML dumps
+        # when use_multiprocess is False or single CPU system
+        # detected
+        _ttfont_save_xml(tt_left, left_ttxpath, include_tables, exclude_tables)
+        _ttfont_save_xml(tt_right, right_ttxpath, include_tables, exclude_tables)
+
+
 def _ttfont_save_xml(ttf, filepath, include_tables, exclude_tables):
     """Writes TTX specification formatted XML to disk on filepath."""
     ttf.saveXML(filepath, tables=include_tables, skipTables=exclude_tables)
     return True
+
+
+def _validate_table_excludes(exclude_tables, tt_left, tt_right):
+    # Validation: exclude_tables request should be for tables that are in one of
+    # the two fonts.  Mis-specified OT table definitions could otherwise result
+    # in the presence of a table in the diff when the request was to exclude it.
+    # For example, when an "OS/2" table request is entered as "OS2".
+    if exclude_tables is not None:
+        for table in exclude_tables:
+            if table not in tt_left and table not in tt_right:
+                raise KeyError(
+                    f"'{table}' table was not identified for exclusion in either font"
+                )
+
+
+def _validate_table_includes(include_tables, tt_left, tt_right):
+    # Validation: include_tables request should be for tables that are in one of
+    # the two fonts. This otherwise silently passes with exit status code 0 which
+    # could lead to the interpretation of no diff between two files when the table
+    # entry is incorrectly defined or is a typo.  Let's be conservative and consider
+    # this an error, force user to use explicit definitions that include tables in
+    # one of the two files, and understand that the diff request was for one or more
+    # tables that are not present.
+    if include_tables is not None:
+        for table in include_tables:
+            if table not in tt_left and table not in tt_right:
+                raise KeyError(
+                    f"'{table}' table was not identified for inclusion in either font"
+                )
+
+
+#
+#
+#  Public functions
+#
+#
 
 
 def u_diff(
@@ -46,102 +185,25 @@ def u_diff(
     :raises: fdiff.exceptions.AIOError if exception raised during execution of async I/O
              GET request for URL or file write
     :raises: fdiff.exceptions.AIOError if GET request to URL returned non-200 response status code"""
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    with tempfile.TemporaryDirectory() as tmpdirpath:
         # define the file paths with either local file requests
-        # or pulls of remote files based on the command line request
-        urls = []
-        if filepath_a.startswith("http"):
-            urls.append(filepath_a)
-            prepath = _get_filepath_from_url(filepath_a, tmpdirname)
-            # keep URL as path name for remote file requests
-            pre_pathname = filepath_a
-        else:
-            prepath = filepath_a
-            pre_pathname = filepath_a
-
-        if filepath_b.startswith("http"):
-            urls.append(filepath_b)
-            postpath = _get_filepath_from_url(filepath_b, tmpdirname)
-            # keep URL as path name for remote file requests
-            post_pathname = filepath_b
-        else:
-            postpath = filepath_b
-            post_pathname = filepath_b
-
-        # Async IO fetch and write of any remote file requests
-        if len(urls) > 0:
-            loop = asyncio.get_event_loop()
-            tasks = loop.run_until_complete(
-                create_async_get_request_session_and_run(urls, tmpdirname)
-            )
-            for task in tasks:
-                if task.exception():
-                    # raise exception here to notify calling code that something
-                    # did not work
-                    raise AIOError(f"{task.exception()}")
-                elif task.result().http_status != 200:
-                    # handle non-200 HTTP response status codes + file write fails
-                    raise AIOError(
-                        f"failed to pull '{task.result().url}' with HTTP status code {task.result().http_status}"
-                    )
-
-        # instantiate left and right fontTools.ttLib.TTFont objects
-        tt_left = TTFont(prepath)
-        tt_right = TTFont(postpath)
-
-        # Validation: include_tables request should be for tables that are in one of
-        # the two fonts. This otherwise silently passes with exit status code 0 which
-        # could lead to the interpretation of no diff between two files when the table
-        # entry is incorrectly defined or is a typo.  Let's be conservative and consider
-        # this an error, force user to use explicit definitions that include tables in
-        # one of the two files, and understand that the diff request was for one or more
-        # tables that are not present.
-        if include_tables is not None:
-            for table in include_tables:
-                if table not in tt_left and table not in tt_right:
-                    raise KeyError(
-                        f"'{table}' table was not identified for inclusion in either font"
-                    )
-
-        # Validation: exclude_tables request should be for tables that are in one of
-        # the two fonts.  Mis-specified OT table definitions could otherwise result
-        # in the presence of a table in the diff when the request was to exclude it.
-        # For example, when an "OS/2" table request is entered as "OS2".
-        if exclude_tables is not None:
-            for table in exclude_tables:
-                if table not in tt_left and table not in tt_right:
-                    raise KeyError(
-                        f"'{table}' table was not identified for exclusion in either font"
-                    )
-
-        fromdate = get_file_modtime(prepath)
-        todate = get_file_modtime(postpath)
-
-        left_ttxpath = os.path.join(tmpdirname, "left.ttx")
-        right_ttxpath = os.path.join(tmpdirname, "right.ttx")
-
-        if use_multiprocess and cpu_count() > 1:
-            # Use parallel fontTools.ttLib.TTFont.saveXML dump
-            # by default on multi CPU systems.  This is a performance
-            # optimization. Profiling demonstrates that this can reduce
-            # execution time by up to 30% for some fonts
-            mp_args_list = [
-                (tt_left, left_ttxpath, include_tables, exclude_tables),
-                (tt_right, right_ttxpath, include_tables, exclude_tables),
-            ]
-            with Pool(processes=2) as pool:
-                pool.starmap(_ttfont_save_xml, mp_args_list)
-        else:
-            # use sequential fontTools.ttLib.TTFont.saveXML dumps
-            # when use_multiprocess is False or single CPU system
-            # detected
-            _ttfont_save_xml(tt_left, left_ttxpath, include_tables, exclude_tables)
-            _ttfont_save_xml(tt_right, right_ttxpath, include_tables, exclude_tables)
+        # or HTTP GET requests of remote files based on the command line request
+        left_ttxpath, right_ttxpath, pre_pathname, prepath, post_pathname, postpath = _get_fonts_and_save_xml(
+            filepath_a,
+            filepath_b,
+            tmpdirpath,
+            include_tables,
+            exclude_tables,
+            use_multiprocess,
+        )
 
         with open(left_ttxpath) as ff:
             fromlines = ff.readlines()
         with open(right_ttxpath) as tf:
             tolines = tf.readlines()
+
+        fromdate = get_file_modtime(prepath)
+        todate = get_file_modtime(postpath)
 
         return unified_diff(
             fromlines,
@@ -152,3 +214,46 @@ def u_diff(
             todate,
             n=context_lines,
         )
+
+
+def external_diff(
+    command,
+    filepath_a,
+    filepath_b,
+    include_tables=None,
+    exclude_tables=None,
+    use_multiprocess=True,
+):
+    # TODO: Add docstring documentation
+    with tempfile.TemporaryDirectory() as tmpdirpath:
+        # define the file paths with either local file requests
+        # or HTTP GET requests of remote files based on the command line request
+        left_ttxpath, right_ttxpath, pre_pathname, prepath, post_pathname, postpath = _get_fonts_and_save_xml(
+            filepath_a,
+            filepath_b,
+            tmpdirpath,
+            include_tables,
+            exclude_tables,
+            use_multiprocess,
+        )
+
+        full_command = f"{command.strip()} {left_ttxpath} {right_ttxpath}"
+
+        process = subprocess.Popen(
+            shlex.split(full_command),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf8",
+        )
+
+        while True:
+            output = process.stdout.readline()
+            exit_status = process.poll()
+            if len(output) == 0 and exit_status is not None:
+                err = process.stderr.read()
+                if err:
+                    raise AIOError(err)
+                yield output, exit_status
+                break
+            else:
+                yield output, None
