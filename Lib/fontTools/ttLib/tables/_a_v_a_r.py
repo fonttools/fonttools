8791caf3b8ac8f01946f7b9266fbb1271cf06b3f
Cosimo Lupo <clupo@google.com>
2019-10-07 15:41:06 +0100

    _a_v_a_r: use floatToFixedToStr and strToFixedToFloat in toXML/fromXML methods

--- Lib/fontTools/ttLib/tables/_a_v_a_r.py
+++ Lib/fontTools/ttLib/tables/_a_v_a_r.py
@@ -1,7 +1,12 @@
 from fontTools.misc.py23 import *
 from fontTools import ttLib
 from fontTools.misc import sstruct
-from fontTools.misc.fixedTools import fixedToFloat, floatToFixed
+from fontTools.misc.fixedTools import (
+    fixedToFloat as fi2fl,
+    floatToFixed as fl2fi,
+    floatToFixedToStr as fl2str,
+    strToFixedToFloat as str2fl,
+)
 from fontTools.misc.textTools import safeEval
 from fontTools.ttLib import TTLibError
 from . import DefaultTable
@@ -46,8 +51,8 @@ class table__a_v_a_r(DefaultTable.DefaultTable):
             mappings = sorted(self.segments[axis].items())
             result.append(struct.pack(">H", len(mappings)))
             for key, value in mappings:
-                fixedKey = floatToFixed(key, 14)
-                fixedValue = floatToFixed(value, 14)
+                fixedKey = fl2fi(key, 14)
+                fixedValue = fl2fi(value, 14)
                 result.append(struct.pack(">hh", fixedKey, fixedValue))
         return bytesjoin(result)
 
@@ -66,7 +71,7 @@ class table__a_v_a_r(DefaultTable.DefaultTable):
             pos = pos + 2
             for _ in range(numPairs):
                 fromValue, toValue = struct.unpack(">hh", data[pos:pos+4])
-                segments[fixedToFloat(fromValue, 14)] = fixedToFloat(toValue, 14)
+                segments[fi2fl(fromValue, 14)] = fi2fl(toValue, 14)
                 pos = pos + 4
 
     def toXML(self, writer, ttFont):
@@ -75,10 +80,8 @@ class table__a_v_a_r(DefaultTable.DefaultTable):
             writer.begintag("segment", axis=axis)
             writer.newline()
             for key, value in sorted(self.segments[axis].items()):
-                # roundtrip float -> fixed -> float to normalize TTX output
-                # as dumped after decompiling or straight from varLib
-                key = fixedToFloat(floatToFixed(key, 14), 14)
-                value = fixedToFloat(floatToFixed(value, 14), 14)
+                key = fl2str(key, 14)
+                value = fl2str(value, 14)
                 writer.simpletag("mapping", **{"from": key, "to": value})
                 writer.newline()
             writer.endtag("segment")
@@ -92,8 +95,8 @@ class table__a_v_a_r(DefaultTable.DefaultTable):
                 if isinstance(element, tuple):
                     elementName, elementAttrs, _ = element
                     if elementName == "mapping":
-                        fromValue = safeEval(elementAttrs["from"])
-                        toValue = safeEval(elementAttrs["to"])
+                        fromValue = str2fl(elementAttrs["from"], 14)
+                        toValue = str2fl(elementAttrs["to"], 14)
                         if fromValue in segment:
                             log.warning("duplicate entry for %s in axis '%s'",
                                         fromValue, axis)
