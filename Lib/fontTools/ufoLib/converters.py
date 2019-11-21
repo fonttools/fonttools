1e32ef7b0354b45f6420a9c8d0981916b05d4466
Jens Kutilek <webmail@kutilek.de>
2019-11-21 13:06:45 +0100

    Add default value

--- Lib/fontTools/ufoLib/converters.py
+++ Lib/fontTools/ufoLib/converters.py
@@ -6,7 +6,7 @@ Conversion functions.
 
 # adapted from the UFO spec
 
-def convertUFO1OrUFO2KerningToUFO3Kerning(kerning, groups, glyphSet):
+def convertUFO1OrUFO2KerningToUFO3Kerning(kerning, groups, glyphSet=set()):
     # gather known kerning groups based on the prefixes
     firstReferencedGroups, secondReferencedGroups = findKnownKerningGroups(groups)
     # Make lists of groups referenced in kerning pairs.
