fa77aac63c4e41c915c167628ccef4d10b2c1ecd
Cosimo Lupo <clupo@google.com>
2020-01-29 17:39:47 +0000

    Release 4.2.5

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -4,6 +4,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "4.2.5.dev0"
+version = __version__ = "4.2.5"
 
 __all__ = ["version", "log", "configLogger"]
