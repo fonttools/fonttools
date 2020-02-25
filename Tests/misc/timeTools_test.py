edbfe95f45974c206fb247a178eaf883e3b22e57
Christof Kaufmann <christofkaufmann.dev@gmail.com>
2020-02-25 20:36:02 +0100

    Add test for non-localized date parsing

--- Tests/misc/timeTools_test.py
+++ Tests/misc/timeTools_test.py
@@ -1,7 +1,8 @@
 from fontTools.misc.py23 import *
-from fontTools.misc.timeTools import asctime, timestampNow, epoch_diff
+from fontTools.misc.timeTools import asctime, timestampNow, timestampToString, timestampFromString, epoch_diff
 import os
 import time
+import locale
 import pytest
 
 
@@ -21,3 +22,17 @@ def test_source_date_epoch():
 
     del os.environ["SOURCE_DATE_EPOCH"]
     assert timestampNow() + epoch_diff != 150687315
+
+
+# test for issue #1838
+def test_date_parsing_with_locale():
+    l = locale.getlocale(locale.LC_TIME)
+    try:
+        locale.setlocale(locale.LC_TIME, 'de_DE.utf8')
+    except locale.Error:
+        pytest.skip("Locale de_DE not available")
+
+    try:
+        assert timestampFromString(timestampToString(timestampNow()))
+    finally:
+        locale.setlocale(locale.LC_TIME, l)
