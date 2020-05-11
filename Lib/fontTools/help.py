9e43e71124aad5646da4ca4ff31147d697f96727
Simon Cozens <simon@simon-cozens.org>
2020-05-11 11:34:51 +0100

    Address feedback

--- Lib/fontTools/help.py
+++ Lib/fontTools/help.py
@@ -5,42 +5,26 @@ import importlib
 import os
 from pathlib import Path
 
-def _discover_path_importables(pkg_pth, pkg_name):
-    """Yield all importables under a given path and package."""
-    for dir_path, _d, file_names in os.walk(pkg_pth):
-        pkg_dir_path = Path(dir_path)
-
-        if pkg_dir_path.parts[-1] == "__pycache__":
-            continue
-
-        if all(Path(_).suffix != ".py" for _ in file_names):
-            continue
-
-        rel_pt = pkg_dir_path.relative_to(pkg_pth)
-        pkg_pref = ".".join((pkg_name,) + rel_pt.parts)
-        yield from (
-            pkg_path
-            for _, pkg_path, _ in pkgutil.walk_packages(
-                (str(pkg_dir_path),), prefix=f"{pkg_pref}.",
-            )
-        )
-
 
 def main():
     """Show this help"""
     path = fontTools.__path__
     descriptions = {}
     for pkg in sorted(
-        set(_discover_path_importables(fontTools.__path__[0], "fontTools"))
+        mod.name for mod in pkgutil.walk_packages([fontTools.__path__[0]], prefix="fontTools.")
     ):
         try:
-            description = __import__(
+            imports = __import__(
                 pkg, globals(), locals(), ["main"]
-            ).main.__doc__
+            )
+        except ImportError as e:
+            continue
+        try:
+            description = imports.main.__doc__
             if description:
                 pkg = pkg[10:].replace(".__main__", "")
                 descriptions[pkg] = description
-        except Exception as e:
+        except AttributeError as e:
             pass
     for pkg, description in descriptions.items():
         print("fonttools %-12s %s" % (pkg, description), file=sys.stderr)
