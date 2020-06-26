bed917273de37d19b0f04ebc218759df50f1de12
Jens Kutilek <webmail@kutilek.de>
2020-06-26 11:28:07 +0200

    Add HashPointPen from psautohint

---
+++ Lib/fontTools/pens/hashPointPen.py
@@ -0,0 +1,52 @@
+# Modified from https://github.com/adobe-type-tools/psautohint/blob/08b346865710ed3c172f1eb581d6ef243b203f99/python/psautohint/ufoFont.py#L800-L838
+import hashlib
+from fontTools.pens.pointPen import AbstractPointPen
+
+
+class HashPointPen(AbstractPointPen):
+    DEFAULT_TRANSFORM = (1, 0, 0, 1, 0, 0)
+
+    def __init__(self, glyph, glyphSet=None):
+        self.glyphset = glyphSet
+        self.width = round(getattr(glyph, "width", 1000), 9)
+        self.data = ["w%s" % self.width]
+
+    def getHash(self):
+        data = "".join(self.data)
+        if len(data) >= 128:
+            data = hashlib.sha512(data.encode("ascii")).hexdigest()
+        return data
+
+    def beginPath(self, identifier=None, **kwargs):
+        pass
+
+    def endPath(self):
+        pass
+
+    def addPoint(
+        self,
+        pt,
+        segmentType=None,
+        smooth=False,
+        name=None,
+        identifier=None,
+        **kwargs
+    ):
+        if segmentType is None:
+            pt_type = ""
+        else:
+            pt_type = segmentType[0]
+        self.data.append("%s%s%s" % (pt_type, repr(pt[0]), repr(pt[1])))
+
+    def addComponent(
+        self, baseGlyphName, transformation, identifier=None, **kwargs
+    ):
+        self.data.append("base:%s" % baseGlyphName)
+
+        for i, v in enumerate(transformation):
+            if transformation[i] != self.DEFAULT_TRANSFORM[i]:
+                self.data.append(str(round(v, 9)))
+
+        self.data.append("w%s" % self.width)
+        glyph = self.glyphset[baseGlyphName]
+        glyph.drawPoints(self)
