fdf11b4be45500111daad37e5fa115ef88faa734
Simon Cozens <simon@simon-cozens.org>
2020-07-02 13:37:32 +0100

    Use NamedTuple for location

--- Lib/fontTools/feaLib/location.py
+++ Lib/fontTools/feaLib/location.py
@@ -1,9 +1,10 @@
-class FeatureLibLocation(object):
+from typing import NamedTuple
+
+class FeatureLibLocation(NamedTuple):
     """A location in a feature file"""
-    def __init__(self, file, line, column):
-        self.file = file
-        self.line = line
-        self.column = column
+    file: str
+    line: int
+    column: int
 
     def __str__(self):
     		return f"{self.file}:{self.line}:{self.column}"
