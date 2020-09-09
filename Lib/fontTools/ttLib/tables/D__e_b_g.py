98803b7bf8cc62fdda70bfd4bbee3d3f81bb90a2
Simon Cozens <simon@simon-cozens.org>
2020-09-09 19:56:55 +0100

    Reformat, tidy from review comments

--- Lib/fontTools/ttLib/tables/D__e_b_g.py
+++ Lib/fontTools/ttLib/tables/D__e_b_g.py
@@ -1,16 +1,17 @@
-from . import DefaultTable
 import json
 
+from . import DefaultTable
+
+
 class table_D__e_b_g(DefaultTable.DefaultTable):
+    def decompile(self, data, ttFont):
+        self.data = json.loads(data)
 
-	def decompile(self, data, ttFont):
-		self.data = json.loads(data)
+    def compile(self, ttFont):
+        return json.dumps(self.data).encode("utf-8")
 
-	def compile(self, ttFont):
-		return str.encode(json.dumps(self.data))
+    def toXML(self, writer, ttFont):
+        writer.writecdata(self.data)
 
-	def toXML(self, writer, ttFont):
-		writer.writecdata(self.compile(ttFont).decode())
-
-	def fromXML(self, name, attrs, content, ttFont):
-		self.data = json.loads(content)
+    def fromXML(self, name, attrs, content, ttFont):
+        self.data = json.loads(content)
