3652d73d87c796dff9c45ab14ed09e318da61806
Jens Kutilek <webmail@kutilek.de>
2020-10-21 12:20:33 +0200

    Adapt tests to new pen api

--- Tests/pens/hashPointPen_test.py
+++ Tests/pens/hashPointPen_test.py
@@ -60,13 +60,13 @@ class _TestGlyph5(_TestGlyph):
 
 class HashPointPenTest(object):
     def test_addComponent(self):
-        pen = HashPointPen(_TestGlyph(), {"a": _TestGlyph()})
+        pen = HashPointPen(_TestGlyph().width, {"a": _TestGlyph()})
         pen.addComponent("a", (2, 0, 0, 3, -10, 5))
         assert pen.hash == "w500[l0+0l10+110o50+75o60+50c50+0|(+2+0+0+3-10+5)]"
 
     def test_NestedComponents(self):
         pen = HashPointPen(
-            _TestGlyph(), {"a": _TestGlyph5(), "b": _TestGlyph()}
+            _TestGlyph().width, {"a": _TestGlyph5(), "b": _TestGlyph()}
         )  # "a" contains "b" as a component
         pen.addComponent("a", (2, 0, 0, 3, -10, 5))
 
@@ -76,7 +76,7 @@ class HashPointPenTest(object):
         )
 
     def test_outlineAndComponent(self):
-        pen = HashPointPen(_TestGlyph(), {"a": _TestGlyph()})
+        pen = HashPointPen(_TestGlyph().width, {"a": _TestGlyph()})
         glyph = _TestGlyph()
         glyph.drawPoints(pen)
         pen.addComponent("a", (2, 0, 0, 2, -10, 5))
@@ -87,13 +87,13 @@ class HashPointPenTest(object):
         )
 
     def test_addComponent_missing_raises(self):
-        pen = HashPointPen(_TestGlyph(), dict())
+        pen = HashPointPen(_TestGlyph().width, dict())
         with pytest.raises(KeyError) as excinfo:
             pen.addComponent("a", Identity)
         assert excinfo.value.args[0] == "a"
 
     def test_similarGlyphs(self):
-        pen = HashPointPen(_TestGlyph())
+        pen = HashPointPen(_TestGlyph().width)
         glyph = _TestGlyph()
         glyph.drawPoints(pen)
 
@@ -104,22 +104,22 @@ class HashPointPenTest(object):
         assert pen.hash != pen2.hash
 
     def test_similarGlyphs2(self):
-        pen = HashPointPen(_TestGlyph())
+        pen = HashPointPen(_TestGlyph().width)
         glyph = _TestGlyph()
         glyph.drawPoints(pen)
 
-        pen2 = HashPointPen(_TestGlyph3())
+        pen2 = HashPointPen(_TestGlyph3().width)
         glyph = _TestGlyph3()
         glyph.drawPoints(pen2)
 
         assert pen.hash != pen2.hash
 
     def test_similarGlyphs3(self):
-        pen = HashPointPen(_TestGlyph())
+        pen = HashPointPen(_TestGlyph().width)
         glyph = _TestGlyph()
         glyph.drawPoints(pen)
 
-        pen2 = HashPointPen(_TestGlyph4())
+        pen2 = HashPointPen(_TestGlyph4().width)
         glyph = _TestGlyph4()
         glyph.drawPoints(pen2)
 
@@ -128,10 +128,10 @@ class HashPointPenTest(object):
     def test_glyphVsComposite(self):
         # If a glyph contains a component, the decomposed glyph should still
         # compare false
-        pen = HashPointPen(_TestGlyph(), {"a": _TestGlyph()})
+        pen = HashPointPen(_TestGlyph().width, {"a": _TestGlyph()})
         pen.addComponent("a", Identity)
 
-        pen2 = HashPointPen(_TestGlyph())
+        pen2 = HashPointPen(_TestGlyph().width)
         glyph = _TestGlyph()
         glyph.drawPoints(pen2)
 
