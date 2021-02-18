fdceeee0c6592fa02a56412803a3bd516af1fd33
Simon Cozens <simon@simon-cozens.org>
2021-02-18 11:39:46 +0000

    Unnecessary parens

--- Lib/fontTools/misc/bezierTools.py
+++ Lib/fontTools/misc/bezierTools.py
@@ -951,7 +951,7 @@ def _curve_line_intersections_t(curve, line):
         intersections.extend(solveCubic(a[1], b[1], c[1], d[1]))
     else:
         raise ValueError("Unknown curve degree")
-    return sorted([i for i in intersections if (0.0 <= i <= 1)])
+    return sorted([i for i in intersections if 0.0 <= i <= 1])
 
 
 def curveLineIntersections(curve, line):
