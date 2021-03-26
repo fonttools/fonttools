0320f701fff4aef3e6eea02144bd55e20a3ecafb
Jens Kutilek <webmail@kutilek.de>
2021-03-26 16:50:20 +0100

    Change test to pytest

--- Tests/pens/ttGlyphPointPen_test.py
+++ Tests/pens/ttGlyphPointPen_test.py
@@ -1,13 +1,12 @@
 import os
-import unittest
+import pytest
 import struct
 
 from fontTools import ttLib
-from fontTools.misc.testTools import TestCase
 from fontTools.pens.ttGlyphPen import TTGlyphPointPen, MAX_F2DOT14
 
 
-class TTGlyphPointPenTest(TestCase):
+class TTGlyphPointPenTest:
 
     def runEndToEnd(self, filename):
         font = ttLib.TTFont()
@@ -29,8 +28,7 @@ class TTGlyphPointPenTest(TestCase):
             if hasattr(oldGlyph, 'program'):
                 newGlyph.program = oldGlyph.program
 
-            self.assertEqual(
-                oldGlyph.compile(glyfTable), newGlyph.compile(glyfTable))
+            assert oldGlyph.compile(glyfTable) == newGlyph.compile(glyfTable)
 
     def test_e2e_linesAndSimpleComponents(self):
         self.runEndToEnd('TestTTF-Regular.ttx')
@@ -47,35 +45,35 @@ class TTGlyphPointPenTest(TestCase):
         pen.addPoint((50, 700), "line")
         pen.endPath()
         glyph = pen.glyph()
-        self.assertEqual(glyph.numberOfContours, 1)
-        self.assertEqual(glyph.endPtsOfContours, [3])
+        assert glyph.numberOfContours == 1
+        assert glyph.endPtsOfContours == [3]
 
     def test_glyph_errorOnCurve(self):
         pen = TTGlyphPointPen(None)
         pen.beginPath()
-        with self.assertRaises(NotImplementedError):
+        with pytest.raises(NotImplementedError):
             pen.addPoint((0, 0), "curve")
 
     def test_beginPath_beginPathOnOpenPath(self):
         pen = TTGlyphPointPen(None)
         pen.beginPath()
         pen.addPoint((0, 0))
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             pen.beginPath()
 
     def test_closePath_ignoresAnchors(self):
         pen = TTGlyphPointPen(None)
         pen.beginPath()
         pen.endPath()
-        self.assertFalse(pen.points)
-        self.assertFalse(pen.types)
-        self.assertFalse(pen.endPts)
+        assert not pen.points
+        assert not pen.types
+        assert not pen.endPts
 
     def test_glyph_errorOnUnendedContour(self):
         pen = TTGlyphPointPen(None)
         pen.beginPath()
         pen.addPoint((0, 0))
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             pen.glyph()
 
     def test_glyph_decomposes(self):
@@ -111,7 +109,7 @@ class TTGlyphPointPenTest(TestCase):
         pen.endPath()
         plainGlyph = pen.glyph()
 
-        self.assertEqual(plainGlyph, compositeGlyph)
+        assert plainGlyph == compositeGlyph
 
     # def test_remove_extra_move_points(self):
     #     pen = TTGlyphPointPen(None)
@@ -119,8 +117,8 @@ class TTGlyphPointPenTest(TestCase):
     #     pen.lineTo((100, 0))
     #     pen.qCurveTo((100, 50), (50, 100), (0, 0))
     #     pen.closePath()
-    #     self.assertEqual(len(pen.points), 4)
-    #     self.assertEqual(pen.points[0], (0, 0))
+    #     assert len(pen.points) == 4
+    #     assert pen.points[0] == (0, 0)
 
     # def test_keep_move_point(self):
     #     pen = TTGlyphPointPen(None)
@@ -129,8 +127,8 @@ class TTGlyphPointPenTest(TestCase):
     #     pen.qCurveTo((100, 50), (50, 100), (30, 30))
     #     # when last and move pts are different, closePath() implies a lineTo
     #     pen.closePath()
-    #     self.assertEqual(len(pen.points), 5)
-    #     self.assertEqual(pen.points[0], (0, 0))
+    #     assert len(pen.points) == 5
+    #     assert pen.points[0] == (0, 0)
 
     # def test_keep_duplicate_end_point(self):
     #     pen = TTGlyphPointPen(None)
@@ -139,8 +137,8 @@ class TTGlyphPointPenTest(TestCase):
     #     pen.qCurveTo((100, 50), (50, 100), (0, 0))
     #     pen.lineTo((0, 0))  # the duplicate point is not removed
     #     pen.closePath()
-    #     self.assertEqual(len(pen.points), 5)
-    #     self.assertEqual(pen.points[0], (0, 0))
+    #     assert len(pen.points) == 5
+    #     assert pen.points[0] == (0, 0)
 
     def test_within_range_component_transform(self):
         componentName = 'a'
@@ -162,7 +160,7 @@ class TTGlyphPointPenTest(TestCase):
         pen.addComponent(componentName, (1, 0, 0, -1.5, 0, 0))
         expectedGlyph = pen.glyph()
 
-        self.assertEqual(expectedGlyph, compositeGlyph)
+        assert expectedGlyph == compositeGlyph
 
     def test_clamp_to_almost_2_component_transform(self):
         componentName = 'a'
@@ -191,7 +189,7 @@ class TTGlyphPointPenTest(TestCase):
         pen.addComponent(componentName, (-2, 0, 0, -2, 0, 0))
         expectedGlyph = pen.glyph()
 
-        self.assertEqual(expectedGlyph, compositeGlyph)
+        assert expectedGlyph == compositeGlyph
 
     def test_out_of_range_transform_decomposed(self):
         componentName = 'a'
@@ -227,7 +225,7 @@ class TTGlyphPointPenTest(TestCase):
         pen.endPath()
         expectedGlyph = pen.glyph()
 
-        self.assertEqual(expectedGlyph, compositeGlyph)
+        assert expectedGlyph == compositeGlyph
 
     def test_no_handle_overflowing_transform(self):
         componentName = 'a'
@@ -245,14 +243,13 @@ class TTGlyphPointPenTest(TestCase):
         pen.addComponent(componentName, (3, 0, 0, 1, 0, 0))
         compositeGlyph = pen.glyph()
 
-        self.assertEqual(compositeGlyph.components[0].transform,
-                         ((3, 0), (0, 1)))
+        assert compositeGlyph.components[0].transform == ((3, 0), (0, 1))
 
-        with self.assertRaises(struct.error):
+        with pytest.raises(struct.error):
             compositeGlyph.compile({'a': baseGlyph})
 
     def assertGlyphBoundsEqual(self, glyph, bounds):
-        self.assertEqual((glyph.xMin, glyph.yMin, glyph.xMax, glyph.yMax), bounds)
+        assert (glyph.xMin, glyph.yMin, glyph.xMax, glyph.yMax) == bounds
 
     def test_round_float_coordinates_and_component_offsets(self):
         glyphSet = {}
@@ -316,8 +313,3 @@ class _TestGlyph(object):
         for point in self.coordinates:
             pen.addPoint(point, "line")
         pen.endPath()
-
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(unittest.main())
