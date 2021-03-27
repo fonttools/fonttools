a199a038cfd5c7fea3545a3c4ef16336ee4a8de5
justvanrossum <justvanrossum@gmail.com>
2021-03-27 21:50:30 +0100

    minor py23 cleanups

--- Lib/fontTools/misc/symfont.py
+++ Lib/fontTools/misc/symfont.py
@@ -111,8 +111,7 @@ MomentXYPen = partial(GreenPen, func=x*y)
 def printGreenPen(penName, funcs, file=sys.stdout):
 
 	print(
-'''from fontTools.misc.py23 import *
-from fontTools.pens.basePen import BasePen
+'''from fontTools.pens.basePen import BasePen
 
 class %s(BasePen):
 
