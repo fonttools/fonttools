cbe2e9a16d3cb006a77d96b8c7bed7987590401f
Cosimo Lupo <clupo@google.com>
2021-04-26 14:42:17 +0100

    Bump version: 4.22.1 → 4.22.2.dev0

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -3,6 +3,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "4.22.1"
+version = __version__ = "4.22.2.dev0"
 
 __all__ = ["version", "log", "configLogger"]
