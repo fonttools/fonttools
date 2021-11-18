d203a784f8f123477c02a306ed2cc6114b4744cc
Simon Cozens <simon@simon-cozens.org>
2021-11-18 10:27:42 +0000

    We don’t use our own static directory

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -104,7 +104,7 @@ html_theme_options = {"display_version": False}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 html_favicon = "assets/img/favicon.ico"
 
