42993ae6917f8f6e4c31f4be123caca24d27d2da
Simon Cozens <simon@simon-cozens.org>
2022-03-07 10:36:40 +0000

    Revert "Fixup test expectations"
    This reverts commit 7db13c9872884772312727e3478fb36ed9883004.

--- Tests/feaLib/builder_test.py
+++ Tests/feaLib/builder_test.py
@@ -78,7 +78,7 @@ class BuilderTest(unittest.TestCase):
         LigatureSubtable AlternateSubtable MultipleSubstSubtable 
         SingleSubstSubtable aalt_chain_contextual_subst AlternateChained 
         MultipleLookupsPerGlyph MultipleLookupsPerGlyph2 GSUB_6_formats
-        GSUB_5_formats delete_glyph STAT_test STAT_test_elidedFallbackNameID
+        delete_glyph STAT_test STAT_test_elidedFallbackNameID
         variable_scalar_valuerecord variable_scalar_anchor variable_conditionset
     """.split()
 
