c6b2ba5286a979f6fddd49ad738b6b827bfca894
Cosimo Lupo <clupo@google.com>
2022-07-29 13:05:26 +0200

    [varLib.merge] fix NameError: 'NotANone'
    Fixes https://github.com/fonttools/fonttools/issues/2712

--- Lib/fontTools/varLib/errors.py
+++ Lib/fontTools/varLib/errors.py
@@ -120,6 +120,20 @@ class FoundANone(VarLibMergeError):
         return f"{stack[0]}=={cause['got']}\n"
 
 
+class NotANone(VarLibMergeError):
+    """one of the values in a list was not empty when it should have been"""
+
+    @property
+    def offender(self):
+        index = [x is not None for x in self.cause["got"]].index(True)
+        return index, self._master_name(index)
+
+    @property
+    def details(self):
+        cause, stack = self.cause, self.stack
+        return f"{stack[0]}=={cause['got']}\n"
+
+
 class MismatchedTypes(VarLibMergeError):
     """data had inconsistent types"""
 
