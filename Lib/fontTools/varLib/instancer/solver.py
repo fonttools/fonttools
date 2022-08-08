245c3e957586154f483ccb284e29e5ecf2b67b66
Behdad Esfahbod <behdad@behdad.org>
2022-08-08 11:49:49 -0600

    [instancer.solver] Comment algorithm

--- Lib/fontTools/varLib/instancer/solver.py
+++ Lib/fontTools/varLib/instancer/solver.py
@@ -23,7 +23,8 @@ def _solve(tent, axisLimit):
 
     # case 2: only the peak and outermost bound fall outside the new limit;
     # we keep the deltaset, update peak and outermost bound and and scale deltas
-    # by the scalar value for the restricted axis at the new limit.
+    # by the scalar value for the restricted axis at the new limit, and solve
+    # recursively.
     if axisMax < peak:
         mult = supportScalar({'tag': axisMax}, {'tag': tent})
         tent = (lower, axisMax, axisMax)
@@ -36,30 +37,41 @@ def _solve(tent, axisLimit):
 
     # First, the positive side
 
-    # case 3a: gain is more than outGain.
+    # outGain is the scalar of axisMax at the tent.
     outGain = supportScalar({'tag': axisMax}, {'tag': tent})
 
+    # case 3a: gain is more than outGain. The tent down-slope crosses
+    # the axis into negative. We have to split it into multiples.
     if gain > outGain:
 
+        # Crossing point on the axis.
         crossing = peak + ((1 - gain) * (upper - peak) / (1 - outGain))
 
         loc = (peak, peak, crossing)
         scalar = 1
 
+        # The part before the crossing point.
         out.append((scalar - gain, loc))
 
-        # case 3a1, similar to case 1neg
+        # The part after the crossing point may use one or two tents,
+        # depending on whether upper is before axisMax or not, in one
+        # case we need to keep it down to eternity.
+
+        # case 3a1, similar to case 1neg; just one tent needed.
         if upper >= axisMax:
             loc = (crossing, axisMax, axisMax)
             scalar = supportScalar({'tag': axisMax}, {'tag': tent})
 
             out.append((scalar - gain, loc))
 
-        # case 3a2, similar to case 2neg
+        # case 3a2, similar to case 2neg; two tents needed, to keep
+        # down to eternity.
         else:
+            # Downslope.
             loc1 = (crossing, upper, axisMax)
             scalar1 = 0
 
+            # Eternity justify.
             loc2 = (upper, axisMax, axisMax)
             scalar2 = supportScalar({'tag': axisMax}, {'tag': tent})
 
@@ -77,13 +89,13 @@ def _solve(tent, axisLimit):
 
         loc = (max(axisDef, lower), peak, upper)
 
+        # Don't add a dirac delta!
         if upper > axisDef:
             out.append((1 - gain, loc))
 
-    # case 4: new limit doesn't fit; we need to chop the deltaset into two 'tents',
-    # because the shape of a triangle with part of one side cut off cannot be
-    # represented as a triangle itself. It can be represented as sum of two triangles.
-    # NOTE: This increases the file size!
+    # case 4: new limit doesn't fit; we need to chop into two tents,
+    # because the shape of a triangle with part of one side cut off
+    # cannot be represented as a triangle itself.
     else:
 
         loc1 = (max(axisDef, lower), peak, axisMax)
@@ -93,25 +105,28 @@ def _solve(tent, axisLimit):
         scalar2 = supportScalar({'tag': axisMax}, {'tag': tent})
 
         out.append((scalar1 - gain, loc1))
+        # Don't add a dirac delta!
         if (peak < axisMax):
             out.append((scalar2 - gain, loc2))
 
 
     # Now, the negative side
 
-    # case 1neg: lower extends beyond axisMin: we chop.
+    # case 1neg: lower extends beyond axisMin: we chop. Simple.
     if lower <= axisMin:
         loc = (axisMin, axisMin, axisDef)
         scalar = supportScalar({'tag': axisMin}, {'tag': tent})
 
         out.append((scalar - gain, loc))
 
-    # case 2neg: lower is betwen axisMin and axisDef: we add two deltasets to
-    # keep it "up" all the way to end.
+    # case 2neg: lower is betwen axisMin and axisDef: we add two
+    # deltasets to # keep it down all the way to eternity.
     else:
+        # Downslope.
         loc1 = (axisMin, lower, axisDef)
         scalar1 = 0
 
+        # Eternity justify.
         loc2 = (axisMin, axisMin, lower)
         scalar2 = 0
 
