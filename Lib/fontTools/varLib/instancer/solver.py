7f79fad3d04bccec93138604dda725027e142e89
Behdad Esfahbod <behdad@behdad.org>
2022-08-08 11:27:17 -0600

    [instancer.solver] Remove unused _solveWithoutGain()

--- Lib/fontTools/varLib/instancer/solver.py
+++ Lib/fontTools/varLib/instancer/solver.py
@@ -5,44 +5,29 @@ from functools import cache
 def _revnegate(v):
     return (-v[2], -v[1], -v[0])
 
-def _solveWithoutGain(tent, axisLimit):
+
+def _solve(tent, axisLimit):
     axisMin, axisDef, axisMax = axisLimit
     lower, peak, upper = tent
 
-    # axisMin <= axisDef <= lower < peak <= axisMax
+    # Mirror the problem such that axisDef is always <= peak
+    if axisDef > peak:
+        return [(scalar, _revnegate(t) if t is not None else None)
+                for scalar,t
+                in _solve(_revnegate(tent), _revnegate(axisLimit))]
+    # axisDef <= peak
 
-    # case 3: outermost limit still fits within F2Dot14 bounds;
-    # we keep deltas as is and only scale the axes bounds. Deltas beyond -1.0
-    # or +1.0 will never be applied as implementations must clamp to that range.
-    if axisDef + (axisMax - axisDef) * 2 >= upper:
+    # case 1: the whole deltaset falls outside the new limit; we can drop it
+    if axisMax <= lower and axisMax < peak:
+        return [] # No overlap
 
-        if axisDef + (axisMax - axisDef) * MAX_F2DOT14 < upper:
-            # we clamp +2.0 to the max F2Dot14 (~1.99994) for convenience
-            upper = axisDef + (axisMax - axisDef) * MAX_F2DOT14
-
-        return [(1, (lower, peak, upper))]
-
-    # case 4: new limit doesn't fit; we need to chop the deltaset into two 'tents',
-    # because the shape of a triangle with part of one side cut off cannot be
-    # represented as a triangle itself. It can be represented as sum of two triangles.
-    # NOTE: This increases the file size!
-    else:
-
-        loc1 = (lower, peak, axisMax)
-        scalar1 = 1
-
-        loc2 = (peak, axisMax, axisMax)
-        scalar2 = supportScalar({'tag': axisMax}, {'tag': tent})
-
-        if (peak < axisMax):
-            return [(scalar1, loc1), (scalar2, loc2)]
-        else:
-            return [(scalar1, loc1)]
-
-
-def _solveWithGain(tent, axisLimit):
-    axisMin, axisDef, axisMax = axisLimit
-    lower, peak, upper = tent
+    # case 2: only the peak and outermost bound fall outside the new limit;
+    # we keep the deltaset, update peak and outermost bound and and scale deltas
+    # by the scalar value for the restricted axis at the new limit.
+    if axisMax < peak:
+        mult = supportScalar({'tag': axisMax}, {'tag': tent})
+        tent = (lower, axisMax, axisMax)
+        return [(scalar*mult, t) for scalar,t in _solve(tent, axisLimit)]
 
     # lower <= axisDef <= peak <= axisMax
 
@@ -133,41 +118,6 @@ def _solveWithGain(tent, axisLimit):
     return out
 
 
-def _solveGeneral(tent, axisLimit):
-    axisMin, axisDef, axisMax = axisLimit
-    lower, peak, upper = tent
-
-    # Mirror the problem such that axisDef is always <= peak
-    if axisDef > peak:
-        return [(scalar, _revnegate(t) if t is not None else None)
-                for scalar,t
-                in _solveGeneral(_revnegate(tent),
-                                 _revnegate(axisLimit))]
-    # axisDef <= peak
-
-    # case 1: the whole deltaset falls outside the new limit; we can drop it
-    if axisMax <= lower and axisMax < peak:
-        return [] # No overlap
-
-    # case 2: only the peak and outermost bound fall outside the new limit;
-    # we keep the deltaset, update peak and outermost bound and and scale deltas
-    # by the scalar value for the restricted axis at the new limit.
-    if axisMax < peak:
-        mult = supportScalar({'tag': axisMax}, {'tag': tent})
-        tent = (lower, axisMax, axisMax)
-        return [(scalar*mult, t) for scalar,t in _solveGeneral(tent, axisLimit)]
-
-    # axisDef <= peak <= axisMax
-
-    if False and axisDef <= lower and axisDef < peak:
-        # No gain to carry
-        return _solveWithoutGain(tent, axisLimit)
-    else:
-        return _solveWithGain(tent, axisLimit)
-
-    raise NotImplementedError
-
-
 @cache
 def rebaseTent(tent, axisLimit):
 
@@ -179,7 +129,9 @@ def rebaseTent(tent, axisLimit):
 
     assert peak != 0
 
-    sols = _solveGeneral(tent, axisLimit)
+    sols = _solve(tent, axisLimit)
+
     n = lambda v: normalizeValue(v, axisLimit, extrapolate=True)
     sols = [(scalar, (n(v[0]), n(v[1]), n(v[2])) if v is not None else None) for scalar,v in sols if scalar != 0]
+
     return sols
