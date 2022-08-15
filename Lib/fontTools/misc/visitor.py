5eb63212ad4afe85cc342d19bfab3b28b8a0b9a1
Behdad Esfahbod <behdad@behdad.org>
2022-08-15 12:03:37 -0600

    [visitor] Convert tabs to spaces and run black

--- Lib/fontTools/misc/visitor.py
+++ Lib/fontTools/misc/visitor.py
@@ -1,93 +1,99 @@
 from fontTools.ttLib import TTFont
 
+
 class Visitor(object):
 
-	defaultStop = False
+    defaultStop = False
 
-	@classmethod
-	def register(celf, clazzes, attrs=(None,)):
-		assert celf != Visitor, 'Subclass Visitor instead.'
-		if '_visitors' not in celf.__dict__:
-			celf._visitors = {}
-		if type(clazzes) != tuple:
-			clazzes = (clazzes,)
-		if type(attrs) == str:
-			attrs = (attrs,)
-		def wrapper(method):
-			assert method.__name__ == 'visit'
-			done = []
-			for clazz in clazzes:
-				if clazz in done: continue # Support multiple names of a clazz
-				done.append(clazz)
-				_visitors = celf._visitors.setdefault(clazz, {})
-				for attr in attrs:
-					assert attr not in _visitors, \
-						"Oops, class '%s' has visitor function for '%s' defined already." % (clazz.__name__, attr)
-					_visitors[attr] = method
-			return None
-		return wrapper
+    @classmethod
+    def register(celf, clazzes, attrs=(None,)):
+        assert celf != Visitor, "Subclass Visitor instead."
+        if "_visitors" not in celf.__dict__:
+            celf._visitors = {}
+        if type(clazzes) != tuple:
+            clazzes = (clazzes,)
+        if type(attrs) == str:
+            attrs = (attrs,)
 
-	@classmethod
-	def _visitorsFor(celf, thing, _default={}):
-		typ = type(thing)
+        def wrapper(method):
+            assert method.__name__ == "visit"
+            done = []
+            for clazz in clazzes:
+                if clazz in done:
+                    continue  # Support multiple names of a clazz
+                done.append(clazz)
+                _visitors = celf._visitors.setdefault(clazz, {})
+                for attr in attrs:
+                    assert attr not in _visitors, (
+                        "Oops, class '%s' has visitor function for '%s' defined already."
+                        % (clazz.__name__, attr)
+                    )
+                    _visitors[attr] = method
+            return None
 
-		for celf in celf.mro():
+        return wrapper
 
-			_visitors = getattr(celf, '_visitors', None)
-			if _visitors is None:
-				break;
+    @classmethod
+    def _visitorsFor(celf, thing, _default={}):
+        typ = type(thing)
 
-			m = celf._visitors.get(typ, None)
-			if m is not None:
-				return m
+        for celf in celf.mro():
 
-		return _default
+            _visitors = getattr(celf, "_visitors", None)
+            if _visitors is None:
+                break
 
-	def visitObject(self, obj, *args, **kwargs):
-		keys = sorted(vars(obj).keys())
-		_visitors = self._visitorsFor(obj)
-		defaultVisitor = _visitors.get('*', None)
-		for key in keys:
-			value = getattr(obj, key)
-			visitorFunc = _visitors.get(key, defaultVisitor)
-			if visitorFunc is not None:
-				ret = visitorFunc(self, obj, key, value, *args, **kwargs)
-				if ret == False or (ret is None and self.defaultStop):
-				  continue
-			self.visitAttr(obj, key, value, *args, **kwargs)
+            m = celf._visitors.get(typ, None)
+            if m is not None:
+                return m
 
-	def visitAttr(self, obj, attr, value, *args, **kwargs):
-		self.visit(value, *args, **kwargs)
+        return _default
 
-	def visitList(self, obj, *args, **kwargs):
-		for value in obj:
-			self.visit(value, *args, **kwargs)
+    def visitObject(self, obj, *args, **kwargs):
+        keys = sorted(vars(obj).keys())
+        _visitors = self._visitorsFor(obj)
+        defaultVisitor = _visitors.get("*", None)
+        for key in keys:
+            value = getattr(obj, key)
+            visitorFunc = _visitors.get(key, defaultVisitor)
+            if visitorFunc is not None:
+                ret = visitorFunc(self, obj, key, value, *args, **kwargs)
+                if ret == False or (ret is None and self.defaultStop):
+                    continue
+            self.visitAttr(obj, key, value, *args, **kwargs)
 
-	def visit(self, obj, *args, **kwargs):
-		visitorFunc = self._visitorsFor(obj).get(None, None)
-		if visitorFunc is not None:
-			ret = visitorFunc(self, obj, *args, **kwargs)
-			if ret == False or (ret is None and self.defaultStop):
-			  return
-		if hasattr(obj, '__dict__'):
-			self.visitObject(obj, *args, **kwargs)
-		elif isinstance(obj, list):
-			self.visitList(obj, *args, **kwargs)
+    def visitAttr(self, obj, attr, value, *args, **kwargs):
+        self.visit(value, *args, **kwargs)
+
+    def visitList(self, obj, *args, **kwargs):
+        for value in obj:
+            self.visit(value, *args, **kwargs)
+
+    def visit(self, obj, *args, **kwargs):
+        visitorFunc = self._visitorsFor(obj).get(None, None)
+        if visitorFunc is not None:
+            ret = visitorFunc(self, obj, *args, **kwargs)
+            if ret == False or (ret is None and self.defaultStop):
+                return
+        if hasattr(obj, "__dict__"):
+            self.visitObject(obj, *args, **kwargs)
+        elif isinstance(obj, list):
+            self.visitList(obj, *args, **kwargs)
 
 
 class TTVisitor(Visitor):
+    def visit(self, obj, *args, **kwargs):
+        if hasattr(obj, "ensureDecompiled"):
+            obj.ensureDecompiled()
+        super().visit(obj, *args, **kwargs)
 
-	def visit(self, obj, *args, **kwargs):
-		if hasattr(obj, "ensureDecompiled"):
-			obj.ensureDecompiled()
-		super().visit(obj, *args, **kwargs)
 
 @TTVisitor.register(TTFont)
 def visit(visitor, font):
-	if hasattr(font, 'visited'):
-		return False
-	font.visited = True
-	for tag in font.keys():
-		visitor.visit(font[tag])
-	del font.visited
-	return False
+    if hasattr(font, "visited"):
+        return False
+    font.visited = True
+    for tag in font.keys():
+        visitor.visit(font[tag])
+    del font.visited
+    return False
