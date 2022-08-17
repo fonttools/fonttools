33b07c757ab1fdaa2f5fc04acc2a6836ac067b6c
Behdad Esfahbod <behdad@behdad.org>
2022-08-17 14:20:36 -0600

    [ttVisitor] Add comment

--- Lib/fontTools/ttLib/ttVisitor.py
+++ Lib/fontTools/ttLib/ttVisitor.py
@@ -18,8 +18,13 @@ class TTVisitor(Visitor):
 
 @TTVisitor.register(TTFont)
 def visit(visitor, font, *args, **kwargs):
+    # Some objects have links back to TTFont; even though we
+    # have a check in visitAttr to stop them from recursing
+    # onto TTFont, sometimes they still do, for example when
+    # someone overrides visitAttr.
     if hasattr(visitor, "font"):
         return False
+
     visitor.font = font
     for tag in font.keys():
         visitor.visit(font[tag], *args, **kwargs)
