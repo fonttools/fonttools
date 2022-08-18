0268f7a67b13418aebc799e7f41675d5260d3f94
Behdad Esfahbod <behdad@behdad.org>
2022-08-18 05:08:40 -0600

    [scaleUpem] Move to ttLib

--- Lib/fontTools/ttLib/scaleUpem.py
+++ Lib/fontTools/ttLib/scaleUpem.py
@@ -153,14 +153,14 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 3:
-        print("usage: fonttools scaleUpem font new-upem")
+        print("usage: fonttools ttLib.scaleUpem font new-upem")
         sys.exit()
 
     font = TTFont(sys.argv[1])
     new_upem = int(sys.argv[2])
 
     if "CFF " in font or "CFF2" in font:
-        print("fonttools scaleUpem: CFF/CFF2 fonts are not supported.", file=sys.stderr)
+        print("fonttools ttLib.scaleUpem: CFF/CFF2 fonts are not supported.", file=sys.stderr)
         sys.exit(1)
 
     scale_upem(font, new_upem)
