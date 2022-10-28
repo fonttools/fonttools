c5b4ae37517b70e8fecca91cb46d897d0f0b6d01
Behdad Esfahbod <behdad@behdad.org>
2022-10-28 16:31:47 -0600

    [ttLib] Add main
    Just opens font files from commandline.

--- Lib/fontTools/ttLib/__init__.py
+++ Lib/fontTools/ttLib/__init__.py
@@ -2,11 +2,13 @@
 
 from fontTools.misc.loggingTools import deprecateFunction
 import logging
+import sys
 
 
 log = logging.getLogger(__name__)
 
 class TTLibError(Exception): pass
+class TTLibFileIsCollectionError (TTLibError): pass
 
 @deprecateFunction("use logging instead", category=DeprecationWarning)
 def debugmsg(msg):
@@ -15,3 +17,23 @@ def debugmsg(msg):
 
 from fontTools.ttLib.ttFont import *
 from fontTools.ttLib.ttCollection import TTCollection
+
+
+
+def main(args=None):
+	"""Open font with TTFont() or TTCollection()"""
+	from fontTools import configLogger
+
+	if args is None:
+		args = sys.argv[1:]
+
+	for f in args:
+		try:
+			font = TTFont(f)
+		except TTLibFileIsCollectionError:
+			font = TTCollection(f)
+
+		#font.save(outfile)
+
+if __name__ == "__main__":
+	sys.exit(main())
