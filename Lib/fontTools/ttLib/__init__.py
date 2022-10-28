e75726efe1fae7fea3975da951d8ebab1e1ec6c0
Behdad Esfahbod <behdad@behdad.org>
2022-10-28 16:47:45 -0600

    [ttLib] Convert fonts <-> collection in main()
    Fixes https://github.com/fonttools/fonttools/pull/2869

--- Lib/fontTools/ttLib/__init__.py
+++ Lib/fontTools/ttLib/__init__.py
@@ -19,21 +19,68 @@ from fontTools.ttLib.ttFont import *
 from fontTools.ttLib.ttCollection import TTCollection
 
 
-
 def main(args=None):
-	"""Open font with TTFont() or TTCollection()"""
+	"""Open font with TTFont() or TTCollection()
+
+	  ./fonttools ttLib [-oFILE] [-yNUMBER] files...
+
+	If multiple files are given on the command-line,
+	they are each opened (as a font or collection),
+	and added to the font list.
+
+	If -o (output-file) argument is given, the font
+	list is then saved to the output file, either as
+	a single font, if there is only one font, or as
+	a collection otherwise.
+
+	If -y (font-number) argument is given, only the
+	specified font from collections is opened.
+
+	The above allow extracting a single font from a
+	collection, or combining multiple fonts into a
+	collection.
+
+	If --lazy or --no-lazy are give, those are passed
+	to the TTFont() or TTCollection() constructors.
+	"""
 	from fontTools import configLogger
 
 	if args is None:
 		args = sys.argv[1:]
 
-	for f in args:
-		try:
-			font = TTFont(f)
-		except TTLibFileIsCollectionError:
-			font = TTCollection(f)
+	import getopt
+	options, files = getopt.getopt(args, "o:y:",
+		["lazy", "no-lazy"])
 
-		#font.save(outfile)
+	fontNumber = -1
+	outFile = None
+	lazy = None
+	for option, value in options:
+		if option == "-o":
+			outFile = value
+		elif option == "-y":
+			fontNumber = int(value)
+		elif option == "--lazy":
+			lazy = True
+		elif option == "--no-lazy":
+			lazy = False
+
+	fonts = []
+	for f in files:
+		try:
+			font = TTFont(f, fontNumber=fontNumber, lazy=lazy)
+			fonts.append(font)
+		except TTLibFileIsCollectionError:
+			collection = TTCollection(f, lazy=lazy)
+			fonts.extend(collection.fonts)
+
+		if outFile is not None:
+			if len(fonts) == 1:
+				fonts[0].save(outFile)
+			else:
+				collection = TTCollection()
+				collection.fonts = fonts
+				collection.save(outFile)
 
 if __name__ == "__main__":
 	sys.exit(main())
