1b8546c47ed55ba7342a243aa1be1de1bdb4f361
Jens Kutilek <webmail@kutilek.de>
2022-12-09 17:07:59 +0100

    Add annotations to cocaPen

--- Lib/fontTools/pens/cocoaPen.py
+++ Lib/fontTools/pens/cocoaPen.py
@@ -1,26 +1,26 @@
-from fontTools.pens.basePen import BasePen
-
+from fontTools.pens.basePen import BasePen, PenPoint
+from typing import Any, Dict, Optional
 
 __all__ = ["CocoaPen"]
 
 
 class CocoaPen(BasePen):
 
-	def __init__(self, glyphSet, path=None):
+	def __init__(self, glyphSet: Optional[Dict[str, Any]], path: Optional[Any] = None) -> None:
 		BasePen.__init__(self, glyphSet)
 		if path is None:
 			from AppKit import NSBezierPath
 			path = NSBezierPath.bezierPath()
 		self.path = path
 
-	def _moveTo(self, p):
+	def _moveTo(self, p: PenPoint) -> None:
 		self.path.moveToPoint_(p)
 
-	def _lineTo(self, p):
+	def _lineTo(self, p: PenPoint) -> None:
 		self.path.lineToPoint_(p)
 
-	def _curveToOne(self, p1, p2, p3):
+	def _curveToOne(self, p1: PenPoint, p2: PenPoint, p3: PenPoint) -> None:
 		self.path.curveToPoint_controlPoint1_controlPoint2_(p3, p1, p2)
 
-	def _closePath(self):
+	def _closePath(self) -> None:
 		self.path.closePath()
