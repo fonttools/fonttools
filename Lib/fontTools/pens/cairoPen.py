9366b9bdd60243f4a3e7063c9a6d25192a0c3483
Jens Kutilek <webmail@kutilek.de>
2022-12-09 17:07:46 +0100

    Add annotations to cairoPen

--- Lib/fontTools/pens/cairoPen.py
+++ Lib/fontTools/pens/cairoPen.py
@@ -1,6 +1,7 @@
 """Pen to draw to a Cairo graphics library context."""
 
-from fontTools.pens.basePen import BasePen
+from typing import Any, Dict, Optional
+from fontTools.pens.basePen import BasePen, PenError, PenPoint
 
 
 __all__ = ["CairoPen"]
@@ -9,18 +10,20 @@ __all__ = ["CairoPen"]
 class CairoPen(BasePen):
     """Pen to draw to a Cairo graphics library context."""
 
-    def __init__(self, glyphSet, context):
+    def __init__(self, glyphSet: Optional[Dict[str, Any]] = None, context: Optional[Any] = None) -> None:
         BasePen.__init__(self, glyphSet)
+        if context is None:
+            raise PenError("Must supply a context")
         self.context = context
 
-    def _moveTo(self, p):
+    def _moveTo(self, p: PenPoint) -> None:
         self.context.move_to(*p)
 
-    def _lineTo(self, p):
+    def _lineTo(self, p: PenPoint) -> None:
         self.context.line_to(*p)
 
-    def _curveToOne(self, p1, p2, p3):
+    def _curveToOne(self, p1: PenPoint, p2: PenPoint, p3: PenPoint) -> None:
         self.context.curve_to(*p1, *p2, *p3)
 
-    def _closePath(self):
+    def _closePath(self) -> None:
         self.context.close_path()
