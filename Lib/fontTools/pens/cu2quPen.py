ac116b18d0f2554539a3d715b4b6ab9510ebe7bb
Nikolaus Waxweiler <madigens@gmail.com>
2022-12-25 21:54:04 +0000

    Type Cu2Qu pens

--- Lib/fontTools/pens/cu2quPen.py
+++ Lib/fontTools/pens/cu2quPen.py
@@ -12,11 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+from typing import Iterable, Sequence
+
 from fontTools.cu2qu import curve_to_quadratic, curves_to_quadratic
 from fontTools.pens.basePen import AbstractPen, decomposeSuperBezierSegment
 from fontTools.pens.reverseContourPen import ReverseContourPen
-from fontTools.pens.pointPen import BasePointToSegmentPen
+from fontTools.pens.pointPen import (
+    AbstractPointPen,
+    BasePointToSegmentPen,
+    SegmentPoints,
+    Segments,
+)
 from fontTools.pens.pointPen import ReverseContourPointPen
+from fontTools.pens.typings import Point, Transformation
 
 
 class Cu2QuPen(AbstractPen):
@@ -42,12 +51,13 @@ class Cu2QuPen(AbstractPen):
 
     def __init__(
         self,
-        other_pen,
-        max_err,
-        reverse_direction=False,
-        stats=None,
-        ignore_single_points=False,
-    ):
+        other_pen: AbstractPen,
+        max_err: float,
+        reverse_direction: bool = False,
+        stats: dict[str, int] | None = None,
+        ignore_single_points: bool = False,
+    ) -> None:
+        self.pen: AbstractPen
         if reverse_direction:
             self.pen = ReverseContourPen(other_pen)
         else:
@@ -64,38 +74,39 @@ class Cu2QuPen(AbstractPen):
                 stacklevel=2,
             )
         self.ignore_single_points = ignore_single_points
-        self.start_pt = None
-        self.current_pt = None
+        self.start_pt: Point | None = None
+        self.current_pt: Point | None = None
 
-    def _check_contour_is_open(self):
+    def _check_contour_is_open(self) -> None:
         if self.current_pt is None:
             raise AssertionError("moveTo is required")
 
-    def _check_contour_is_closed(self):
+    def _check_contour_is_closed(self) -> None:
         if self.current_pt is not None:
             raise AssertionError("closePath or endPath is required")
 
-    def _add_moveTo(self):
+    def _add_moveTo(self) -> None:
         if self.start_pt is not None:
             self.pen.moveTo(self.start_pt)
             self.start_pt = None
 
-    def moveTo(self, pt):
+    def moveTo(self, pt: Point) -> None:
         self._check_contour_is_closed()
         self.start_pt = self.current_pt = pt
         if not self.ignore_single_points:
             self._add_moveTo()
 
-    def lineTo(self, pt):
+    def lineTo(self, pt: Point) -> None:
         self._check_contour_is_open()
         self._add_moveTo()
         self.pen.lineTo(pt)
         self.current_pt = pt
 
-    def qCurveTo(self, *points):
+    def qCurveTo(self, *points: Point | None) -> None:
         self._check_contour_is_open()
         n = len(points)
         if n == 1:
+            assert points[0] is not None
             self.lineTo(points[0])
         elif n > 1:
             self._add_moveTo()
@@ -104,7 +115,7 @@ class Cu2QuPen(AbstractPen):
         else:
             raise AssertionError("illegal qcurve segment point count: %d" % n)
 
-    def _curve_to_quadratic(self, pt1, pt2, pt3):
+    def _curve_to_quadratic(self, pt1: Point, pt2: Point, pt3: Point) -> None:
         curve = (self.current_pt, pt1, pt2, pt3)
         quadratic = curve_to_quadratic(curve, self.max_err)
         if self.stats is not None:
@@ -112,7 +123,7 @@ class Cu2QuPen(AbstractPen):
             self.stats[n] = self.stats.get(n, 0) + 1
         self.qCurveTo(*quadratic[1:])
 
-    def curveTo(self, *points):
+    def curveTo(self, *points: Point) -> None:
         self._check_contour_is_open()
         n = len(points)
         if n == 3:
@@ -128,20 +139,20 @@ class Cu2QuPen(AbstractPen):
         else:
             raise AssertionError("illegal curve segment point count: %d" % n)
 
-    def closePath(self):
+    def closePath(self) -> None:
         self._check_contour_is_open()
         if self.start_pt is None:
             # if 'start_pt' is _not_ None, we are ignoring single-point paths
             self.pen.closePath()
         self.current_pt = self.start_pt = None
 
-    def endPath(self):
+    def endPath(self) -> None:
         self._check_contour_is_open()
         if self.start_pt is None:
             self.pen.endPath()
         self.current_pt = self.start_pt = None
 
-    def addComponent(self, glyphName, transformation):
+    def addComponent(self, glyphName: str, transformation: Transformation) -> None:
         self._check_contour_is_closed()
         self.pen.addComponent(glyphName, transformation)
 
@@ -159,8 +170,15 @@ class Cu2QuPointPen(BasePointToSegmentPen):
         stats: a dictionary counting the point numbers of quadratic segments.
     """
 
-    def __init__(self, other_point_pen, max_err, reverse_direction=False, stats=None):
+    def __init__(
+        self,
+        other_point_pen: AbstractPointPen,
+        max_err: float,
+        reverse_direction: bool = False,
+        stats: dict[str, int] | None = None,
+    ) -> None:
         BasePointToSegmentPen.__init__(self)
+        self.pen: AbstractPointPen
         if reverse_direction:
             self.pen = ReverseContourPointPen(other_point_pen)
         else:
@@ -168,10 +186,10 @@ class Cu2QuPointPen(BasePointToSegmentPen):
         self.max_err = max_err
         self.stats = stats
 
-    def _flushContour(self, segments):
+    def _flushContour(self, segments: Segments) -> None:
         assert len(segments) >= 1
         closed = segments[0][0] != "move"
-        new_segments = []
+        new_segments: Segments = []
         prev_points = segments[-1][1]
         prev_on_curve = prev_points[-1][0]
         for segment_type, points in segments:
@@ -184,12 +202,13 @@ class Cu2QuPointPen(BasePointToSegmentPen):
                     if self.stats is not None:
                         n = str(len(quad) - 2)
                         self.stats[n] = self.stats.get(n, 0) + 1
+                    new_points: SegmentPoints
                     new_points = [(pt, False, None, {}) for pt in quad[1:-1]]
                     new_points.append((on_curve, smooth, name, kwargs))
-                    new_segments.append(["qcurve", new_points])
+                    new_segments.append(("qcurve", new_points))
                     prev_on_curve = sub_points[-1][0]
             else:
-                new_segments.append([segment_type, points])
+                new_segments.append((segment_type, points))
                 prev_on_curve = points[-1][0]
         if closed:
             # the BasePointToSegmentPen.endPath method that calls _flushContour
@@ -198,7 +217,9 @@ class Cu2QuPointPen(BasePointToSegmentPen):
             new_segments = new_segments[-1:] + new_segments[:-1]
         self._drawPoints(new_segments)
 
-    def _split_super_bezier_segments(self, points):
+    def _split_super_bezier_segments(
+        self, points: SegmentPoints
+    ) -> list[SegmentPoints]:
         sub_segments = []
         # n is the number of control points
         n = len(points) - 1
@@ -210,9 +231,9 @@ class Cu2QuPointPen(BasePointToSegmentPen):
             on_curve, smooth, name, kwargs = points[-1]
             num_sub_segments = n - 1
             for i, sub_points in enumerate(
-                decomposeSuperBezierSegment([pt for pt, _, _, _ in points])
+                decomposeSuperBezierSegment([pt for pt, _, _, _ in points])  # type: ignore
             ):
-                new_segment = []
+                new_segment: SegmentPoints = []
                 for point in sub_points[:-1]:
                     new_segment.append((point, False, None, {}))
                 if i == (num_sub_segments - 1):
@@ -226,7 +247,7 @@ class Cu2QuPointPen(BasePointToSegmentPen):
             raise AssertionError("expected 2 control points, found: %d" % n)
         return sub_segments
 
-    def _drawPoints(self, segments):
+    def _drawPoints(self, segments: Segments) -> None:
         pen = self.pen
         pen.beginPath()
         last_offcurves = []
@@ -236,6 +257,8 @@ class Cu2QuPointPen(BasePointToSegmentPen):
                     points
                 )
                 pt, smooth, name, kwargs = points[0]
+                assert pt is not None
+                assert kwargs is not None
                 pen.addPoint(pt, segment_type, smooth, name, **kwargs)
             elif segment_type == "qcurve":
                 assert len(points) >= 2, "illegal qcurve segment point count: %d" % len(
@@ -249,6 +272,8 @@ class Cu2QuPointPen(BasePointToSegmentPen):
                         last_offcurves = offcurves
                     else:
                         for (pt, smooth, name, kwargs) in offcurves:
+                            assert pt is not None
+                            assert kwargs is not None
                             pen.addPoint(pt, None, smooth, name, **kwargs)
                 pt, smooth, name, kwargs = points[-1]
                 if pt is None:
@@ -257,15 +282,19 @@ class Cu2QuPointPen(BasePointToSegmentPen):
                     # protocol's qCurveTo() method and fontTools.pens.basePen
                     pass
                 else:
+                    assert pt is not None
+                    assert kwargs is not None
                     pen.addPoint(pt, segment_type, smooth, name, **kwargs)
             else:
                 # 'curve' segments must have been converted to 'qcurve' by now
                 raise AssertionError("unexpected segment type: %r" % segment_type)
         for (pt, smooth, name, kwargs) in last_offcurves:
+            assert pt is not None
+            assert kwargs is not None
             pen.addPoint(pt, None, smooth, name, **kwargs)
         pen.endPath()
 
-    def addComponent(self, baseGlyphName, transformation):
+    def addComponent(self, baseGlyphName: str, transformation: Transformation) -> None:  # type: ignore
         assert self.currentPath is None
         self.pen.addComponent(baseGlyphName, transformation)
 
@@ -288,7 +317,12 @@ class Cu2QuMultiPen:
     each of the pens in other_pens.
     """
 
-    def __init__(self, other_pens, max_err, reverse_direction=False):
+    def __init__(
+        self,
+        other_pens: Sequence[AbstractPen],
+        max_err: float,
+        reverse_direction: bool = False,
+    ) -> None:
         if reverse_direction:
             other_pens = [
                 ReverseContourPen(pen, outputImpliedClosingLine=True)
@@ -296,76 +330,81 @@ class Cu2QuMultiPen:
             ]
         self.pens = other_pens
         self.max_err = max_err
-        self.start_pts = None
-        self.current_pts = None
+        # NOTE: Let's ignore the special case that qCurves may contain None as the last
+        # Point.
+        self.start_pts: list[tuple[Point, ...]] | None = None
+        self.current_pts: list[tuple[Point, ...]] | None = None
 
-    def _check_contour_is_open(self):
+    def _check_contour_is_open(self) -> None:
         if self.current_pts is None:
             raise AssertionError("moveTo is required")
 
-    def _check_contour_is_closed(self):
+    def _check_contour_is_closed(self) -> None:
         if self.current_pts is not None:
             raise AssertionError("closePath or endPath is required")
 
-    def _add_moveTo(self):
+    def _add_moveTo(self) -> None:
         if self.start_pts is not None:
             for pt, pen in zip(self.start_pts, self.pens):
                 pen.moveTo(*pt)
             self.start_pts = None
 
-    def moveTo(self, pts):
+    def moveTo(self, pts: list[tuple[Point, ...]]) -> None:
         self._check_contour_is_closed()
         self.start_pts = self.current_pts = pts
         self._add_moveTo()
 
-    def lineTo(self, pts):
+    def lineTo(self, pts: list[tuple[Point, ...]]) -> None:
         self._check_contour_is_open()
         self._add_moveTo()
         for pt, pen in zip(pts, self.pens):
             pen.lineTo(*pt)
         self.current_pts = pts
 
-    def qCurveTo(self, pointsList):
+    def qCurveTo(self, pointsList: list[tuple[Point | None, ...]]) -> None:
         self._check_contour_is_open()
         if len(pointsList[0]) == 1:
-            self.lineTo([(points[0],) for points in pointsList])
+            self.lineTo([(points[0],) for points in pointsList])  # type: ignore
             return
         self._add_moveTo()
         current_pts = []
         for points, pen in zip(pointsList, self.pens):
             pen.qCurveTo(*points)
             current_pts.append((points[-1],))
-        self.current_pts = current_pts
+        self.current_pts = current_pts  # type: ignore
 
-    def _curves_to_quadratic(self, pointsList):
+    def _curves_to_quadratic(self, pointsList: list[tuple[Point, ...]]) -> None:
         curves = []
+        assert self.current_pts is not None
         for current_pt, points in zip(self.current_pts, pointsList):
             curves.append(current_pt + points)
         quadratics = curves_to_quadratic(curves, [self.max_err] * len(curves))
         pointsList = []
         for quadratic in quadratics:
             pointsList.append(quadratic[1:])
-        self.qCurveTo(pointsList)
+        self.qCurveTo(pointsList)  # type: ignore
 
-    def curveTo(self, pointsList):
+    def curveTo(self, pointsList: list[tuple[Point, ...]]) -> None:
         self._check_contour_is_open()
         self._curves_to_quadratic(pointsList)
 
-    def closePath(self):
+    def closePath(self) -> None:
         self._check_contour_is_open()
         if self.start_pts is None:
             for pen in self.pens:
                 pen.closePath()
         self.current_pts = self.start_pts = None
 
-    def endPath(self):
+    def endPath(self) -> None:
         self._check_contour_is_open()
         if self.start_pts is None:
             for pen in self.pens:
                 pen.endPath()
         self.current_pts = self.start_pts = None
 
-    def addComponent(self, glyphName, transformations):
+    def addComponent(
+        self, glyphName: str, transformations: Iterable[Transformation]
+    ) -> None:
         self._check_contour_is_closed()
         for trans, pen in zip(transformations, self.pens):
             pen.addComponent(glyphName, trans)
