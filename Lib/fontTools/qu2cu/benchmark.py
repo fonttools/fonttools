c77ddcad45c15a3e04f9231043d2a84037906710
Behdad Esfahbod <behdad@behdad.org>
2023-02-20 17:06:06 -0700

    [qu2cu.benchmark] Convert multiple quadratics splines instead of 1
    More realistic benchmark.

--- Lib/fontTools/qu2cu/benchmark.py
+++ Lib/fontTools/qu2cu/benchmark.py
@@ -5,23 +5,28 @@ from fontTools.cu2qu import curve_to_quadratic
 import random
 import timeit
 
-MAX_ERR = 0.05
+MAX_ERR = 0.5
+NUM_CURVES = 5
 
 
-def generate_curve():
-    return [
+def generate_curves(n):
+    points = [
         tuple(float(random.randint(0, 2048)) for coord in range(2))
-        for point in range(4)
+        for point in range(1 + 3 * n)
     ]
+    curves = []
+    for i in range(n):
+        curves.append(tuple(points[i * 3: i * 3 + 4]))
+    return curves
 
 
 def setup_quadratic_to_curves():
-    curve = generate_curve()
-    quadratics = curve_to_quadratic(curve, MAX_ERR)
-    return [quadratics], MAX_ERR
+    curves = generate_curves(NUM_CURVES)
+    quadratics = [curve_to_quadratic(curve, MAX_ERR) for curve in curves]
+    return quadratics, MAX_ERR
 
 
-def run_benchmark(module, function, setup_suffix="", repeat=10, number=20):
+def run_benchmark(module, function, setup_suffix="", repeat=25, number=1):
     setup_func = "setup_" + function
     if setup_suffix:
         print("%s with %s:" % (function, setup_suffix), end="")
