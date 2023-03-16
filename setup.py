43e2108f938c5264001a0aaaf2f3e7e2ddde6044
Cosimo Lupo <clupo@google.com>
2023-03-16 19:08:18 +0000

    Bump version: 4.39.2 → 4.39.3.dev0

--- setup.py
+++ setup.py
@@ -467,7 +467,7 @@ if ext_modules:
 
 setup_params = dict(
     name="fonttools",
-    version="4.39.2",
+    version="4.39.3.dev0",
     description="Tools to manipulate font files",
     author="Just van Rossum",
     author_email="just@letterror.com",
