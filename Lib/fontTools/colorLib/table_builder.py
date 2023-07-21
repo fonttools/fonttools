9d044605a976d1cb901896c4a70e1941d1543e4b
Cosimo Lupo <clupo@google.com>
2023-07-21 15:53:07 +0100

    table_builder: use a LRU cache to avoid growing unbounded
    maybe to choose the default cache size we need more experiments with real-world fonts

--- Lib/fontTools/colorLib/table_builder.py
+++ Lib/fontTools/colorLib/table_builder.py
@@ -83,11 +83,15 @@ class TableBuilder:
     based on otData info for the target class. See BuildCallbacks.
     """
 
-    def __init__(self, callbackTable=None):
+    def __init__(self, callbackTable=None, cacheMaxSize=128):
         if callbackTable is None:
             callbackTable = {}
         self._callbackTable = callbackTable
-        self.cache = {}
+        # a LRU cache of (cls, sha256(pickle.dumps(source)).digest()) => dest.
+        self.cache = collections.OrderedDict() if cacheMaxSize > 0 else None
+        # cacheMaxSize of 0 means no cache; None means no limit.
+        # Not sure what a good default cache size is, 128 is just a random number...
+        self.cacheMaxSize = cacheMaxSize
 
     def _convert(self, dest, field, converter, value):
         enumClass = getattr(converter, "enumClass", None)
@@ -128,10 +132,13 @@ class TableBuilder:
         if isinstance(source, cls):
             return source
 
-        key = (cls, sha256(pickle.dumps(source)).digest())
-        if key in self.cache:
-            # pickle/unpickle is faster than copy.deepcopy
-            return pickle.loads(pickle.dumps(self.cache[key]))
+        if self.cache is not None:
+            cacheKey = (cls, sha256(pickle.dumps(source)).digest())
+            if cacheKey in self.cache:
+                # Move to the end to mark it as most recently used
+                self.cache.move_to_end(cacheKey)
+                # return unique copies; pickle/unpickle is faster than copy.deepcopy
+                return pickle.loads(pickle.dumps(self.cache[cacheKey]))
 
         callbackKey = (cls,)
         fmt = None
@@ -187,7 +194,11 @@ class TableBuilder:
             (BuildCallback.AFTER_BUILD,) + callbackKey, lambda d: d
         )(dest)
 
-        self.cache[key] = dest
+        if self.cache is not None:
+            if self.cacheMaxSize is not None and len(self.cache) >= self.cacheMaxSize:
+                # Evict the least recently used item at the front of the cache
+                self.cache.popitem(last=False)
+            self.cache[cacheKey] = dest
 
         return dest
 
