5738b4638a30d424857da468e926d57f7ac60879
Behdad Esfahbod <behdad@behdad.org>
2023-11-27 12:57:57 -0700

    [interpolatable] Remove fixing countour start order for kinks
    This was broken if multiple master-pairs tried to do the
    same. Just leave it.

--- Lib/fontTools/varLib/interpolatablePlot.py
+++ Lib/fontTools/varLib/interpolatablePlot.py
@@ -950,22 +950,6 @@ class InterpolatablePlot:
                     converter
                 )
 
-                if which == 1 or midway:
-                    wrong_start_point_problem = [
-                        pt
-                        for pt in problems
-                        if pt["type"] == "wrong_start_point"
-                        and pt.get("contour") == idx
-                    ]
-                    if wrong_start_point_problem:
-                        proposed_start = wrong_start_point_problem[0]["value_2"]
-                        points.value = (
-                            points.value[proposed_start:]
-                            + points.value[:proposed_start]
-                        )
-                        if wrong_start_point_problem[0]["reversed"]:
-                            points.value = points.value[::-1]
-
                 targetPoint = points.value[problem["value"]][0]
                 cr.save()
                 cr.translate(*targetPoint)
