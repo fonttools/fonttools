aceaf10b78be3949c4f255e389219e449a8794fd
Behdad Esfahbod <behdad@behdad.org>
2023-12-05 15:10:57 -0700

    [interpolatableTestStartingPoint] Remove unneeded code

--- Lib/fontTools/varLib/interpolatableTestStartingPoint.py
+++ Lib/fontTools/varLib/interpolatableTestStartingPoint.py
@@ -9,16 +9,11 @@ def test_starting_point(glyph0, glyph1, ix, tolerance, matching):
     m0Vectors = glyph0.greenVectors
     m1Vectors = [glyph1.greenVectors[i] for i in matching]
 
-    proposed_point = 0
-    reverse = False
-    min_cost = first_cost = 1
-
     c0 = contour0[0]
     # Next few lines duplicated below.
     costs = [vdiff_hypot2_complex(c0[0], c1[0]) for c1 in contour1]
     min_cost_idx, min_cost = min(enumerate(costs), key=lambda x: x[1])
     first_cost = costs[0]
-
     proposed_point = contour1[min_cost_idx][1]
     reverse = contour1[min_cost_idx][2]
 
