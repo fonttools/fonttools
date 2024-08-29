558ccfeef4549198148bb8cd2da732b70b5a19ce
NFSL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2024-08-29 16:30:02 +0000

    allow passing in TTFont, change to use argparse

--- Lib/fontTools/merge/__init__.py
+++ Lib/fontTools/merge/__init__.py
@@ -10,12 +10,15 @@ from fontTools.merge.cmap import (
     renameCFFCharStrings,
 )
 from fontTools.merge.layout import layoutPreMerge, layoutPostMerge
-from fontTools.merge.options import Options
 import fontTools.merge.tables
 from fontTools.misc.loggingTools import Timer
 from functools import reduce
+import argparse
+import os
 import sys
 import logging
+from types import SimpleNamespace
+from copy import deepcopy
 
 
 log = logging.getLogger("fontTools.merge")
@@ -27,24 +30,24 @@ class Merger(object):
 
     This class merges multiple files into a single OpenType font, taking into
     account complexities such as OpenType layout (``GSUB``/``GPOS``) tables and
-    cross-font metrics (for example ``hhea.ascent`` is set to the maximum value
-    across all the fonts).
+    cross-font metrics (e.g. ``hhea.ascent`` is set to the maximum value across
+    all the fonts).
 
     If multiple glyphs map to the same Unicode value, and the glyphs are considered
     sufficiently different (that is, they differ in any of paths, widths, or
     height), then subsequent glyphs are renamed and a lookup in the ``locl``
     feature will be created to disambiguate them. For example, if the arguments
     are an Arabic font and a Latin font and both contain a set of parentheses,
-    the Latin glyphs will be renamed to ``parenleft.1`` and ``parenright.1``,
+    the Latin glyphs will be renamed to ``parenleft#1`` and ``parenright#1``,
     and a lookup will be inserted into the to ``locl`` feature (creating it if
     necessary) under the ``latn`` script to substitute ``parenleft`` with
-    ``parenleft.1`` etc.
+    ``parenleft#1`` etc.
 
     Restrictions:
 
     - All fonts must have the same units per em.
     - If duplicate glyph disambiguation takes place as described above then the
-      fonts must have a ``GSUB`` table.
+            fonts must have a ``GSUB`` table.
 
     Attributes:
             options: Currently unused.
@@ -52,7 +55,13 @@ class Merger(object):
 
     def __init__(self, options=None):
         if not options:
-            options = Options()
+            options = SimpleNamespace(**{
+                "drop_tables": [],
+                "font_files": []
+            })
+
+        assert hasattr(options, "drop_tables")
+        assert hasattr(options, "font_files")
 
         self.options = options
 
@@ -62,6 +71,9 @@ class Merger(object):
             font._merger__fontfile = fontfile
             font._merger__name = font["name"].getDebugName(4)
         return fonts
+    
+    def openFonts(self, fontfiles):
+        return self._openFonts(fontfiles)
 
     def merge(self, fontfiles):
         """Merges fonts together.
@@ -73,18 +85,30 @@ class Merger(object):
                 A :class:`fontTools.ttLib.TTFont` object. Call the ``save`` method on
                 this to write it out to an OTF file.
         """
+        fonts = self._openFonts(fontfiles)
+        return self.mergeTTFonts(fonts)
+    
+    def mergeTTFonts(self, ttfonts):
+        """Merges fonts together.
+
+        Args:
+                ttfonts: A list of :class:`fontTools.ttLib.TTFont` objects to be merged
+
+        Returns:
+                A :class:`fontTools.ttLib.TTFont` object. Call the ``save`` method on
+                this to write it out to an OTF file.
+        """
         #
         # Settle on a mega glyph order.
         #
-        fonts = self._openFonts(fontfiles)
-        glyphOrders = [list(font.getGlyphOrder()) for font in fonts]
+        glyphOrders = [list(font.getGlyphOrder()) for font in ttfonts]
         computeMegaGlyphOrder(self, glyphOrders)
 
         # Take first input file sfntVersion
-        sfntVersion = fonts[0].sfntVersion
+        sfntVersion = ttfonts[0].sfntVersion
 
         # Reload fonts and set new glyph names on them.
-        fonts = self._openFonts(fontfiles)
+        fonts = deepcopy(ttfonts)
         for font, glyphOrder in zip(fonts, glyphOrders):
             font.setGlyphOrder(glyphOrder)
             if "CFF " in font:
@@ -172,76 +196,62 @@ class Merger(object):
 
 __all__ = ["Options", "Merger", "main"]
 
+class SplitArgs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values.split(','))
 
 @timer("make one with everything (TOTAL TIME)")
 def main(args=None):
     """Merge multiple fonts into one"""
     from fontTools import configLogger
 
-    if args is None:
-        args = sys.argv[1:]
+    parser = argparse.ArgumentParser(
+                    prog='pyftmerge',
+                    description='Merge multiple fonts into one font',)
+    
+    parser.add_argument('font_files', metavar='font', type=str, nargs='*',
+                        help='Files to merge')
+    parser.add_argument('--input-file', type=str, help='Read font files to merge from a text file, each path new line. # Comment lines allowed.')
+    parser.add_argument('--output-file', type=str, help='Specify output file name (default: merged.ttf)', default='merged.ttf')
+    parser.add_argument('--import-file', type=str, help='TTX file to import after merging. This can be used to set metadata.')
+    parser.add_argument('--drop-tables', type=str, action=SplitArgs, help='Comma separated list of table tags to skip, case sensitive.', default=[])
+    parser.add_argument('--verbose', action='store_true', help='Output progress information.', default=False)
+    parser.add_argument('--timing', action='store_true', help='Output progress timing.', default=False)
 
-    options = Options()
-    args = options.parse_opts(args)
-    fontfiles = []
-    if options.input_file:
-        with open(options.input_file) as inputfile:
+    args = parser.parse_args(args)
+
+    # File validation
+    if args.input_file is not None and not os.path.exists(args.input_file):
+        parser.error(f'Input font lists {args.input_file} through --input-file does not exist')
+    if args.import_file is not None and not os.path.exists(args.import_file):
+        parser.error(f'Import TTC file {args.import_file} through --import-file does not exist')
+
+    if args.input_file:
+        with open(args.input_file) as inputfile:
             fontfiles = [
                 line.strip()
                 for line in inputfile.readlines()
                 if not line.lstrip().startswith("#")
             ]
-    for g in args:
-        fontfiles.append(g)
+            args.font_files.extend(fontfiles)
 
-    if len(fontfiles) < 1:
-        print(
-            "usage: pyftmerge [font1 ... fontN] [--input-file=filelist.txt] [--output-file=merged.ttf] [--import-file=tables.ttx]",
-            file=sys.stderr,
-        )
-        print(
-            "                                   [--drop-tables=tags] [--verbose] [--timing]",
-            file=sys.stderr,
-        )
-        print("", file=sys.stderr)
-        print(" font1 ... fontN              Files to merge.", file=sys.stderr)
-        print(
-            " --input-file=<filename>      Read files to merge from a text file, each path new line. # Comment lines allowed.",
-            file=sys.stderr,
-        )
-        print(
-            " --output-file=<filename>     Specify output file name (default: merged.ttf).",
-            file=sys.stderr,
-        )
-        print(
-            " --import-file=<filename>     TTX file to import after merging. This can be used to set metadata.",
-            file=sys.stderr,
-        )
-        print(
-            " --drop-tables=<table tags>   Comma separated list of table tags to skip, case sensitive.",
-            file=sys.stderr,
-        )
-        print(
-            " --verbose                    Output progress information.",
-            file=sys.stderr,
-        )
-        print(" --timing                     Output progress timing.", file=sys.stderr)
-        return 1
+    if len(args.font_files) < 1:
+        parser.error('No font files specified through [font ...] or --input-file')
 
-    configLogger(level=logging.INFO if options.verbose else logging.WARNING)
-    if options.timing:
+    configLogger(level=logging.INFO if args.verbose else logging.WARNING)
+    if args.timing:
         timer.logger.setLevel(logging.DEBUG)
     else:
         timer.logger.disabled = True
 
-    merger = Merger(options=options)
-    font = merger.merge(fontfiles)
+    merger = Merger(options=args)
+    font = merger.merge(args.font_files)
 
-    if options.import_file:
-        font.importXML(options.import_file)
+    if args.import_file:
+        font.importXML(args.import_file)
 
     with timer("compile and save font"):
-        font.save(options.output_file)
+        font.save(args.output_file)
 
 
 if __name__ == "__main__":
