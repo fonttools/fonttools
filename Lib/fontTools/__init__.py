6af41afddfe8ab49c144eb0b6cb62077fef2c996
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2024-09-23 15:22:33 +0100

    Bump version: 4.54.0 → 4.54.1.dev0

--- Lib/fontTools/__init__.py
+++ Lib/fontTools/__init__.py
@@ -3,6 +3,6 @@ from fontTools.misc.loggingTools import configLogger
 
 log = logging.getLogger(__name__)
 
-version = __version__ = "4.54.0"
+version = __version__ = "4.54.1.dev0"
 
 __all__ = ["version", "log", "configLogger"]
--- setup.py
+++ setup.py
@@ -481,7 +481,7 @@ if ext_modules:
 
 setup_params = dict(
     name="fonttools",
-    version="4.54.0",
+    version="4.54.1.dev0",
     description="Tools to manipulate font files",
     author="Just van Rossum",
     author_email="just@letterror.com",
