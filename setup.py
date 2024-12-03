d068f84a157e105fd5794701441116adac6d0170
rsheeter <rsheeter@google.com>
2024-12-02 19:38:26 -0800

    Release 4.55.1

--- setup.py
+++ setup.py
@@ -482,7 +482,7 @@ if ext_modules:
 
 setup_params = dict(
     name="fonttools",
-    version="4.55.1.dev0",
+    version="4.55.1",
     description="Tools to manipulate font files",
     author="Just van Rossum",
     author_email="just@letterror.com",
