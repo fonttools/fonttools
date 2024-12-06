314eee05c755bbb5c46c89dad0b76586477ae888
Nathan Williis <n8willis@gmail.com>
2024-12-06 11:37:17 +0000

    [Docs] Fix docstring typo in gcid table module.

--- Lib/fontTools/ttLib/tables/_g_c_i_d.py
+++ Lib/fontTools/ttLib/tables/_g_c_i_d.py
@@ -5,7 +5,7 @@ from .otBase import BaseTTXConverter
 class table__g_c_i_d(BaseTTXConverter):
     """Glyph ID to CID table
 
-    The AAT ``gcid` table stores glyphID-to-CID mappings.
+    The AAT ``gcid`` table stores glyphID-to-CID mappings.
 
     See also https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6gcid.html
     """
