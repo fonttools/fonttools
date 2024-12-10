7e374c53da9a7443d32b31138a0e5be478bcbab9
Rod S <rsheeter@google.com>
2024-12-10 12:59:30 -0800

    Bump version: 4.55.3 → 4.55.4.dev0

--- setup.py
+++ setup.py
@@ -482,7 +482,7 @@ if ext_modules:
 
 setup_params = dict(
     name="fonttools",
-    version="4.55.3",
+    version="4.55.4.dev0",
     description="Tools to manipulate font files",
     author="Just van Rossum",
     author_email="just@letterror.com",
