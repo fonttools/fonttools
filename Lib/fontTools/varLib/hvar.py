f78997ecda24402270dbeb98f30dca88ab04c889
Behdad Esfahbod <behdad@behdad.org>
2025-02-25 13:10:58 -0700

    [varLib.hvar] Refactor and reuse code

--- Lib/fontTools/varLib/hvar.py
+++ Lib/fontTools/varLib/hvar.py
@@ -2,36 +2,17 @@ from fontTools.misc.roundTools import noRound
 from fontTools.ttLib import TTFont, newTable
 from fontTools.ttLib.tables import otTables as ot
 from fontTools.ttLib.tables.otBase import OTTableWriter
-from fontTools.varLib import HVAR_FIELDS, VVAR_FIELDS
+from fontTools.varLib import HVAR_FIELDS, VVAR_FIELDS, _add_VHVAR
 from fontTools.varLib import builder, models, varStore
 from fontTools.misc.fixedTools import fixedToFloat as fi2fl
 from fontTools.misc.cliTools import makeOutputFileName
-from itertools import product
+from functools import partial
 import logging
 
 log = logging.getLogger("fontTools.varLib.avar")
 
 
-def add_VHVAR(font, tableFields):
-    tableTag = tableFields.tableTag
-    if tableTag in font:
-        log.warning(tableTag + " table already present, overwriting.")
-        del font[tableTag]
-    log.info("Generating " + tableTag)
-    VHVAR = newTable(tableTag)
-    tableClass = getattr(ot, tableTag)
-    vhvar = VHVAR.table = tableClass()
-    vhvar.Version = 0x00010000
-    font[tableTag] = VHVAR
-
-    glyphOrder = font.getGlyphOrder()
-    vhmtx = font[tableFields.metricsTag]
-    advances = {g: vhmtx.metrics[g][0] for g in glyphOrder}
-    phantomIndex = tableFields.phantomIndex
-
-    axisTags = [a.axisTag for a in font["fvar"].axes]
-    storeBuilder = varStore.OnlineVarStoreBuilder(axisTags)
-
+def _get_advance_metrics(font, axisTags, tableFields):
     # There's two ways we can go from here:
     # 1. For each glyph, at each master peak, compute the value of the
     #    advance width at that peak.  Then pass these all to a VariationModel
@@ -40,9 +21,9 @@ def add_VHVAR(font, tableFields):
     #    and feed these to the VarStoreBuilder, forgoing the remoding step.
     # We'll go with the second option, as it's simpler, faster, and more direct.
     gvar = font["gvar"]
-    advMapping = {}
-    allSupports = {}
-    allDeltas = {}
+    vhAdvanceDeltasAndSupports = {}
+    glyphOrder = font.getGlyphOrder()
+    phantomIndex = tableFields.phantomIndex
     for glyphName in glyphOrder:
         supports = []
         deltas = []
@@ -57,65 +38,27 @@ def add_VHVAR(font, tableFields):
             phantoms[1] = phantoms[1][phantomIndex] if phantoms[1] is not None else 0
             deltas.append(phantoms[1] - phantoms[0])
 
-        storeBuilder.setSupports(supports)
-        advMapping[glyphName] = storeBuilder.storeDeltas(deltas, round=noRound)
+        vhAdvanceDeltasAndSupports[glyphName] = (deltas, supports)
 
-        allSupports[glyphName] = supports
-        allDeltas[glyphName] = deltas
+    vOrigDeltasAndSupports = None  # TODO
 
-    singleModel = models.allEqual(allSupports.values())
-
-    directStore = None
-    if singleModel:
-        # Build direct mapping
-        supports = supports
-        varTupleList = builder.buildVarRegionList(supports, axisTags)
-        varTupleIndexes = list(range(len(supports)))
-        varData = builder.buildVarData(varTupleIndexes, [], optimize=False)
-        for glyphName in glyphOrder:
-            varData.addItem(allDeltas[glyphName], round=noRound)
-        varData.optimize()
-        directStore = builder.buildVarStore(varTupleList, [varData])
-
-    # Build optimized indirect mapping
-    indirectStore = storeBuilder.finish()
-    mapping2 = indirectStore.optimize(use_NO_VARIATION_INDEX=False)
-    advMapping = [mapping2[advMapping[g]] for g in glyphOrder]
-    advanceMapping = builder.buildVarIdxMap(advMapping, glyphOrder)
-
-    useDirect = False
-    if directStore:
-        # Compile both, see which is more compact
-
-        writer = OTTableWriter()
-        directStore.compile(writer, font)
-        directSize = len(writer.getAllData())
-
-        writer = OTTableWriter()
-        indirectStore.compile(writer, font)
-        advanceMapping.compile(writer, font)
-        indirectSize = len(writer.getAllData())
-
-        useDirect = directSize < indirectSize
-
-    if useDirect:
-        metricsStore = directStore
-        advanceMapping = None
-    else:
-        metricsStore = indirectStore
-
-    vhvar.VarStore = metricsStore
-    setattr(vhvar, tableFields.advMapping, advanceMapping)
-    setattr(vhvar, tableFields.sb1, None)
-    setattr(vhvar, tableFields.sb2, None)
+    return vhAdvanceDeltasAndSupports, vOrigDeltasAndSupports
 
 
 def add_HVAR(font):
-    add_VHVAR(font, HVAR_FIELDS)
+    if "HVAR" in font:
+        del font["HVAR"]
+    axisTags = [axis.axisTag for axis in font["fvar"].axes]
+    getAdvanceMetrics = partial(_get_advance_metrics, font, axisTags, HVAR_FIELDS)
+    _add_VHVAR(font, axisTags, HVAR_FIELDS, getAdvanceMetrics)
 
 
 def add_VVAR(font):
-    add_VHVAR(font, VVAR_FIELDS)
+    if "VVAR" in font:
+        del font["VVAR"]
+    getAdvanceMetrics = partial(_get_advance_metrics, font, axisTags, HVAR_FIELDS)
+    axisTags = [axis.axisTag for axis in font["fvar"].axes]
+    _add_VHVAR(font, axisTags, VVAR_FIELDS, getAdvanceMetrics)
 
 
 def main(args=None):
