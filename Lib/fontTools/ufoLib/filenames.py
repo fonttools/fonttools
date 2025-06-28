34add16d6cda6287712800064211af54350ea2d9
knutnergaard <knut.nergaard@gmail.com>
2025-06-28 09:17:09 +0200

    Annotate `ufoLib.filenames`.

--- Lib/fontTools/ufoLib/filenames.py
+++ Lib/fontTools/ufoLib/filenames.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+from typing import Set
+
 """
 Convert user-provided internal UFO names to spec-compliant filenames.
 
@@ -27,7 +32,7 @@ by Tal Leming and is copyright (c) 2005-2016, The RoboFab Developers:
 #    inclusive.
 # 3. Various characters that (mostly) Windows and POSIX-y filesystems don't
 #    allow, plus "(" and ")", as per the specification.
-illegalCharacters = {
+illegalCharacters: Set[str] = {
     "\x00",
     "\x01",
     "\x02",
@@ -76,7 +81,7 @@ illegalCharacters = {
     "|",
     "\x7f",
 }
-reservedFileNames = {
+reservedFileNames: Set[str] = {
     "aux",
     "clock$",
     "com1",
@@ -101,14 +106,16 @@ reservedFileNames = {
     "nul",
     "prn",
 }
-maxFileNameLength = 255
+maxFileNameLength: int = 255
 
 
 class NameTranslationError(Exception):
     pass
 
 
-def userNameToFileName(userName: str, existing=(), prefix="", suffix=""):
+def userNameToFileName(
+    userName: str, existing: Iterable[str] = (), prefix: str = "", suffix: str = ""
+) -> str:
     """Converts from a user name to a file name.
 
     Takes care to avoid illegal characters, reserved file names, ambiguity between
@@ -212,7 +219,9 @@ def userNameToFileName(userName: str, existing=(), prefix="", suffix=""):
     return fullName
 
 
-def handleClash1(userName, existing=[], prefix="", suffix=""):
+def handleClash1(
+    userName: str, existing: Iterable[str] = [], prefix: str = "", suffix: str = ""
+) -> str:
     """A helper function that resolves collisions with existing names when choosing a filename.
 
     This function attempts to append an unused integer counter to the filename.
@@ -278,7 +287,9 @@ def handleClash1(userName, existing=[], prefix="", suffix=""):
     return finalName
 
 
-def handleClash2(existing=[], prefix="", suffix=""):
+def handleClash2(
+    existing: Iterable[str] = [], prefix: str = "", suffix: str = ""
+) -> str:
     """A helper function that resolves collisions with existing names when choosing a filename.
 
     This function is a fallback to :func:`handleClash1`. It attempts to append an unused integer counter to the filename.
