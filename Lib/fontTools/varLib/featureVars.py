5ae2943a4334d5bbaef6d7984c131479e6759f96
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2025-08-11 17:19:00 +0100

    Update OS/2.usMaxContext if possible after addFeatureVariationsRaw (#3894)

--- Lib/fontTools/varLib/featureVars.py
+++ Lib/fontTools/varLib/featureVars.py
@@ -95,6 +95,14 @@ def addFeatureVariations(font, conditionalSubstitutions, featureTag="rvrn"):
 
     addFeatureVariationsRaw(font, font["GSUB"].table, conditionsAndLookups, featureTags)
 
+    # Update OS/2.usMaxContext in case the font didn't have features before, but
+    # does now, if the OS/2 table exists. The table may be required, but
+    # fontTools needs to be able to deal with non-standard fonts. Since feature
+    # variations are always 1:1 mappings, we can set the value to at least 1
+    # instead of recomputing it with `otlLib.maxContextCalc.maxCtxFont()`.
+    if (os2 := font.get("OS/2")) is not None:
+        os2.usMaxContext = max(1, os2.usMaxContext)
+
 
 def _existingVariableFeatures(table):
     existingFeatureVarsTags = set()
