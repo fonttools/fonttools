efc07e234dbf435ac5dc8290b31af37a82a01fe9
knutnergaard <knut.nergaard@gmail.com>
2026-02-07 15:43:38 +0100

    Fix overlooked errors in `pointInsidePen`

--- Lib/fontTools/pens/pointInsidePen.py
+++ Lib/fontTools/pens/pointInsidePen.py
@@ -191,7 +191,7 @@ class PointInsidePen(BasePen):
         a = y3 - c - b
         solutions = sorted(solveQuadratic(a, b, c - y))
         solutions = [
-            t for t in solutions if ZERO_MINUS_EPSILON <= t <= ONE_PLUS_EPSILON
+            t for t in solutions if ZERO_MINUS_EPSILON <= t <= ONE_PLUS_EPSILON  # type: ignore[name-defined]
         ]
         if not solutions:
             return
