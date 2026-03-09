ee39edeaec03b90bc796c26bcbb6792740a4f9ee
Cosimo Lupo <clupo@google.com>
2026-03-09 16:11:20 +0000

    [tests] Remove obsolete py23 OpenFuncWrapperTest
    This test verified that fontTools.misc.py23.open (which is just
    builtins.open re-exported) can roundtrip binary data through a pipe.
    The py23 module is deprecated and nothing in fontTools imports it.
    The test was failing on free-threaded Python 3.14 on Windows (cp314t-win32):
    https://github.com/fonttools/fonttools/actions/runs/22860342656/job/66317680873#step:5:10804

--- Tests/misc/py23_test.py
+++ Tests/misc/py23_test.py
@@ -1,11 +1,5 @@
-from fontTools.misc.py23 import tobytes
-from fontTools.misc.textTools import deHexStr
-import filecmp
 from io import StringIO
-import tempfile
-from subprocess import check_call
 import sys
-import os
 import unittest
 
 from fontTools.misc.py23 import (
@@ -15,54 +9,6 @@ from fontTools.misc.py23 import (
     redirect_stdout,
     redirect_stderr,
 )
-
-
-PIPE_SCRIPT = """\
-import sys
-binary_stdin = open(sys.stdin.fileno(), mode='rb', closefd=False)
-binary_stdout = open(sys.stdout.fileno(), mode='wb', closefd=False)
-binary_stdout.write(binary_stdin.read())
-"""
-
-# the string contains a mix of line endings, plus the Win "EOF" charater (0x1A)
-# 'hello\rworld\r\n\x1a\r\n'
-TEST_BIN_DATA = deHexStr("68 65 6c 6c 6f 0d 77 6f 72 6c 64 0d 0a 1a 0d 0a")
-
-
-class OpenFuncWrapperTest(unittest.TestCase):
-    @staticmethod
-    def make_temp(data):
-        with tempfile.NamedTemporaryFile(delete=False) as f:
-            f.write(tobytes(data))
-        return f.name
-
-    def diff_piped(self, data, import_statement):
-        script = self.make_temp("\n".join([import_statement, PIPE_SCRIPT]))
-        datafile = self.make_temp(data)
-        try:
-            with (
-                open(datafile, "rb") as infile,
-                tempfile.NamedTemporaryFile(delete=False) as outfile,
-            ):
-                env = dict(os.environ)
-                env["PYTHONPATH"] = os.pathsep.join(sys.path)
-                check_call(
-                    [sys.executable, script], stdin=infile, stdout=outfile, env=env
-                )
-            result = not filecmp.cmp(infile.name, outfile.name, shallow=False)
-        finally:
-            os.remove(script)
-            os.remove(datafile)
-            os.remove(outfile.name)
-        return result
-
-    def test_binary_pipe_py23_open_wrapper(self):
-        if self.diff_piped(TEST_BIN_DATA, "from fontTools.misc.py23 import open"):
-            self.fail("Input and output data differ!")
-
-    def test_binary_pipe_built_in_io_open(self):
-        result = self.diff_piped(TEST_BIN_DATA, "from io import open")
-        self.assertEqual(result, False)
 
 
 class Round2Test(unittest.TestCase):
