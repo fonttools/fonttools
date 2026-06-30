4801a311136bfdbc35e6b1b1ddbeae9e57fa0bbe
Rob Hagemans <rob.hagemans@hotmail.com>
2026-06-30 08:46:47 +0100

    reformat w/ black

--- Lib/fontTools/ttLib/tables/_b_d_a_t.py
+++ Lib/fontTools/ttLib/tables/_b_d_a_t.py
@@ -1,4 +1,5 @@
 from fontTools.ttLib.tables.E_B_D_T_ import table_E_B_D_T_
 
+
 class table__b_d_a_t(table_E_B_D_T_):
     locatorName = "bloc"
--- Lib/fontTools/ttLib/tables/_b_h_e_d.py
+++ Lib/fontTools/ttLib/tables/_b_h_e_d.py
@@ -1,4 +1,5 @@
 from fontTools.ttLib.tables._h_e_a_d import table__h_e_a_d
 
+
 class table__b_h_e_d(table__h_e_a_d):
     pass
